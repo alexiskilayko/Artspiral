612e3bdacb33affb1621fc0488984066f3719809
nchan0154 <channicoleelise@gmail.com>
2015-08-06 17:22:55 -0400

    rewrite menu

--- _inc/menu.js
+++ _inc/menu.js
@@ -1,20 +1,9 @@
 $( document ).ready(function() {
 
-$('li.has-sub>a').on('click', function(){
+$('.has-sub>a').on('click', function(){
 		$(this).removeAttr('href');
 		var a = $(this).parent('li');
-		if (a.hasClass('open')) {
-			a.removeClass('open');
-			a.find('li').removeClass('open');
-			a.find('ul').slideUp();
-		}
-		else {
-			a.addClass('open');
-			a.children('ul').slideDown();
-			a.siblings('li').children('ul').slideUp();
-			a.siblings('li').removeClass('open');
-			a.siblings('li').find('li').removeClass('open');
-			a.siblings('li').find('ul').slideUp();
-		}
+        a.children('ul').slideToggle();
+        a.siblings('.has-sub').children('ul').slideUp();    
 	});
 });
