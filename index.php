4b041c3c28ce37bbe58d508b91fc3849511d09e5
Digital Archive <digitalarchive@Roberts-iMac-3.local>
2019-06-07 13:43:36 -0400

    Removed Cultural Equity Group from homepage

--- index.php
+++ index.php
@@ -15,20 +15,16 @@ include '_inc/head.php';
 		<p class="emphasis">The Asian American Arts Centre was founded in 1974 in New York City to address the distinctive concerns of Asian Americans in the United States.</p>
 		<p>Our mission is to promote the preservation and creative vitality of Asian American cultural growth through the arts, and its historical and aesthetic linkage to other communities. The Arts Centre accomplishes this by presenting and interpreting the ongoing synthesis of contemporary American and Asian art forms, utilizing performance, exhibition, and public education. </p>
 		<p>This website primarily serves as a record for our past programs and exhibitions, to keep up with us follow us on <a href="https://www.flickr.com/photos/asianamericanartscentre/">Flickr</a>, <a href="https://www.facebook.com/AsianAmericanArtsCentre">Facebook</a> or <a href="http://artspiral.blogspot.com/">our blog</a>.</p>
-	<section>
-		<a href="https://www.facebook.com/culturalequitygroupnyc"><img class="center" src="/_images/CEG.png"></a>
-		<p>The AAAC is proud to be on the steering committee for the Cultural Equity Group. <a href="https://www.facebook.com/culturalequitygroupnyc">Find out more <i class="fa fa-external-link"></i></a>.</p>
-	</section>	
 	<div class="col-1-2 google-maps">
 		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.9528701499758!2d-73.987135!3d40.719053999999986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259811858e801%3A0x93697cf8503caf94!2sAsian+American+Arts+Centre!5e0!3m2!1sen!2sus!4v1431553920149" frameborder="0" style="border:0"></iframe>
-	</div>	
+	</div>
 	<div class="right-align col-1-2">
 		<h3>Reach Out to Us</h3>
 		<p>Asian American Arts Centre
 		<br/>111 Norfolk Street
 		<br />New York, NY 10002</p>
 		<p>Tel: 212.233.2154</p>
-		<p>Hours: M-F 12:30pm-6:30pm 
+		<p>Hours: M-F 12:30pm-6:30pm
 		<br />(by appointment only)</p>
 		<p>Main: <a href="mailto:aaacinfo@artspiral.org">aaacinfo@artspiral.org </a>
 		<br />Archive: <a href="mailto:archive@artspiral.org">archive@artspiral.org</a></p>
@@ -40,4 +36,3 @@ include '_inc/head.php';
 </div>
 </body>
 </html>
-
