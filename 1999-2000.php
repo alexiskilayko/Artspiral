a9f416b546d5662a0620f22bba26b21384668237
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:06:27 -0400

    1999-2000 update

--- 1999-2000.php
+++ 1999-2000.php
@@ -7,146 +7,150 @@ include '_inc/head.php';
 
 <body class="exhibitions">
 <div class="page-wrapper">
-	<?php include '_inc/header.php'; ?>
-	<nav id="menu" role="navigation">
-	<?php include '_inc/menu.php';?>
-	</nav>
-	<main class="content-wrapper narrow" role="main">
-		<span class="tag">1999 &ndash; 2000</span>
-			<h2>Cross-cultural Voices II: Between Memories</h2>
-      <p><span class="date">September 1 - October 15, 1999</span></p>
-      <p>An exhibition on cross-cultural experience, focusing on Asian American and Western artists and their migrant experiences. Centers on the global sense of being exiled - not in the sense of being banished for dissidance but in the sense of moving to another country for the sake of economical and cultural reasons - and the art community’s dependence on memories to overcome such a feeling of loss. A collaboration with The Korea Society and the Stephen Gang Gallery. Curated by Young M. Park.</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Sukran Aziz</li>
-        <li>Hu Bing</li>
-        <li>Wenda Gu</li>
-        <li>Hey-Yeun Jang</li>
-        <li>Jin Seok Kim</li>
-        <li>Shu-Min Lin, 	</li>
-        <li>Osamu James Nakagawa</li>
-        <li>Leemour Pelli</li>
-        <li>Nicholetta West</li>
-			</ul>
+    <?php include '_inc/header.php'; ?>
+    <nav id="menu" role="navigation">
+    <?php include '_inc/menu.php';?>
+    </nav>
+    <main class="content-wrapper narrow" role="main">
+      <span class="tag">1999 &ndash; 2000</span>
+        <h2>Cross-cultural Voices II: Between Memories</h2>
+        <p><span class="date">September 1 - October 15, 1999</span></p>
+        <p>An exhibition on cross-cultural experience, focusing on Asian American and Western artists and their migrant experiences. Centers on the global sense of being exiled - not in the sense of being banished for dissidance but in the sense of moving to another country for the sake of economical and cultural reasons - and the art community’s dependence on memories to overcome such a feeling of loss. A collaboration with The Korea Society and the Stephen Gang Gallery. <i>Learn more: <a href="http://artasiamerica.org/documents/1853/98" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/1868/98" target="_blank">catalog</a>.</i></p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Sukran Aziz</li>
+          <li>Hu Bing</li>
+          <li>Wenda Gu</li>
+          <li>Hey-Yeun Jang</li>
+          <li>Jin Seok Kim</li>
+          <li>Shu-Min Lin</li>
+          <li>Osamu James Nakagawa</li>
+          <li>Leemour Pelli</li>
+          <li>Nicholetta West</li>
+        </ul>
 
-			<h2>Firehouse: AAAC 25th Anniversary</h2>
-			<p><span class="date">Octpber 30 – November 14, 1999</span></p>
-      <p>An exhibition of 25 artists and a celebration of the 25th anniversaries of the Asian American Arts Centre, of Asian American community leaders Rocky and May Chin’s marriage, and of Robert Lee and Eleanor Yung’s marriage. Organized by Young M. Park, Trinh Le, and Robert Lee.</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Jackie Chang</li>
-        <li>Bivas Chaudhuri</li>
-        <li>Ming Fay</li>
-        <li>Columbia Fiero</li>
-        <li>Kenta Furusho</li>
-        <li>Byron Goto</li>
-        <li>Yun Fei Ji</li>
-        <li>Jessie Joo</li>
-        <li>Earl Jung</li>
-        <li>Toshinora Kuga</li>
-        <li>Bovey Lee</li>
-        <li>Mikiya Matsuda	</li>
-        <li>Chee Wang Ng</li>
-        <li>Eung Ho Park</li>
-        <li>Howardena Pindell</li>
-        <li>Tara Sabharwal</li>
-        <li>Hilda Shen</li>
-        <li>Alex Shinohara</li>
-        <li>Noriko Shinohara</li>
-        <li>Ushio Shinohara</li>
-        <li>Osami Tanaka</li>
-        <li>Kim Tran</li>
-        <li>Dolly Unithan</li>
-        <li>Charles Yuen</li>
-			</ul>
+        <h2>Firehouse: AAAC 25th Anniversary</h2>
+        <p><span class="date">Octpber 30 – November 14, 1999</span></p>
+        <p>An exhibition of 25 artists and a celebration of the 25th anniversaries of the Asian American Arts Centre, of Asian American community leaders Rocky and May Chin’s marriage, and of Robert Lee and Eleanor Yung’s marriage. Organized by Young M. Park, Trinh Le, and Robert Lee. <i><a href="http://artasiamerica.org/documents/2418/45" target="_blank">Learn more</a>.</i></p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Jackie Chang</li>
+          <li>Bivas Chaudhuri</li>
+          <li>Ming Fay</li>
+          <li>Columbia Fiero</li>
+          <li>Kenta Furusho</li>
+          <li>Byron Goto</li>
+          <li>Yun Fei Ji</li>
+          <li>Jessie Joo</li>
+          <li>Earl Jung</li>
+          <li>Toshinora Kuga</li>
+          <li>Bovey Lee</li>
+          <li>Mikiya Matsuda  </li>
+          <li>Chee Wang Ng</li>
+          <li>Eung Ho Park</li>
+          <li>Howardena Pindell</li>
+          <li>Tara Sabharwal</li>
+          <li>Hilda Shen</li>
+          <li>Alex Shinohara</li>
+          <li>Noriko Shinohara</li>
+          <li>Ushio Shinohara</li>
+          <li>Osami Tanaka</li>
+          <li>Kim Tran</li>
+          <li>Dolly Unithan</li>
+          <li>Charles Yuen</li>
+        </ul>
 
-	    <h2>"This Used To Be a Poodle": 9th AAAC Annual Exhibition</h2>
-			<p><span class="date">October 29 - December 14, 1999</span></p>
-			<p>An exhibition of photography, sculpture, drawing, and installation. The exhibited artists demonstrate innovative approaches by young artists today, displaying a range of thought and expression in the Asian American community as it becomes integrated into the context of international artistic developments both here and in Asia. The exhibition is not meant to establish any current notion of “Asianness” but rather serve as an introduction to the broad and diverse concerns of artists who together compose a part of the contemporary presence of Asia in America.</p>
-			<h6>Selection panel:</h6>
-			<ul class="list-columns">
-				<li>Zhang Hongtu</li>
-        <li>Kaoru Yanase</li>
-        <li>Zarina Hashmi</li>
-        <li>Junghyang Kim</li>
-        <li>Fay Torres-yap</li>
-			</ul>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Dino Blanche</li>
-        <li>Pao-Tzwu Chang</li>
-        <li>Eunyoug Choi</li>
-        <li>Sung Won Choi</li>
-        <li>Hei Han Khiang</li>
-        <li>Niizeki Hiromi</li>
-        <li>In Sook Hwang</li>
-        <li>Sadashi Inuzuka</li>
-        <li>Akiko Kotani</li>
-        <li>Jun Ho Lee</li>
-			</ul>
+        <h2>9th AAAC Annual: This Used To Be a Poodle</h2>
+        <p><span class="date">October 29 - December 14, 1999</span></p>
+        <p>An exhibition of photography, sculpture, drawing, and installation. The exhibited artists demonstrate innovative approaches by young artists today, displaying a range of thought and expression in the Asian American community as it becomes integrated into the context of international artistic developments both here and in Asia. The exhibition is not meant to establish any current notion of “Asianness” but rather serve as an introduction to the broad and diverse concerns of artists who together compose a part of the contemporary presence of Asia in America. <i>Learn more: <a href="http://artasiamerica.org/documents/1174/524" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2409/524" target="_blank">press release</a>.</i></p>
+        <h6>Selection panelists:</h6>
+        <ul class="list-columns">
+          <li>Zhang Hongtu</li>
+          <li>Kaoru Yanase</li>
+          <li>Zarina Hashmi</li>
+          <li>Junghyang Kim</li>
+          <li>Fay Torres-yap</li>
+        </ul>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Dino Blanche</li>
+          <li>Pao-Tzwu Chang</li>
+          <li>Eunyoug Choi</li>
+          <li>Sung Won Choi</li>
+          <li>Hei Han Khiang</li>
+          <li>Niizeki Hiromi</li>
+          <li>In Sook Hwang</li>
+          <li>Sadashi Inuzuka</li>
+          <li>Akiko Kotani</li>
+          <li>Jun Ho Lee</li>
+        </ul>
 
-      <h2>Looking at Beijing: Photography as Art/CHINA</h2>
-			<p><span class="date">December 16, 1999 - January 6, 2000</span></p>
-      <p>A collaboration with the New York University Department of Art and Art Professions. Summer Study Abroad program in Photography initiated by NYU, co-sponsored by the Beijing Film Institute in Beijing.</p>
-      <p>Curated by Professors Gerald Pryor & Ann Chwatsky.</p>
-      <h6>Photographers:</h6>
-			<ul class="list-columns">
-				<li>Sandra Bermudez</li>
-        <li>Shira Bocar</li>
-        <li>Julia Chiang</li>
-        <li>Nancy Costello</li>
-        <li>Stephan Crasniansky</li>
-        <li>Tracy Gitnick</li>
-        <li>Tracy Hennige</li>
-        <li>Lisa Kaim</li>
-        <li>Karen Marcovici</li>
-        <li>Peter Matra</li>
-        <li>Jennifer Obadia</li>
-        <li>Jessica Rockstein</li>
-        <li>Jasmine Sripa</li>
-        <li>Rebecca Stockham</li>
-        <li>Julian Underwood</li>
-        <li>Bridget Webber</li>
-        <li>Juan Zhang</li>
-    	</ul>
+        <h2>Looking at Beijing: Photography as Art/CHINA</h2>
+        <p><span class="date">December 16, 1999 - January 6, 2000</span></p>
+        <p>A collaboration with the New York University Dept. of Art and Art Professions. Summer Study Abroad program in Photography initiated by NYU, co-sponsored by the Beijing Film Institute in Beijing. Curated by Prof. Gerald Pryor & Ann Chwatsky.</p>
+        <h6>Participating photographers:</h6>
+        <ul class="list-columns">
+          <li>Sandra Bermudez</li>
+          <li>Shira Bocar</li>
+          <li>Julia Chiang</li>
+          <li>Nancy Costello</li>
+          <li>Stephan Crasniansky</li>
+          <li>Tracy Gitnick</li>
+          <li>Tracy Hennige</li>
+          <li>Lisa Kaim</li>
+          <li>Karen Marcovici</li>
+          <li>Peter Matra</li>
+          <li>Jennifer Obadia</li>
+          <li>Jessica Rockstein</li>
+          <li>Jasmine Sripa</li>
+          <li>Rebecca Stockham</li>
+          <li>Julian Underwood</li>
+          <li>Bridget Webber</li>
+          <li>Juan Zhang</li>
+        </ul>
 
-	    <h2>Lunar New Year Exhibition</h2>
-			<p><span class="date">January 20 - March 4, 2000</span></p>
-      <p>A Sand Painting Creation by Lama Tenzin  and  several "Nuo Masks" and Tibetan Thang-ka paintings by Kalsang Wangmo. Lecture by John M. Lundquist -"Mandala & Temple: Sacred Architecture in Tibet."</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Lama Tenzin</li>
-        <li>Kalsang Wangmo</li>
-			</ul>
+        <h2>Lunar New Year Exhibition</h2>
+        <p><span class="date">January 20 - March 4, 2000</span></p>
+        <p>A Sand Painting Creation by Lama Tenzin. Several "Nuo Masks" and Tibetan Thang-ka paintings by Kalsang Wangmo. Lecture by John M. Lundquist titled"Mandala & Temple: Sacred Architecture in Tibet" on February 24.</p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Lama Tenzin</li>
+          <li>Kalsang Wangmo</li>
+        </ul>
 
-      <h2>Apartment #3F</h2>
-			<p><span class="date">March 17 - April 29, 2000</span></p>
-      <p>This exhibition conceptually converts the exhibition gallery space back into its original living environment and in this way, challenge the formalists' notion of viewing art in an empty, pristine "white cube."</p>
-      <p>Curated by Mihee Ahn.</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Jean Shin</li>
-        <li>Niizeki Hiromi</li>
-        <li>Yangah Ham</li>
-			</ul>
+        <h2>Apartment #3F</h2>
+        <p><span class="date">March 17 - April 29, 2000</span></p>
+        <p>An installation/video exhibition. This exhibition conceptually converts the exhibition gallery space back into its original living environment and in this way, challenge the formalists' notion of viewing art in an empty, pristine "white cube." Curated by Mihee Ahn. <i>Learn more: <a href="http://artasiamerica.org/documents/5362/42" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/5363/42" target="_blank">press release</a>.</i></p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Jean Shin</li>
+          <li>Niizeki Hiromi</li>
+          <li>Yangah Ham</li>
+        </ul>
 
-      <h2>Milieu Part III: Color in the Art of </h2>
-			<p><span class="date">May 18 - July 15, 2000</span></p>
-      <p>The third of a three-part exhibition exploring Asian American artists and their social environments from the years 1945 to 1965. Based on the research project begun in 1986 entitled "Asian American Artists and Their Milieu 1945 – 1965.” Curated by Robert Lee.</p>
-      <p>Panel Discussion: Natvar Bhavsar, Corinne Robins, Ninochka Rosca, Patricia Karetzky, co-sponsored by China Institute.</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Natvar Bhavsar</li>
-        <li>Venancio C. Igarta</li>
-        <li>James Kuo</li>
-        <li>Ted Kurahara</li>
-        <li>Seong Moy</li>
-			</ul>
-	</main>
-	<div class="fade"></div>
-	<nav id="mm" role="navigation">
-	<?php include '_inc/menu.php'; ?>
-	</nav>
+        <h2>Milieu Part III: Color in the Art of </h2>
+        <p><span class="date">May 18 - July 15, 2000</span></p>
+        <p>Third in a series of exhibitions, Milieu: Part III features artists who participated in the historical experience of America in the post World War II period (1945-1965). This era in history saw the restriction of Chinese and Korean immigration and the recovery of Japanese Americans from the internment camps. Asian American artists’ works reflect the struggle to respond to these conditions, and for all five artists featured in this installment, color is key. Their work gives form to an art of color where both the sensibility of Asia and the innovative energy of America can be seen. Curated by Robert Lee. Co-sponsored by China Institute. <i>Learn more: <a href="http://artasiamerica.org/documents/3725/48" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/3728/48" target="_blank">press release</a>.</i></p>
+        <h6>Panelists:</h6>
+        <ul class="list-columns">
+          <li>Natvar Bhavsar</li>
+          <li>Corinne Robins</li>
+          <li>Ninochka Rosca</li>
+          <li>Patricia Karetzky</li>
+        </ul>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Natvar Bhavsar</li>
+          <li>Venancio C. Igarta</li>
+          <li>James Kuo</li>
+          <li>Ted Kurahara</li>
+          <li>Seong Moy</li>
+        </ul>
+    </main>
+    <div class="fade"></div>
+    <nav id="mm" role="navigation">
+    <?php include '_inc/menu.php'; ?>
+    </nav>
 </div>
 </body>
 </html>
