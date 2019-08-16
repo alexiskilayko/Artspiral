4fe237768f33606e0aad7fd72e6eae03c1c5108b
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:07:46 -0400

    2002-2003 update

--- 2002-2003.php
+++ 2002-2003.php
@@ -7,95 +7,108 @@ include '_inc/head.php';
 
 <body class="exhibitions">
 <div class="page-wrapper">
-	<?php include '_inc/header.php'; ?>
-	<nav id="menu" role="navigation"> <!--HERE -->
-	<?php include '_inc/menu.php';?>
-	</nav><!--HERE -->
-	<main class="content-wrapper narrow" role="main"><!--HERE -->
-		<span class="tag">2002 &ndash; 2003</span>
-			<h2>Contrary Equilibriums: 12th AAAC Annual Exhibition</h2>
-      <p><span class="date">September 27 – November 8, 2002</span></p>
-      <p>A collaboration with The Korea Society.</p>
-      <p>Eleven artists reflect the diversity of work being done today. Selected artists were chosen from a national field of nearly 150 submissions by the selection panel: Mihee Ahn, Timothy Liu, Margo Machida Tricia Paik, and Lydia Yee</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Seongmin Ahn</li>
-        <li>Susan Choi</li>
-        <li>Phuong M. Do</li>
-        <li>C.J. Lee</li>
-        <li>Eva Lee</li>
-        <li>Asuka Ohsawa</li>
-        <li>Sara Ching-Yu Sun</li>
-        <li>Noelle Tan</li>
-        <li>Mary Ting</li>
-        <li>Kaori Ukaji</li>
-        <li>Katarina Wong</li>
-			</ul>
+    <?php include '_inc/header.php'; ?>
+    <nav id="menu" role="navigation"> <!--HERE -->
+    <?php include '_inc/menu.php';?>
+    </nav><!--HERE -->
+    <main class="content-wrapper narrow" role="main"><!--HERE -->
+      <span class="tag">2002 &ndash; 2003</span>
+        <h2>12th AAAC Annual: Contrary Equilibriums</h2>
+        <p><span class="date">September 27 – November 8, 2002</span></p>
+        <p>Eleven artists who reflect the diversity of work being done today annually. Selected artists were chosen from a national field of nearly 150 submissions. A collaboration with The Korea Society. <i>Learn more: <a href="http://artasiamerica.org/documents/2375/58" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2380/58" target="_blank">press relase</a>.</i></p>
+        <h6>Selection Panelists:</h6>
+        <ul class="list-columns">
+          <li>Mihee Ahn</li>
+          <li>Timothy Liu</li>
+          <li>Margo Machida</li>
+          <li>Tricia Paik</li>
+          <li>Lydia Yee</li>
+        </ul>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Seongmin Ahn</li>
+          <li>Susan Choi</li>
+          <li>Phuong M. Do</li>
+          <li>C.J. Lee</li>
+          <li>Eva Lee</li>
+          <li>Asuka Ohsawa</li>
+          <li>Sara Ching-Yu Sun</li>
+          <li>Noelle Tan</li>
+          <li>Mary Ting</li>
+          <li>Kaori Ukaji</li>
+          <li>Katarina Wong</li>
+        </ul>
 
-			<h2>Not Your Chop Suey Chinatown: Eight Photographers selected by Corky Lee</h2>
-			<p><span class="date">December 6, 2002 – January 13, 2003</span></p>
-			<p>Each artist offers a different point of view that expresses the contemporary world of Chinatown in today's society. After 9/11, this exhibition seeks to show the inner life of Chinatown. Bringing these images into focus may help alter how Chinatown is regarded, cutting through misperceptions.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Chien-Chi Chang</li>
-				<li>Alan Chin</li>
-				<li>Julia Cowling</li>
-				<li>Cheng Hui Hsu</li>
-				<li>Botumroath Keo Lebun</li>
-				<li>Chee Wang Ng</li>
-				<li>Wai Ng</li>
-				<li>Joseph Songco</li>
-			</ul>
+        <h2>Not Your Chop Suey Chinatown: Eight Photographers Selected by Corky Lee</h2>
+        <p><span class="date">December 6, 2002 – January 13, 2003</span></p>
+        <p>Each artist offers a different point of view that expresses the contemporary world of Chinatown in today’s society. After 9/11, this exhibition seeks to show the inner life of Chinatown. Bringing these images into focus may help alter how Chinatown is regarded, cutting through misperceptions and the neglect of too many years. <i>Learn more: <a href="http://artasiamerica.org/documents/6410/177" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/6411/177" target="_blank">press release</a>.</i></p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Chien-Chi Chang</li>
+          <li>Alan Chin</li>
+          <li>Julia Cowling</li>
+          <li>Cheng Hui Hsu</li>
+          <li>Botumroath Keo Lebun</li>
+          <li>Chee Wang Ng</li>
+          <li>Wai Ng</li>
+          <li>Joseph Songco</li>
+        </ul>
 
-      <h2>Below the Canal: After 9/11</h2>
-			<p><span class="date">March 21 – May 2, 2003</span></p>
-      <p>This exhibition is of artists who experienced 9/11 first hand as long time residents in Lower Manhattan and the Chinatown neighborhood. Their work can help to revitalize Lower Manhattan and place 9/11 in a context of local history. The indigenous role and vital meaning of New York’s downtown should not be diminished by large scale undertakings.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-        <li>Grimanesa Amoros</li>
-        <li>Olivia Beens</li>
-        <li>Y.J. Cho</li>
-        <li>Jane Freeman</li>
-        <li>Carter Hodgkin</li>
-        <li>Tishan Hsu</li>
-        <li>A. Lebowski</li>
-        <li>Pamela Lee</li>
-        <li>Choong Sup Lim</li>
-        <li>Karen Margolis</li>
-        <li>John L. Moore</li>
-        <li>Naoto Nakagawa</li>
-        <li>Paolo Pelosini</li>
-        <li>Richard Rudich</li>
-        <li>Paul K Wong</li>
-			</ul>
+        <h2>Below the Canal: After 9/11</h2>
+        <p><span class="date">March 21 – May 2, 2003</span></p>
+        <p>This exhibition is of artists who experienced 9/11 first hand as long time residents in Lower Manhattan and the Chinatown neighborhood. Their work can help to revitalize Lower Manhattan and place 9/11 in a context of local history.  The indigenous role and vital meaning of New York’s downtown should not be diminished by large scale undertakings. A panel talk of artists of Lower Manhattan was held on Tuesday, April 8: Grimanesa Amoros, Jane Freeman, Naoto Nakagawa, Paolo Pelosini, and Richard Rudich. <i>Learn more: <a href="http://artasiamerica.org/documents/6661/201" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/6662/201" target="_blank">press release</a>.</i></p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Grimanesa Amoros</li>
+          <li>Olivia Beens</li>
+          <li>Y.J. Cho</li>
+          <li>Jane Freeman</li>
+          <li>Carter Hodgkin</li>
+          <li>Tishan Hsu</li>
+          <li>A. Lebowski</li>
+          <li>Pamela Lee</li>
+          <li>Choong Sup Lim</li>
+          <li>Karen Margolis</li>
+          <li>John L. Moore</li>
+          <li>Naoto Nakagawa</li>
+          <li>Paolo Pelosini</li>
+          <li>Richard Rudich</li>
+          <li>Paul K Wong</li>
+        </ul>
 
-      <h2>Shaping Wholeness: Asian American Artists Create</h2>
-			<p><span class="date">May 14 – 31, 2003</span></p>
-      <p>Elmhurst Hospital exhibition. Curated by Robert Lee.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-        <li>Seongmin Ahn</li>
-        <li>Trang Bui</li>
-        <li>Dallas Chang</li>
-        <li>Rona Chang	 </li>
-        <li>Iwao Hosoda</li>
-        <li>Marina de Rosario Huang</li>
-        <li>Vandana Jain</li>
-      </ul>
+        <h2>Shaping Wholeness: Asian American Artists Create</h2>
+        <p><span class="date">May 14 – 31, 2003</span></p>
+        <p>To celebrate Asian American Heritage Month, an art exhibition of seven Asian American artists is presented. These artists give shape to wholeness. The wholeness of their being through art is expressed by each artist. This ability is a gift that is the special provenance of the creative disciplines. These are young artists, whose diversity in background, media, and outlook exemplify the Asian American community both in Queens and across the nation. This exhibition is designed to gather artists in and around Elmhurt Hospital to highlight and revitalize the local community. Sponsored by Elmhurst Hospital Center of the Queens Health Network. Curated by Robert Lee.</p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Seongmin Ahn</li>
+          <li>Trang Bui</li>
+          <li>Dallas Chang</li>
+          <li>Rona Chang   </li>
+          <li>Iwao Hosoda</li>
+          <li>Marina de Rosario Huang</li>
+          <li>Vandana Jain</li>
+        </ul>
 
-      <h2>Mid Career: David Diao/Gang Zhao</h2>
-			<p><span class="date">May 16 – June 27, 2003</span></p>
-      <p>Nominating Panelists: Ko'an Jeff Baysa, Emily Cheng, Johnathan Goodman, and Lilly Wei.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>David Diao</li>
-				<li>Gang Zhao</li>
-			</ul>
-	</main><!--HERE -->
-	<div class="fade"></div>
-	<nav id="mm" role="navigation"><!--HERE -->
-	<?php include '_inc/menu.php'; ?>
-	</nav><!--HERE -->
+        <h2>David Diao / Gang Zhao at Mid Career</h2>
+        <p><span class="date">May 16 – June 27, 2003</span></p>
+        <p>Diao and Zhao respond to the great enterprise of modernism and its economic and political innovations, which are reflected in its arts, particularly within the movement’s core tenet of progress. They have joined in a reconsideration of the spirit of modernism/postmodernism, offering their perspective as Asian American artists. <i>Learn more: <a href="http://artasiamerica.org/documents/4692/61" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/4693/61" target="_blank">essay by Robert Lee</a>, <a href="http://artasiamerica.org/documents/4694/61" target="_blank">essay by Alexandra Chang</a>.</p>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>Ko'an Jeff Baysa</li>
+          <li>Emily Cheng</li>
+          <li>Johnathan Goodman</li>
+          <li>Lilly Wei.</li>
+        <h6>Participating artists:</h6>
+        <ul class="list-columns">
+          <li>David Diao</li>
+          <li>Gang Zhao</li>
+        </ul>
+    </main><!--HERE -->
+    <div class="fade"></div>
+    <nav id="mm" role="navigation"><!--HERE -->
+    <?php include '_inc/menu.php'; ?>
+    </nav><!--HERE -->
 </div>
 </body>
 </html>
