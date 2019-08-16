939447dc52673b931e1b7876e9303f5a9d9b4b0c
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:06:48 -0400

    2000-2001 update

--- 2000-2001.php
+++ 2000-2001.php
@@ -7,27 +7,26 @@ include '_inc/head.php';
 
 <body class="exhibitions">
 <div class="page-wrapper">
-	<?php include '_inc/header.php'; ?>
-	<nav id="menu" role="navigation">
-	<?php include '_inc/menu.php';?>
-	</nav>
-	<main class="content-wrapper narrow" role="main">
-		<span class="tag">2000 &ndash; 2001</span>
-			<h2>Too Far Too Close: 10th AAAC Annual Exhibition</h2>
+  <?php include '_inc/header.php'; ?>
+  <nav id="menu" role="navigation">
+  <?php include '_inc/menu.php';?>
+  </nav>
+  <main class="content-wrapper narrow" role="main">
+    <span class="tag">2000 &ndash; 2001</span>
+      <h2>10th AAAC Annual: Too Far Too Close</h2>
       <p><span class="date">September 22 – November 4, 2000</span></p>
-      <p>A collaboration with The Korea Society.</p>
-      <p>This exhibition includes selections from recent entries into the Asian American Artists Archive. This show does not adopt a theme nor seek to establish a current notion of "Asianness."</p>
+      <p>A collaboration with The Korea Society.  This exhibition includes selections from recent entries into the Asian American Artists Archive. This show does not adopt a theme nor seek to establish a current notion of "Asianness." <i>Learn more: <a href="http://artasiamerica.org/documents/2705/70" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2706/70" target="_blank">press release</a>.</i></p>
       <h6>Panelists:</h6>
-			<ul class="list-columns">
-				<li>Hitomi Iwasaki</li>
+      <ul class="list-columns">
+        <li>Hitomi Iwasaki</li>
         <li>Il Lee</li>
         <li>Shazhia Sikander</li>
         <li>Fred Yee</li>
         <li>Lydia Yee</li>
-			</ul>
+      </ul>
       <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Soo-Yeun Ahn</li>
+      <ul class="list-columns">
+        <li>Soo-Yeun Ahn</li>
         <li>Woo Song Bang</li>
         <li>Byung-Wang Cho</li>
         <li>Taro Hattori</li>
@@ -36,53 +35,50 @@ include '_inc/head.php';
         <li>Han Sam Son</li>
         <li>Sookjin Suh</li>
         <li>Jane Tsong</li>
-			</ul>
+      </ul>
 
       <h2>Car Pooling from LA</h2>
-			<p><span class="date">November 17 – December 30, 2000</span></p>
-      <p>A collaboration with The Korea Society.</p>
-      <p>This show consists of four foreign born Asian American Artists. In all the work there is a attempt to make visible the experience of living in cities which are epicenters of colonized spaces. Cities represent achieved human experiences.</p>
-      <p>Organized by Tam Van Tran</p>
+      <p><span class="date">November 17 – December 30, 2000</span></p>
+      <p>A collaboration with The Korea Society. This show consists of four foreign born Asian American Artists. In all the work there is a attempt to make visible the experience of living in cities which are epicenters of colonized spaces. Cities represent achieved human experiences. Organized by Tam Van Tran. <i>Learn more: <a href="http://artasiamerica.org/documents/2809/40" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2810/40" target="_blank">press release</a>.</i></p>
       <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Susan Choi</li>
+      <ul class="list-columns">
+        <li>Susan Choi</li>
         <li>Shirley Tse</li>
         <li>Dean Sameshima</li>
         <li>T. Kim-Trang Tran</li>
         <li>Kyungmi Shin</li>
         <li>Tam Van Tran</li>
-			</ul>
+      </ul>
 
-      <h2>Mid Career: Colin Lee</h2>
-			<p><span class="date">March 16 – April 28, 2001</span></p>
-      <p>The Arts Centre hosts the Mid Career Group annually to select peer artist/s to be featured in this exhibition series. This Mid Career Artist Exhibition features Colin Lee. Panelists were Natvar Bhavsar, Tony Wong, Byron Kim, Reiko Tomii, and Mako Wakasa.</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Colin Lee</li>
+      <h2>Colin Lee at Mid Career</h2>
+      <p><span class="date">March 16 – April 28, 2001</span></p>
+      <p>The Arts Centre hosts the Mid Career Group annually to select peer artist/s to be featured in this exhibition series. This Mid Career Artist Exhibition features Colin Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/4474/199" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/4471/199" target="_blank">press release</a>.</i></p>
+      <h6>Participating artist:</h6>
+      <ul class="list-columns">
+        <li>Colin Lee</li>
       </ul>
 
       <h2>Point Arabesque: Paintings by Charles Yuen</h2>
-			<p><span class="date">May 11 - June 23, 2001</span></p>
-      <p>Curated by Robert Lee.</p>
-      <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Charles Yuen</li>
-			</ul>
+      <p><span class="date">May 11 - June 23, 2001</span></p>
+      <p>Charles Yuen’s paintings are enigmatic, cryptic, odd, whimsical, and dark in an attempt to unsettle our expectations, to subvert and question appearances and identities. In Yuen’s wonderland of inspired nonsense and alternatives, there is nothing that does not have another meaning, another way of seeing the situation, another layer of ambiguity that keeps things open-ended and in perpetual question. Essay by Lilly Wei. Curated by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/1585/25" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/1590/25" target="_blank">press release</a>.</i></p>
+      <h6>Participating artist:</h6>
+      <ul class="list-columns">
+        <li>Charles Yuen</li>
+      </ul>
 
       <h2>Power Print</h2>
-			<p><span class="date">July 13 - Aug 3, 2001</span></p>
-			<p>This woodblock prints exhibition from Nepal reflects a view on modernity from a 3rd World nation that is shaped by the ancient Vedic language whose historic texts are crucial to the earliest beginnings of civilization. For Nepal this remains a living language today. Ram Kumar Panday through his own creative vision, has culled and combined these symbols and applied them to contemporary living.</p>
-      <p>Presented in collaboration with Yin Peet.</p>
+      <p><span class="date">July 13 - Aug 3, 2001</span></p>
+      <p>This woodblock prints exhibition from Nepal reflects a view on modernity from a 3rd World nation that is shaped by the ancient Vedic language whose historic texts are crucial to the earliest beginnings of civilization. For Nepal this remains a living language today. Ram Kumar Panday through his own creative vision, has culled and combined these symbols and applied them to contemporary living. Woodblock prints by Ram Kumar Panday. Presented in collaboration with Ms. Yin Peet.</p>
       <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Ram Kumar Panday</li>
+      <ul class="list-columns">
+        <li>Ram Kumar Panday</li>
         <li>Yin Peet</li>
-			</ul>
-	</main>
-	<div class="fade"></div>
-	<nav id="mm" role="navigation">
-	<?php include '_inc/menu.php'; ?>
-	</nav>
+      </ul>
+  </main>
+  <div class="fade"></div>
+  <nav id="mm" role="navigation">
+  <?php include '_inc/menu.php'; ?>
+  </nav>
 </div>
 </body>
 </html>
