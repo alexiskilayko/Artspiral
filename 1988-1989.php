4763189d20dcabb0b75a151fc556e6af2a425f3a
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:01:02 -0400

    1988-1989 update

--- 1988-1989.php
+++ 1988-1989.php
@@ -15,8 +15,8 @@ include '_inc/head.php';
 		<span class="tag">1988 &ndash; 1989</span>
 			<h2>The 5th Annual Open Studio Show</h2>
 			<p><span class="date">October 1988</span></p>
-			<p>Featured guided tours to six artists' studios in the Chinatown community, and a group show of works by ten artists. </p>
-			<h6>Open studios participants:</h6>
+			<p>Featured guided tours to six artists’ studios in Chinatown community and a group show of works by ten artists. <i><a href="http://artasiamerica.org/documents/2155/23" target="_blank">Learn more</a>.</i></p>
+			<h6>Open studio participants:</h6>
 			<ul class="list-columns">
 				<li>Lee Brozgold</li>
 				<li>Bruce Edelstein</li>
@@ -41,7 +41,7 @@ include '_inc/head.php';
 
 			<h2>The 18 Levels of Hell</h2>
 			<p><span class="date">November 3 – 23, 1988</span></p>
-			<p>An installation by Epoxy Art Group, a group of artists originally from Hong Kong and China now based in New York City. “Epoxy’s joint creative endeavors embody a shared Chinese heritage and cross-cultural influences unique in the United States, but without consciously proclaiming statements about those bi-cultural experiences.”</p>
+			<p>An installation by Epoxy Art Group, a group of artists originally from Hong Kong and China now based in New York City.  “Epoxy’s joint creative endeavors embody a shared Chinese heritage and cross-cultural influences unique in the United States, but without consciously proclaiming statements about those bi-cultural experiences.” </p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Kang Lok Chung </li>
@@ -53,7 +53,7 @@ include '_inc/head.php';
 
 			<h2>Invented Selves: Images of Asian American Identity</h2>
 			<p><span class="date">December 1 – 21, 1988</span></p>
-			<p>Guest curator Margo Machida.</p>
+			<p>An exhibition featuring three young artists who each use self-imaging to explore their Asian American identity. Their works reject stereotypic representations of Asians in popular media yet offer honest, non-idealized commentary on the Asian American community. With guest curator Margo Machida. <i>Learn more: <a href="http://artasiamerica.org/documents/5803/141" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/5802/141" target="_blank">flyer</a>. </i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Ken Chu</li>
@@ -63,20 +63,11 @@ include '_inc/head.php';
 
 			<h2>The Chinese Folk Heritage: Performance, Exhibition, Folk Art Sharing, Talks</h2>
 			<p><span class="date">January 12 - February 16, 1989</span></p>
-			<p>Rocks - "Sculpture of the Gods" from C.C. Wang. Toys from Lily Yeh. Talks on February 14 by Lily Yeh on folk toys of the Yellow River Region, Anne Lee of Leekan Design on collecting.</p>
-			<h6>Folk performers (February 12):</h6>
-			<ul class="list-columns">
-				<li>Kuang Yu Fong</li>
-				<li>Jeffery Yuen</li>
-				<li>Kevin Yao</li>
-				<li>Yu Chung Cheng</li>
-				<li>Lui Sui Ming</li>
-				<li>Jenny Tsui</li>
-			</ul>
+			<p>An exhibition culled from several collectors in New York. Included: rocks, titled “Sculpture of the Gods” by C.C. Wang; toys, puppets, embroidered clothing, and flour figures from the Yellow River Region; original door god paintings, wood figurine, paper cuts, and New Year Prints; utensils, lunch baskets, lacquer ware, and other household items. Folk performance/traditions on February 12 by Kuang Yu Fong, Jeffery Yuen, Kevin Yao, Yu Chung Cheng, Lui Sui Ming, Jenny Tsui. Talks on February 14 by Lily Yeh on folk toys of the Yellow River Region and Anne Lee of Leekan Design on collecting. <i><a href="http://artspiral.blogspot.com/2019/08/the-asian-american-arts-centre-its.html" target="_blank">Learn more</a>.</i></p>
 
 			<h2>Two Emerging Artists</h2>
 			<p><span class="date">March 2 – 22, 1989</span></p>
-			<p>Guest curator John Yau.</p>
+			<p>Guest curated by John Yau, who selected photographer Edith Mesina, who had then recently completed her artist-in-residency at AAAC in 1988, and Cal Lom from New Mexico, who had been constructing complex mechanical structures. </p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Editha Mesina</li>
@@ -85,7 +76,7 @@ include '_inc/head.php';
 
 			<h2>Uptown/DownTown</h2>
 			<p><span class="date">April 6 – May 12, 1989</span></p>
-			<p>An exhibition of Asian American Artists held at the City Gallery, NY Department of Cultural affairs and at the AAAC. Mary Schimdt Campell gave a talk to accompany the exhibition entitled “Welcoming.” Curated by Robert Lee.</p>
+			<p>An exhibition of Asian American Artists held at the City Gallery, NY Department of Cultural affairs and at the AAAC gallery. Mary Schimdt Campell gave a talk to accompany the exhibition entitled “Welcoming.” Curated by Robert Lee. <i><a href="http://artasiamerica.org/documents/1261/176" target="_blank">Learn more</a>. </i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Toshio Sasaki </li>
@@ -101,9 +92,9 @@ include '_inc/head.php';
 				<li>Zhang Hongtu</li>
 			</ul>
 
-			<h2>CHINA: June 4 1989</h2>
-			<p><span class="date">June 9 - September 30, 198</span></p>
-			<p>The initial stage of a traveling exhibition of art responding to the Tiananmen Square event in China, in which students protesting for democracy were massacred. <a href="june4-exhibition-timeline.php">View a complete timeline</a> of this exhibition.</p>
+			<h2>CHINA June 4 1989: First Stage</h2>
+			<p><span class="date">June 9 - September 30, 1989</span></p>
+			<p>A one-year exhibition/event responding to the massacre of the students in Tiananmen Square.  <i><a href="http://www.artspiral.org/june4.php" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Stephen J. Albair</li>
