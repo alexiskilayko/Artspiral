ed065dc636f02d6aae26ef4282f6b90a84906dc8
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:09:45 -0400

    2010-2011 update

--- 2010-2011.php
+++ 2010-2011.php
@@ -13,24 +13,9 @@ include '_inc/head.php';
 	</nav>
 	<main role="main" class="content-wrapper narrow">
 		<span class="tag">2010 &ndash; 2011</span>
-			<h2>8 Artists: From the Archive</h2>
-			<p><span class="date">August 14 – September 11, 2010</span></p>
-			<p>In collaboration with the Artists Alliance Inc., this exhibition was held at the Chuchifritos art gallery and featured artists from the AAAC digital archive.  </p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Sin-ying Ho</li>
-				<li>Nancy Hom</li>
-				<li>Eunjung Hwang</li>
-				<li>Dorothy Imagire</li>
-				<li>Dinh Q. Le</li>
-				<li>Howardina Pindell</li>
-				<li>Roger Shimomura</li>
-				<li>Charles Yuen</li>
-			</ul>
-
-			<h2>ART SLAM 2010</h2>
+			<h2>ARTSLAM 2010</h2>
 			<p><span class="date">October 14, 2010</span></p>
-			<p>ARTSLAM is part of a series of events allowing emerging artists the opportunity to present their work, as well as meet and network with established artists and critics/curators. </p>
+			<p>Showcase of works by emerging Asian-American & Asia influenced artists. Held at 111 Norfolk St.</p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Jing Zhou</li>
@@ -40,7 +25,7 @@ include '_inc/head.php';
 
 			<h2>Infinite Mirror: Images of American Identity</h2>
 			<p><span class="date">January 25 – March 20, 2011</span></p>
-			<p>The Infinite Mirror exhibition, sponsored by Artrain in conjunction with Brandywine & in partnership with International Arts & Artists, began its exhibition tour at Syracuse University on Jan 25, 2011. Infinite Mirror explores America’s image of itself through 65 diverse works of paintings, prints, photographs and video by 40 different artists. Utilizing portraiture and figurative images, the exhibition tells the story of an array of triumphs, tragedies, relationships, and traditions that inform our notions of nationhood. The artwork is personal, reflective, and often autobiographical. The Curators include Blake Bradford, director of education at the Barnes Foundation, Philadelphia; Robert Lee, executive director of the Asian American Arts Centre, New York; and Benito Huerta, associate professor & director of the Gallery at the University of Texas at Arlington. </p>
+			<p>The Infinite Mirror exhibition, sponsored by Artrain in conjunction with Brandywine and in partnership with International Arts & Artists, began its exhibition tour at Syracuse University. Infinite Mirror explores America’s image of itself through 65 diverse works of paintings, prints, photographs and video by 40 artists. Utilizing portraiture and figurative images, the exhibition tells the story of an array of triumphs, tragedies, relationships, and traditions that inform our notions of nationhood. The artwork is personal, reflective, and often autobiographical.  Curators are: Blake Bradford, director of education at the Barnes Foundation, Philadelphia; Robert Lee, executive director of the Asian American Arts Centre, New York; and Benito Huerta, associate professor & director of the Gallery at the University of Texas at Arlington. Asian American artists included are: Tomie Arai, Sung ho Choi, John Yoyogi Fortes, Eunjung Hwang, Indira Johnson, Swati Khurana, Kakyoung Lee, China Marks, Chunwoo Nam, Tara Sabharwal, and Scott Tsuchitani. Infinite Mirror traveling was scheduled through 2015. <i>Learn more: <a href="http://www.artrainusa.org/exhibitions.asp" target="_blank">Artrain</a>, <a href="http://www.brandywineworkshop.com" target="_blank">Brandywine</a>, <a href="http://www.artsandartists.org/exhibitions/infinitemirror.html" target="_blank">Infinite Mirror</a>.</i></p>
 			<h6>Participating Asian American artists:</h6>
 			<ul class="list-columns">
 				<li>Tomie Arai</li>
@@ -58,13 +43,21 @@ include '_inc/head.php';
 
 			<h2>Boom Box</h2>
 			<p><span class="date">March 16 – March 30, 2011</span></p>
-			<p>Curated by Eun Young Choi.</p>
+			<p>An exhibition of two artists from AAAC’s digital archive, artasiamerica.org, and two artists unknown to this archive, chosen by Eun Young Choi. The exhibition explores the visual harmony inspired by nature’s Boom Box, overflowing with rhythms and melodies that combine together to create a complex yet intimate visual symphony. A collaboration with Clemente Soto Vélez Cultural Center. The gallery at 107 Suffolk Street was open seven days a week, 3:30 to 7pm. Curated by Eun Young Choi. <i><a href="http://artspiral.blogspot.com/2011/03/asian-american-arts-centre-presents.html" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Amy Chan</li>
 				<li>Ming Fay</li>
 				<li>Jung Hyang Kim</li>
 				<li>Hyungsub Shin</li>
+			</ul>
+
+			<h2>The Art of Danny Yung</h2>
+			<p><span class="date">April 16, 2011</span></p>
+			<p>AAAC presented Danny Yung, introducing him, his Tian Tian Xian Shang blank boy canvas figure, and his book on Experimental Theatre.  Having been honored with UNESCO’s International Theatre Institute award in 2008, as well as honored by Germany as one of three top students for cultural exchange between Hong Kong and Germany, Danny Yung is widely acknowledged as an art pioneer. He is particularly influential for his work in encouraging the next stage of the evolution of Chinese Opera and the fresh development of Asia’s traditional arts. (Largely unknown - he is the seminal figure in founding Basement Workshop in 1970 in NY Chinatown.) Held at NYC Asian American Student Conference (NYCAASC). <i>Learn more: <a href="http://artspiral.blogspot.com/2017/04/danny-yung-blank-boy-community-event-in.html" target="_blank">blog 1</a>, <a href="http://artspiral.blogspot.com/2014/07/danny-yungs-flower-plaque-installation.html" target="blank">2</a>, <a href="https://www.flickr.com/photos/asianamericanartscentre/4229707066/in/album-72157622978683669/" target="_blank">photograph</a>.</i></p>
+			<h6>Participating artist:</h6>
+			<ul class="list-columns">
+				<li>Danny Yung</li>
 			</ul>
 	</main>
 	<div class="fade"></div>
