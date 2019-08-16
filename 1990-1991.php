d4ea3d3a0f3debdf3a6af3110b02eefacfd4d991
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:01:53 -0400

    1990-1991 update

--- 1990-1991.php
+++ 1990-1991.php
@@ -13,8 +13,9 @@ include '_inc/head.php';
 	</nav>
 	<main role="main" class="content-wrapper narrow">
 		<span class="tag">1990 &ndash; 1991</span>
-			<h2>1st AAAC Annual Exhibition</h2>
-			<p><span class="date">September – November, 1990</span></p>
+			<h2>AAAC Annual</h2>
+			<p><span class="date">September 27 – November 16, 1990</span></p>
+			<p>The first in a series of annual group exhibitions featuring artists’ works selected from the AAAC Artists Archive. This became the opportunity to exhibit several interesting artists who were acculumating in the Archive. Without a theme, artists could be brought together to see if some theme emeged or if one did not. Kip Fulbeck emerged from this show, as did Dean-E Mei, Sui Ying Hung, and Sui Kang Zhao. <i><a href="http://artasiamerica.org/documents/8896/30" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Lalitha Ananthanaman</li>
@@ -37,6 +38,7 @@ include '_inc/head.php';
 
 			<h2>Focus on Artists from Hong Kong</h2>
 			<p><span class="date">November – December 1990</span></p>
+			<p>Featuring Choi Yan Chi, who painted one half of the gallery red, and two young artists—Comyn Mo and Pia Ho—who, as a pair, built a set and performed one night in the other half of the gallery. This was seven years before Hong Kong’s transfer to the mainland took place, when the artists residing in Hong Kong were still largely bypassed there in favor of important artists in the West. </p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Choi Yan Chi</li>
@@ -47,15 +49,15 @@ include '_inc/head.php';
 
 			<h2>Folk Tradition</h2>
 			<p><span class="date">January 17 - February 21, 1991</span></p>
-			<p>AAAC photographic documentation of nine traditional artists in Chinese American community.</p>
+			<p>AAAC Photographic documentation of nine traditional artists in the Chinese American Community. Photographs by Leh Chyun Lin, Editha Mesina, and Gene Moy. Festival performances hosted by Kuang Yu Fong. <i><a href="http://www.artspiral.org/uncle-ng.php" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
-				<li>Richard Chen<li>
+				<li>Richard Chen</li>
 				<li>Lechuan Hwa</li>
 				<li>Shang Chi Eng</li>
 				<li>Bo Lian Huang</li>
 				<li>Liang Xing Tang</li>
-				<li>Yun Shu Xu<li>
+				<li>Yun Shu Xu</li>
 				<li>Kevin Yao</li>
 				<li>Jeffrey Yuen</li>
 				<li>Mei Wang</li>
@@ -71,21 +73,13 @@ include '_inc/head.php';
 				<li>Richard Chen - Chinese origami</li>
 				<li>Lechuan Hwa - embroidery</li>
 				<li>Yun Shu Xu - New Years mottoes</li>
-				<li>Bo Lian Huang - dough figures<li>
+				<li>Bo Lian Huang - dough figures</li>
 				<li>Liang Xing Tang - Pi Pa music</li>
-			</ul>
-
-			<h2>Tai Dang</h2>
-			<p><span class="date">April 26 - 27, 1991</span></p>
-			<p>Performance at Henry St. Settlement sponsored by AAAC.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Tai Dang<li>
 			</ul>
 
 			<h2>Violence of Victory</h2>
 			<p><span class="date">April 18 – May 16, 1991</span></p>
-			<p>Curated by Robert Lee.</p>
+			<p>An exhibition focusing on particular Asian groups’ experiences of violence in contemporary life as a result of war and aggression. The works also speak on the continuous presence of violence in our everyday life—a presence we take for granted. The title suggests that in winning the undercurrent of violence may leave its markings. The three featured photographers, in expressing their attitudes to violence, rely on contemporarary and historic experience significant to Asians in America. <i>Learn more: <a href="http://artasiamerica.org/documents/2069/143" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2070/143" target="_blank">press release</a>. </i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Vinod Dave</li>
@@ -94,21 +88,21 @@ include '_inc/head.php';
 				<li>Vivian Mak</li>
 			</ul>
 
-			<h2>The Art of Asian American Artists</h2>
-			<p><span class="date">May 12, 1991</span></p>
-			<p>A slide lecture by Robert Lee. Held at the National Museum of American Art in Washington, D.C.</p>
-
-			<h2>Okuhara Tetsuaki</h2>
-			<p><span class="date">June 4 - June 24, 1990</span></p>
-			<p>A one-person photography show featuring the Japanese-American artist Tetsuaki Okuhara. The exhibition features 14 photography assemblage of black and white photos, primarily works made in the year of and a range of various sizes.</p>
+			<h2>Come Let's Go View the Snow Until We're Buried</h2>
+			<p><span class="date">April 26 - 27, 1991</span></p>
+			<p>Tai Dang’s art is informed by his experiences as a Vietnamese refugee. This particular performance is inspired by a Japanese haiku and his desire to explore balance—balance between the spiritual and the physical, the East and the West, oneself and the material world. Directed by Andres Serrano. Sponsored by AAAC. Performance at Henry St. Settlement. <i>Learn more: <a href="http://artasiamerica.org/documents/6458/142" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/6456/142" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/6457/142" target="_blank">flyer</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
-				<li>Tetsu Okuhara</li>
+				<li>Tai Dang</li>
 			</ul>
 
-			<h2>...wa(IT) WA(i)T(in)G... part II: Lessons of History &/or Forgetting Amnesia</h2>
-			<p><span class="date">January - March 1991</span></p>
-			<p>Performance by Stan Nishimura. A presentation of D'Asia Vu by Asian American Dance Theatre (AADT).</p>
+			<h2>The Art of Asian American Artists</h2>
+			<p><span class="date">May 12, 1991</span></p>
+			<p>A slide lecture by R. Lee. Held at the National Museum of American Art, Washington, DC. The first presentation of its kind where an overview of AAAC Artists Archive sorted hundreds of artists into various types of artwork from public art to humor, from spirituality to feminism.</p>
+
+			<h2>…wa(IT) WA(i)T(in)G…part II: LESSONS of History &/or forgetting AMNESIA</h2>
+			<p><span class="date">June 28 - 29, 1991</span></p>
+			<p>Performance by Stan Nishimura. A presentation of D'Asia Vu, Asian American Dance Theatre (AADT).</p>
 			<h6>Performers:</h6>
 			<ul class="list-columns">
 				<li>Stan Nishimura - trombone</li>
@@ -119,6 +113,14 @@ include '_inc/head.php';
 				<li>Rob Galinsky - vocals</li>
 				<li>George Gramelis - vocals</li>
 				<li>July Rose - vocals</li>
+			</ul>
+
+			<h2>Okuhara Tetsuaki</h2>
+			<p><span class="date">June 4 - June 24, 1990</span></p>
+			<p>A one-person photography show featuring the Japanese-American artist Tetsuaki Okuhara. The exhibition features 14 photography assemblage of black and white photos, primarily works made in the year of and a range of various sizes. <i>Learn more: <a href="http://artasiamerica.org/documents/2399/83" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/2403/83" target="_blank">proposal</a>.</i> </p>
+			<h6>Participating artist:</h6>
+			<ul class="list-columns">
+				<li>Tetsu Okuhara</li>
 			</ul>
 	</main>
 	<div class="fade"></div>
