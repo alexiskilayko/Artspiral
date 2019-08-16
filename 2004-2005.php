efe5bb21fc219d017ae7a165121632a9d749f344
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:08:20 -0400

    2004-2005 update

--- 2004-2005.php
+++ 2004-2005.php
@@ -13,9 +13,17 @@ include '_inc/head.php';
 	</nav>
 	<main role="navigation" class="content-wrapper narrow">
 		<span class="tag">2005 &ndash; 2006</span>
-			<h2>Butternut Ink: 14th AAAC Annual Exhibition</h2>
+			<h2>14th AAAC Annual: Butternut Ink</h2>
 			<p><span class="date">September 24 – November 25, 2004</span></p>
-			<p>An exhibition of eleven artists from diverse backgrounds. Butternuts are a natural ingredient specific to the state of Vermont that the artist James Jack uses to create ink for his Eastern calligraphy influenced work. Just as Jack’s butternut ink represents an infusion of cultures, the works exhibited in Butternut Ink represent the reciprocal exchanges and influences of Asian and Western aesthetic values and how these influences affect a wide variety of subjects, ranging from everyday life to politics. Held in collaboration with the Korea Society.</p>
+			<p>Collaboration with The Korea Society. An exhibition of eleven artists from diverse backgrounds. Butternuts are a natural ingredient specific to the state of Vermont that the artist James Jack uses to create ink for his Eastern calligraphy influenced work. Just as Jack’s butternut ink represents an infusion of cultures, the works exhibited in Butternut Ink represent the reciprocal exchanges and influences of Asian and Western aesthetic values and how these influences affect a wide variety of subjects, ranging from everyday life to politics. <i>Learn more: <a href="http://artasiamerica.org/documents/4335/164" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/4336/164" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/6183/164" target="_blank">review</a>.</i></p>
+			<h6>Selection panelists:</h6>
+			<ul class="list-columns">
+				<li>Koan-Jeff Baysa</li>
+				<li>Joan Kee</li>
+				<li>Christopher Ho</li>
+				<li>Thuy Linh Nguyen Tu</li>
+				<li>Jean Shin</li>
+			</ul>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Rutherford Chang</li>
@@ -30,18 +38,10 @@ include '_inc/head.php';
 				<li>Jaye Rhee</li>
 				<li>Rashmi Talpade</li>
 			</ul>
-			<h6>Selection panelists:</h6>
-			<ul class="list-columns">
-				<li>Koan-Jeff Baysa</li>
-				<li>Joan Kee</li>
-				<li>Christopher Ho</li>
-				<li>Thuy Linh Nguyen Tu</li>
-				<li>Jean Shin</li>
-			</ul>
 
 			<h2>The Topography of Absence</h2>
 			<p><span class="date">November 19 – December 31, 2004</span></p>
-			<p>Curated by Katarina Wong, this exhibition offers different ways artists delve into the landscape of their own experiences in an attempt to understand and grasp absence as fullness. Through work that offers viewers a series of quiet meditations of overlooked actions, impulses and detritus, the empty trivial mundane moments of life are given an unexpected wonder. By examining the topography of their lives, artists find overlooked landscapes, not unlike the visual voyage through a Chinese landscape painting, providing an opportunity for us to re-assess what is truly important.</p>
+			<p>This exhibition offers different ways artists delve into the landscape of their own experiences in an attempt to understand and grasp absence as fullness. Through work that offers viewers a series of quiet “meditations” of overlooked actions, impulses, and detritus, the empty trivial mundane moments of life are given an unexpected wonder. By examining the topography of their lives, artists find overlooked landscapes, not unlike the visual voyage through a Chinese landscape painting, providing an opportunity for us to re-assess what is truly important. Guest curated by Katarina Wong. <i>Learn more: <a href="http://artasiamerica.org/documents/5693/116" target="_blank">flyer</a>, <a href="http://artasiamerica.org/artist/detail/116" target="_blank">artist profile</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Amy Kao</li>
@@ -51,11 +51,9 @@ include '_inc/head.php';
 				<li>Lisa Young</li>
 			</ul>
 
-			<h2>為盼 New York Eviction Blues</h2>
+			<h2>New York Eviction Blues</h2>
 			<p><span class="date">March 31 – March 10, 2006</span></p>
-			<p>Asian American Arts Centre (AAAC) fights eviction from its home of 28 years. It featured a select group of artists who have supported AAAC over many years. Many have a statement/story about their struggle with space, landlords, city regulations and real estate. The history of this urban struggle is reviewed, beginning in the 60s, to the passage of the Loft Law in 1982, to the current recognition among planners and government agencies of the central role the arts play in neighborhood revitalization. </p>
-			<p>A Panel Talk was held on April 13th featuring Arlene Raven the art critic, Rebecca Goyette of First St. Studios, and Cathy Nanda of chashama.</p>
-			<p>Another exhibition space - ABC No Rio 為V mounted a sister show 為盼 in solidarity 為洫 from April 21 - May 12, 2005</p>
+			<p>Asian American Arts Centre (AAAC) fights eviction from its home of 28 years. This exhibition featured a select group of artists who have supported AAAC over many years. Many have a statement/story about their struggle with space, landlords, city regulations and real estate. The history of this urban struggle is reviewed, beginning in the 60s, to the passage of the Loft Law in 1982, to the current recognition among planners and government agencies of the central role the arts play in neighborhood revitalization. A panel talk was held on April 13th by Arlene Raven, art critic; Rebecca Goyette of First St. Studios; and Cathy Nanda of chashama. Another exhibition space, ABC No Rio, mounted “Eviction Blues,” an exhibition in solidarity with AAAC from April 21 to May 12, 2005. <i>Learn more: <a href="http://artasiamerica.org/documents/1488/201" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/1510/201" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>ON/Megumi Akiyoshi</li>
@@ -110,17 +108,25 @@ include '_inc/head.php';
 				<li>Lianjie Zheng</li>
 			</ul>
 
-			<h2>Mid Career: Mei-ling Hom / Zhang Jian-jun</h2>
+			<h2>Mei-ling Hom / Zhang Jian-jun at Mid Career</h2>
 			<p><span class="date">May 27 – July 1, 2005</span></p>
-			<p>Mid-Career Nominating Panelists: Reiko Tomii, Lilly Wei, Johnathan Goodman, Judy Collischan, and Tomie Arai.</p>
+			<p>Both Jian-Jun Zhang and Mei-ling Hom have chosen “culture” as a subject of their work and approached it from a personal perspective. This theme of culture defies concreteness and, like art, exists through space and time. It is this state that Mei-ling and Jian-Jun have come to investigate through their distinctive background experiences and unique philosophical beliefs. This exhibition then provides two apprehensions of art and culture that are distinct visually, yet alike in their basis upon the realization that art and culture are subject to constant fluctuation. Using different technical and material means, these artists provoke viewers to view inevitable change (rather than permanence) as the quality of existence. Essay by Judy Collischan, Ph.D. <i>Learn more: <a href="http://artasiamerica.org/documents/1782/127" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/1789/127" target="_blank">press release</a>.</i></p>
+			<h6>Nominating panelists:</h6>
+			<ul class="list-columns">
+				<li>Reiko Tomii</li>
+				<li>Lilly Wei</li>
+				<li>Johnathan Goodman</li>
+				<li>Judy Collischan</li>
+				<li>Tomie Arai</li>
+			</ul>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Mei-ling Hom</li>
 				<li>Zhang Jian-jun</li>
 			</ul>
 
-			<h2>ARTSLAMS</h2>
-			<p>Each evening five artists presented their work and had an interactive exchange of ideas with the audience. This is an opportunity for Asian American artists and artists influenced by Asia to share their work with peers in an open forum for critical exchange. Curators Reiko Tomii and Midori Yoshimoto were often present, as was artist Emily Cheng. Artist/university art instructor Bovey Lee also joined the discussion.</p>
+			<h2>ARTSLAMS 2005</h2>
+			<p>Each evening five artists presented their work and had an interactive exchange of ideas with the audience. This is an opportunity for Asian American artists and artists influenced by Asia to share their work with peers in an open forum for critical exchange. Curators Reiko Tomii and Midori Yoshimoto were often present, as was artist Emily Cheng. Artist/university art instructor Bovey Lee also joined the discussion. <i><a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157621899520607" target="_blank">Learn more</a>.</i></p>
 			<p><span class="date">June 16, 2005</span></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
@@ -147,6 +153,26 @@ include '_inc/head.php';
 				<li>Kiriko Shirobayashi</li>
 				<li>La Thoriel Badenhausen</li>
 				<li>Deanna Lee</li>
+			</ul>
+
+			<h2>Organized Spaces</h2>
+			<p><span class="date">August 26 – September 9, 2005</span></p>
+			<p>A special prelude to AAAC’s  05-06 exhibition season. These New York-based artists come from Singapore, one of the smallest countries in the world, an island-nation establishing its place in today's world, a culture balancing Eastern and Western values, and a generation attracted to the old and the new. Curated by Meridith McNeal. Sponsored by the Permanent Mission of Singapore to the United Nations as it marks its 40th year of independence.</p>
+			<h6>Participating artists:</h6>
+			<ul class="list-columns">
+				<li>Colin Goh</li>
+				<li>Jon Kher Kaw</li>
+				<li>Geraldine Lau</li>
+				<li>Alan Oei</li>
+				<li>Joel Seah</li>
+				<li>Hong-Ling Wee</li>
+				<li>Jason Wee</li>
+				<li>Su-en Wong</li>
+				<li>Yen Yen Woo</li>
+				<li>H. Koon Wee</li>
+				<li>Eunice Seng</li>
+				<li>I-Shin Chow</li>
+				<li>Steve Chen</li>
 			</ul>
 	</main>
 	<div class="fade"></div>
