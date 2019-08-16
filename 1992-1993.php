685ed225939005851e81b07724c9755243d83fe3
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:02:43 -0400

    1992-1993 update

--- 1992-1993.php
+++ 1992-1993.php
@@ -7,163 +7,98 @@ include '_inc/head.php';
 
 <body class="exhibitions">
 <div class="page-wrapper">
-	<?php include '_inc/header.php'; ?>
-	<nav id="menu" role="navigation">
-	<?php include '_inc/menu.php';?>
-	</nav>
-	<main class="content-wrapper narrow" role="main">
-		<span class="tag">1992 &ndash; 1993</span>
-			<h2>CHINA: June 4 1989</h2>
-			<p><span class="date">June 4 – July 14, 1992</span> at Mexic-Arte in Austin, Texas</p>
-			<p><span class="date">August 24, 1992 - October 4, 1992</span> at the Cleveland Institute of Art in Cleveland, Ohio</p>
-			<p>The continuation of a traveling exhibition of art responding to the Tiananmen Square event in China, in which students protesting for democracy were massacred.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Aprile, Thomas R.</li>
-				<li>Albair, Stephen J.</li>
-				<li>Allen, John Amoros, Grimanesa </li>
-				<li>Azaceta, Luis Cruz </li>
-				<li>Bahc, Mo </li>
-				<li>Baron, Ronald </li>
-				<li>Beaumont, Betty </li>
-				<li>Bowman, Daniel </li>
-				<li>Boza, Juan </li>
-				<li>Briere, Manon </li>
-				<li>Byler, James Jay</li>
-				<li>Camnitzer, Luis </li>
-				<li>Chin, Mel & Concoran School of Art </li>
-				<li>Chu, Julia Nee/Ca</li>
-				<li>Clark, Scott Klee </li>
-				<li>Colin, Muriel </li>
-				<li>Cortes, Esperanz </li>
-				<li>Decreeft, Donna Maria </li>
-				<li>Denes, Agnes </li>
-				<li>Diao, David </li>
-				<li>Dimon, Roz </li>
-				<li>Do, Lotus </li>
-				<li>Duff, John </li>
-				<li>Fowler, Paul M.	Fu, Woody </li>
-				<li>Golub, Leon </li>
-				<li>Gonzalez, Maria Elena </li>
-				<li>Heap of Birds, Edgar </li>
-				<li>Hill, Robin Hoop </li>
-				<li>Hughes, Holly </li>
-				<li>Hung, Kuan - Chak </li>
-				<li>Hung, Ying S. </li>
-				<li>Ishino, Cathy </li>
-				<li>Izuka, Kunio </li>
-				<li>Kang, Ik-Joong </li>
-				<li>Kim, Byron </li>
-				<li>Koorland, Vivienne </li>
-				<li>Kruger, Barbara </li>
-				<li>Kuo, Anna </li>
-				<li>Laughin, Ian </li>
-				<li>Lascari, Anna </li>
-				<li>Lee, Bing </li>
-				<li>Lee, Gerald Ling Ling (Billy Harlem) </li>
-				<li>Lipski, Donald </li>
-				<li>Lum, Mary </li>
-				<li>Mei, Dean-I </li>
-				<li>Morr, Erik </li>
-				<li>Msika, Jean</li>
-				<li>Mur-Ray, Ming</li>
-				<li>Nabeska, Eva </li>
-				<li>Nemec, Vernita </li>
-				<li>Oshima, Mari </li>
-				<li>Palaia, Franc </li>
-				<li>Parcher, Joyce </li>
-				<li>Porter, Liliana </li>
-				<li>Rodrigo, Pascal </li>
-				<li>Red Spot (J. Allen Daugherty) </li>
-				<li>Ruhren, Andrew </li>
-				<li>Schimdt, James T. </li>
-				<li>Shih, Nora Ling-Yun </li>
-				<li>Shoemaker, Pamela </li>
-				<li>Sivilli, Valerie </li>
-				<li>Tacla, Jorge </li>
-				<li>Tsuda, Rumiko & Georges, Daniel </li>
-				<li>Hei Han Khiang</li>
-				<li>Watkins, Tim </li>
-				<li>White, Peter </li>
-				<li>Wong, Martin </li>
-				<li>Young, Laura </li>
-				<li>Yu, Sun Ching </li>
-				<li>Zezmer, Sofia </li>
-				<li>Zhang, Hongtu</li>
-			</ul>
+    <?php include '_inc/header.php'; ?>
+    <nav id="menu" role="navigation">
+    <?php include '_inc/menu.php';?>
+    </nav>
+    <main class="content-wrapper narrow" role="main">
+        <span class="tag">1992 &ndash; 1993</span>
+            <h2>And He Was Looking for Asia: Alternatives to the Story of Christopher Columbus Today</h2>
+            <p><span class="date">September 25 – October 29, 1992</span></p>
+            <p>An exhibition exploring the Migration Period, a neglected era in European history.</p>
+            <p>A story that has haunted me for years, is the link between Asia and the West.  But it did not begin to come together for me until I learned about the art of the so called 'Migration Peoples'.  After so many years, fragments remain hopelessly scattered over vast stretches of history and language. Without the artifacts to meticulously piece these together, how can I relate to you a story so fugitive?   But I dare not delay further, to leave in the hands of professional historians, a tale that has been ignored and denied in the West for so long.</p>
+            <p>Briefly: The master narrative of Western history, of European history is missing a major component - the Migration Peoples who settled through much of Europe, has remained central to its story. Who were these diverse nomadic peoples….?</p>
+            <p>In the so called Dark Ages - what historian Michael Wood on PBS describes as the "barbarian becomes the Roman, forming the 'Barbarian West'".  Goths,  Visigoths, Merovingians, Avars, Huns, migration peoples of the 2ed to the 6th Century whose oral traditions and animism were suppressed by the literary traditions and religion of the Mediterranian.  Venerable Bead in the 8th C. taught that the peoples of the Barbarian West were chosen Christian heirs of the Roman Empire destined to lead humanity to the City of God.</p>
+            <p>The art of polychrome fibula pins, the only remains today of the original traditions of the migration peoples. Picasso's bull is identical to migration peoples art. “Christopher Columbus wore a round fibula pin with studded semi precious stones worked with interlacery”, ...perhaps, yes say perhaps... </p>
+            <p>In looking for Asia, the myths and perceptions of Asia were foremost: the Tree of Life motif, the fountain of youth, El Dorado, Hawaii’s paradise, Hitler's Valhalla, temple of racial purity.   Midst all this how did East-West become polar opposites? if in deep history there was contact and interest in Asia then there are cultural ties we live with yet to be discovered, their implications yet to be unraveled.</p>
+            <p>“To fight the good fight is all that ennobles us, and gives us finally our triumph over this mortal coil.”  How often have you heard Capt. Kirk or Shakespeare say this?  Are not the ethics and traditions of battle in every moment of our Western lives, to push us beyond our limits.  A key image of our nomadic heritage:  the open road.  As the first former President Bush once termed it, “We’re not done yet!”.</p>
+            <p>Vaclav Havel, Feb. 4, 1992 in the NYT:  "The fall of Communism can be regarded as a sign that modern thought - based on the premise that the world is objectively knowable, and that the knowledge so obtained can be absolutely generalized - has come to a final crisis. We are looking for an objective way out of the crisis of objectivity.... Things must once more be given a chance to present themselves as they are, to be perceived in their individuality.  We must see the pluralism of the world, and not bind it by seeking common denominators or reducing everything to a single common equation.  Soul, individual spirituality, first-hand personal insight into things; the courage to be oneself and go the way one's conscience points, humility in the face of the mysterious Order of Being, confidence in its natural direction and, above all, trust in one's own subjectivity as the principal link with the subjectivity of the world - these are the qualities that ...the future should cultivate."</p>
+            <p>“I suddenly realized that the molecules in my body, and the molecules in the spacecraft and in my partners bodies were manufactured in … the furnaces of stars. Suddenly those were my molecules, not intellectually but viscerally.” (He points to his heart. Samadhi).  <a href="https://ultraculture.org/blog/2014/06/02/astronauts-spiritual-experiences/" target="_blank">The late Edgar Mitchell, astronaut.</a></p>
+            <p>– Bob Lee, Curator. <i>Learn more: <a href="http://artasiamerica.org/documents/5721/67" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/5718/67" target="_blank">postcard</a>, <a href="http://artasiamerica.org/works/1138/67" target="_blank">artwork</a>.</i></p>
+            <img src="/_images/JorgeTacla_LookingForAsia.jpg" alt="Jorge Tacla's painting, Study on One Transformation">
+            <h6>Participating artists:</h6>
+            <ul class="list-columns">
+                <li>Mo Bahc</li>
+                <li>Willie Cole</li>
+                <li>Joanna Osburn</li>
+                <li>Big Feather </li>
+                <li>Arlan Huang</li>
+                <li>Young K.    </li>
+                <li>Betty Lee</li>
+                <li>Jorge Tacla</li>
+                <li>Barbara Takenaga</li>
+            </ul>
 
-			<h2>And He Was Looking for Asia: Alternatives to the Story of Christopher Columbus Today</h2>
-			<p><span class="date">September 25 – October 29, 1992</span></p>
-			<p>An exhibition exploring the Migration Period, a neglected era in European history.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Mo Bahc</li>
-				<li>Willie Cole</li>
-				<li>Joanna Osburn</li>
-				<li>Big Feather	</li>
-				<li>Arlan Huang</li>
-				<li>Young K.	</li>
-				<li>Betty Lee</li>
-				<li>Jorge Tacla</li>
-				<li>Barbara Takenaga</li>
-			</ul>
+            <h2>3rd AAAC Annual: My Eyes Blur Sometimes, at Beauty</h2>
+            <p><span class="date">November 20 – December 30, 1992</span></p>
+            <p>An exhibition showing women artists selected from AAAC’s archive. Curated by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/3350/95" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/3351/95" target="_blank">press release</a>.</i> </p>
+            <h6>Participating artists:</h6>
+            <ul class="list-columns">
+                <li>Ela Shah </li>
+                <li>Gaye Chan </li>
+                <li>Hee-Sook Kim</li>
+                <li>Hyun-Mi Yoo</li>
+                <li>Jacqueline Chang</li>
+                <li>May-Ling Chang </li>
+                <li>Sungmi Naylor   </li>
+                <li>Yoshiko Shimada</li>
+                <li>Ava Hsueh </li>
+                <li>Sowon Kwon </li>
+                <li>Ellen Pau</li>
+            </ul>
 
-			<h2>My Eyes Blur Sometimes, at Beauty: 3rd AAAC Annual Exhibition</h2>
-			<p><span class="date">November 20 – December 30, 1992</span></p>
-			<p>An exhibition showing women artists selected from AAAC’s archive. Curated by Robert Lee.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Ela Shah </li>
-				<li>Gaye Chan </li>
-				<li>Hee-Sook Kim</li>
-				<li>Hyun-Mi Yoo</li>
-				<li>Jacqueline Chang</li>
-				<li>May-Ling Chang </li>
-				<li>Sungmi Naylor	</li>
-				<li>Yoshiko Shimada</li>
-				<li>Ava Hsueh </li>
-				<li>Sowon Kwon </li>
-				<li>Ellen Pau</li>
-			</ul>
+            <h2>Year of the Rooster: A People’s Folk Art Exhibition</h2>
+            <p><span class="date">January 15 – February 20, 1993</span></p>
+            <p>Selections from Prof Liu of papercuts, woodblock prints, folk paintings with Lu Min-quin dough figurines that prizes living vitality.  Sculptural energetic cuttings from Shaanxi & Hebei master grandmothers. Woodblocks from Hebei, Sichuan, Hunan, & Shandong. Together with bilingual notes, myths, & stories. Festival with calligraphy demonstrations, fortune telling, dough figurines, paper folding, Peking Opera face painting, & Chinese chess game demonstration.  Marking the major role of women in Chinese folk traditions.  Lectures by Qian Liu on January 26 and February 16, “Repelling Demons: the Tradition & the Masks,” reviewing his recent original field research. Festival Performance on January 24. Uncle Ng sang on February 21.</p>
 
-			<h2>Yin Peet: A Woman's Life Choices</h2>
-			<p><span class="date">March 12 – April 16, 1993</span></p>
-			<p>An exhibition of seal carvings, sculptures, and installations by Yin Peet through which she re-visits her past life and relates it to the life’s choices she has made as an Asian woman living in American society today. Her seal carvings reflect the idea that seals are a recognition of the importance of our existence in society since ancient times; her sculptures combine elements of Western antiquity with modern images of ripe fruit; her installation of a life chess board tells of the current state of her life using strategically positioned chess pieces. Curated by Robert Lee.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Yin Peet</li>
-			</ul>
+            <h2>Yin Peet</h2>
+            <p><span class="date">March 12 – April 16, 1993</span></p>
+            <p>An exhibition of seal carvings, sculptures, and installations by Yin Peet through which she re-visits her past life and relates it to the life’s choices she has made as an Asian woman living in American society today. Her seal carvings reflect the idea that seals are a recognition of the importance of our existence in society since ancient times; her sculptures combine elements of Western antiquity with modern images of ripe fruit; her installation of a life chess board tells of the current state of her life using strategically positioned chess pieces. Curated by Robert Lee. <i><a href="http://artasiamerica.org/documents/3043/29" target="_blank">Learn more</a>.</i> </p>
+            <h6>Participating artists:</h6>
+            <ul class="list-columns">
+                <li>Yin Peet</li>
+            </ul>
 
-			<h2>We Count! The State of Asian Pacific America</h2>
-			<p><span class="date">May 10 – May 31, 1993</span></p>
-			<p>Extensive documentation accompanied the exhibition on issues gathered by LEAP and amplified by Lorinda Chen with information on NYC. Portions were published in Artspiral. Curated by Robert Lee. Held at Court House in New York, NY.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Rashid Arshed</li>
-				<li>Kip Fulbeck</li>
-				<li>Kitty Katz</li>
-				<li>Yeong Gill Kim </li>
-				<li>Marcelo Krasilcic</li>
-				<li>Vijay Kumar</li>
-				<li>Dinh Le</li>
-				<li>Sang Kyoon Noh</li>
-				<li>Alfonso Ossorio</li>
-				<li>Yin Peet</li>
-				<li>Rudjen Roldan</li>
-				<li>Halide Salam</li>
-				<li>Roger Shimomura</li>
-				<li>Sikay Tang</li>
-				<li>Kwong Chi Tseng</li>
-				<li>Toyo Tsuchiya</li>
-				<li>Dolly Unithan</li>
-				<li>Charles Yuen</li>
-				<li>Zarina</li>
-			</ul>
-	</main>
-	<div class="fade"></div>
-	<nav id="mm" role="navigation">
-	<?php include '_inc/menu.php'; ?>
-	</nav>
+            <h2>We Count! The State of Asian Pacific America</h2>
+            <p><span class="date">May 10 – May 31, 1993</span></p>
+            <p>Extensive documentation accompanied the exhibition on issues gathered by LEAP and amplified by Lorinda Chen with information on NYC. Portions were published in Artspiral. Curated by Robert Lee. Held at Court House, adjacent City Hall, NYC. <i>Learn more: <a href="http://artasiamerica.org/documents/1105/71" target="_blank">brochure</a>, <a href="http://artasiamerica.org/documents/1340/71" target="_blank">press release</a>.</i></p>
+            <h6>Participating artists:</h6>
+            <ul class="list-columns">
+                <li>Rashid Arshed</li>
+                <li>Kip Fulbeck</li>
+                <li>Kitty Katz</li>
+                <li>Yeong Gill Kim </li>
+                <li>Marcelo Krasilcic</li>
+                <li>Vijay Kumar</li>
+                <li>Dinh Le</li>
+                <li>Sang Kyoon Noh</li>
+                <li>Alfonso Ossorio</li>
+                <li>Yin Peet</li>
+                <li>Rudjen Roldan</li>
+                <li>Halide Salam</li>
+                <li>Roger Shimomura</li>
+                <li>Sikay Tang</li>
+                <li>Kwong Chi Tseng</li>
+                <li>Toyo Tsuchiya</li>
+                <li>Dolly Unithan</li>
+                <li>Charles Yuen</li>
+                <li>Zarina</li>
+            </ul>
+    </main>
+    <div class="fade"></div>
+    <nav id="mm" role="navigation">
+    <?php include '_inc/menu.php'; ?>
+    </nav>
 </div>
 </body>
 </html>
