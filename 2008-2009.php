9d937acdf6f259cdd079f3e3d13210408f871e61
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:09:16 -0400

    2008-2009 update

--- 2008-2009.php
+++ 2008-2009.php
@@ -13,9 +13,9 @@ include '_inc/head.php';
 	</nav>
 	<main role="main" class="content-wrapper narrow">
 		<span class="tag">2008 &ndash; 2009</span>
-			<h2>Metro Poles in China Town</h2>
+			<h2>Metro Poles in Chinatown</h2>
 			<p><span class="date">October 2008 – January 2009</span></p>
-			<p>The Asian American Arts Centre partnered with the Charles B. Wang Community Health Center, the Chinatown Manpower Project and other local institutions in a cross-borough collaboration occuring simultaneously in Jamaica, the Bronx and Lower Manhattan. </p>
+			<p>A Collaboration with Jamaica Center for Arts and Learning and Bronx River Arts Center, MetroPoles is curated by Jose Ruiz, Heng Gil, and Bob Lee. Opened and closed in various community locations— At the Charles B. Wang Community Health Center from Oct 22 till Jan 10, 2009. At Chinatown Manpower Project from Oct 14 till Dec 5 th and at the Chatham Green Cooperative, Nov 1 to Dec 13. The sites are:  Chinatown Manpower Project at 70 Mulberry St 3fl., and the Charles B. Wang Community Health Centre on Canal St. at 268 Canal St, and an outdoor site - the Chatham Green Cooperatives. <i>Learn more: <a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157616235427494" target="_blank">photo album</a>, <a href="http://artasiamerica.org/documents/8625" target="_blank">flyer</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Katarina Wong</li>
@@ -31,9 +31,16 @@ include '_inc/head.php';
 				<li>Avani Patel</li>
 			</ul>
 
-			<h2>Clusterbomb!</h2>
+			<h2>Teen Map Project</h2>
+			<p><span class="date">November 2008</span></p>
+			<p>AAAC Saturday Art School teacher Chi Lee managed and directed 17 teenagers between 18 and 21 years old from all over China at CMP to address the problem of making Chinatown a welcoming place for teens. They planned, researched, compiled agreements with shopkeepers as well as designed together a Map for Teens, locating twenty-two places welcoming teenagers in Chinatown. This was finalized and easily carried by folding into an origami shape. A collaboration between AAAC Saturday Art School & Chinatown Manpower Project (CMP).</p>
+			<img src="/_images/TeenMap_Front.jpg" alt="Chinatown map created by students (front)">
+			<img src="/_images/TeenMap_Back.jpg" alt="Chinatown map created by students (back)">
+
+
+			<h2>Clusterbomb! Zero Capital Exhibition</h2>
 			<p><span class="date">December 13 – January 15, 2009</span></p>
-			<p>Zero Capital's CLUSTERBOMB! is a creative response to physical and psychological displacement in the post 9-11 era of disillusionment and artistic inertia. It is an interdisciplinary, intergenerational and transnational network of artists, writers, activists, cultural workers and youth. Participants are posing a collective challenge to the issue of have and have-nots, why those who have are getting more and those who don't have are getting less or nothing.</p>
+			<p>Clusterbomb! is a creative response to physical and psychological displacement in the post 9-11 era of disillusionment and inertia.  It is an interdisciplinary, intergenerational and transnational network of artists, writers, activists, cultural workers and youth. Participants are posing a collective challenge to why those who have are getting even more and those who don't have are getting even less, or nothing. A meeting at the closing of artists, stakeholders and interested parties included performances, new works, home cooked food, drinks and a discussion on the future Zero Capital international artists collaborations, events, and fundraising efforts. Artist Tomie Arai distributed signed, audience-selected Free Art chosen at the Opening. <i><a href="http://artasiamerica.org/documents/1357/74" target="_blank">Learn more</a>.</i></p>
 			<img src="/_images/zerocapital_2.gif" alt="Poster for the Zero Capital CLUSTERBOMB! Exhibition">
 			<h6>Zero Capital artists:</h6>
 			<ul class="list-columns">
@@ -57,19 +64,18 @@ include '_inc/head.php';
 				<li>Angel Velasco Shaw</li>
 			</ul>
 
-			<h2>Tibetan Thangka Paintings</h2>
+			<h2>An Exhibition of Tibetan Thangka Paintings</h2>
 			<p><span class="date">January 26 – March 27, 2009</span></p>
-			<p>This exhibition was a part of the AAAC's Lunar New Year festival in 2009. Five traditional artists/crafts people gave demonstrations, including Ye Xun — a Chinese miniature dough figurine artist, Rose Sigal-Ibsen — Sumi-e ink calligrapher, Kavita Vyas — an Indian Mahendi hand painting artist, Duong Truong — fortune teller, Kwok Kay Choey — an Er Hu performer, together with Jampa Youden — a Tibetan folk singing performance. </p>
-			<p>At the opening reception, Lama Tenzin presented a talk on the significance of Thangka paintings. Thangka paintings usually depict deities and saints, aspects of the life of the Buddha, as well as the more abstract Mandalas, astrological charts, medical descriptions and scenes, usually serving as a supplementary aid to religious educational development. They are meant as focal elements for visualization, meditation and contemplation.</p>
+			<p>A three hour talk by Lama Tenzin presented at the opening reception.</p>
 			<img src="/_images/5_Medicine_Buddha.jpg" alt="A Tibetan Thangka Painting of Medicine Buddha">
 
 			<h2>Art & the Asian Hemisphere</h2>
 			<p><span class="date">April 29, 2009</span></p>
 			<p>A talk by Robert Lee at Central Connecticut State University for Professor Ki Hoon Kim on April 29, 2009. Based on book by Kishore Mahbubani entitled <i>The New Asian Hemisphere: THe Irresistible Shift of Global Power to the East</i>.</p>
 
-			<h2>America’s Chinatown Voices</h2>
-			<p><span class="date">April 23 – May 9, 2010</span></p>
-			<p>This event included both an exhibition and a <a href="https://www.flickr.com/photos/asianamericanartscentre/collections/72157624006184156/">silent auction</a> of red panel artworks from the <a href="https://www.flickr.com/photos/asianamericanartscentre/sets/72157618119632906/">Summer 2009 Outdoor Exhibition</a> in Columbus Park where community members were invited to paint on panels. Three artists generously donated the panels to benefit the Asian American Art Centre.</p>
+			<h2>America’s Chinatown Voices 藝匯唐人街</h2>
+			<p><span class="date">May 9 – August 8, 2010</span></p>
+			<p>Columbus Park becomes the site of a special outdoor art throughout the Summer where artists & volunteers install, rotate and maintain about 80 painted panels on which community people, children, artists and other New Yorkers can express themselves in written Chinese or English and painted images. People who live in Chinatown, as well as people who enjoy and care about this corner of New York City, are given the opportunity to speak their mind or simply to express their joy. <i>Learn more: <a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157618119632906" target="_blank">photo album 1</a>, <a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157623881782137" target="_blank">2</a>, <a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157624025562178" target="_blank">3</a>, <a href="https://www.nycgovparks.org/art/art14" target="_blank">NYC parks</a>, <a href="https://www.flickr.com/photos/asianamericanartscentre/4584064125/in/album-72157624006043804/" target="_blank">artwork</a>, <a href="http://artasiamerica.org/artist/detail/180" target="_blank">artist profile 1</a>, <a href="http://artasiamerica.org/artist/detail/181" target="_blank">2</a>.</i></p>
 			<img src="/_images/red_panels.png" alt="Three black on red panel paintings by Avani Patel and Natalie Pham">
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
@@ -77,13 +83,9 @@ include '_inc/head.php';
 				<li>Avani Patel</li>
 			</ul>
 
-			<h2>artasiamerica Launch</h2>
-			<p><span class="date">July 28, 2009</span></p>
-			<p>AAAC announces the digital archive, artasiamerica.org marking the start of this expanding resource for the history of Asian American contemporary art.  It consists of historical images and documents focused on Asian American visual culture from 1945 to the present. The digital archive, selected from our physical archive of 1500 entries, is a professional collection of Asian/Asian American contemporary visual artists. The second phase will continue to cull the artists who were important in articulating the subject of Asian American Art and the issues of diversity in America.</p>
-
-			<h2>ARTSLAM</h2>
+			<h2>ARTSLAM 2009</h2>
 			<p><span class="date">July 3, 2009</span></p>
-			<p>ARTSLAM is part of a series of events allowing emerging artists the opportunity to present their work, as well as meet and network with established artists and critics/curators. </p>
+			<p>Comments by Gregory Coates and Bob Lee. <i><a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157621899520607" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Hwa Hyun Kim</li>
@@ -92,9 +94,13 @@ include '_inc/head.php';
 				<li>Xiaoqing Ding</li>
 			</ul>
 
-			<h2>SPECIAL ARTSLAM: Artist Talk by Kerry Ann Lee</h2>
+			<h2>SPECIAL ARTSLAM</h2>
 			<p><span class="date">July 8, 2009</span></p>
-			<p>Kerry Ann Lee is an artist, designer and educator based in Wellington, New Zealand. Her work: Home Made: Picturing Chinese Settlement in New Zealand celebrates an alternative cultural history through cut-paper, paint, found text and images. Presenting both personal and local experiences of Chinese settlement in New Zealand, both the Chinese face behind the takeout counter and the home customs housed behind the plastic ribbon curtain.</p>
+			<p>Artist Talk by Kerry Ann Lee. Kerry Ann Lee is an artist, designer and educator based in Wellington, New Zealand. Her work, “Home Made: Picturing Chinese Settlement in New Zealand,” celebrates an alternative cultural history through cut-paper, paint, found text and images. She presents both personal and local experiences of the Chinese settlement in New Zealand, those that the Chinese face behind the takeout counter as well as the home customs housed behind the plastic ribbon curtain. <i><a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157621899520607" target="_blank">Learn more</a>.</i></p>
+
+			<h2>artasiamerica launch!</h2>
+			<p><span class="date">July 28, 2009</span></p>
+			<p>AAAC announces the digital archive, <a href="http://www.artasiamerica.org" target="_blank">artasiamerica.org</a>, marking the start of this expanding resource for the history of Asian American contemporary art.  It consists of historical images and documents focused on Asian American visual culture from 1945 to the present. The digital archive, selected from our physical archive of 1500 entries, is a professional collection of Asian/Asian American contemporary visual artists. The second phase will continue to cull the artists who were important in articulating the subject of Asian American Art and the issues of diversity in America.</p>
 	</main>
 	<div class="fade"></div>
 	<nav role="navigation" id="mm">
