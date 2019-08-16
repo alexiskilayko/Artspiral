888b0c8568417e2f76ab6ef20a234e91949a9c12
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:08:35 -0400

    2005-2006 update

--- 2005-2006.php
+++ 2005-2006.php
@@ -13,25 +13,15 @@ include '_inc/head.php';
 	</nav>
 	<main role="navigation" class="content-wrapper narrow">
 		<span class="tag">2005 &ndash; 2006</span>
-			<h2>Organized Spaces</h2>
-			<p><span class="date">August 26 – September 9, 2005</span></p>
-			<p>Curated by Meridith McNeal as a special prelude to the 2005 – 2006 exhibition season, featuring Singaporean artists. </p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Colin Goh</li>
-				<li>Jon Kher Kaw</li>
-				<li>Geraldine Lau</li>
-				<li>Alan Oei</li>
-				<li>Joel Seah</li>
-				<li>Hong-Ling Wee</li>
-				<li>Jason Wee</li>
-				<li>Su-en Wong</li>
-				<li>Yen Yen Woo</li>
-				<li>sciSKEW collaborative</li>
-			</ul>
-
-			<h2>Aphasia: 15th AAAC Annual Exhibition</h2>
+			<h2>15th AAAC Annual: Aphasia</h2>
 			<p><span class="date">September 23 – October 28, 2005</span></p>
+			<p>This exhibition concerns itself more with a variety of subjective and ethnic themes than one overriding motif.  If we look for a common Asian theme in the work of these artists, we will find that the show may seem a bit confused. Several of these artists are similar, however, in their use of digital and mixed new media and of images from commodities, games, and popular interpretations of alien beings. An artists’ talk was held October 2. <i>Learn more: <a href="http://artasiamerica.org/documents/3996/187" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/4003/187" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/4902/121" target="_blank">artist statement</a>.</i></p>
+			<h6>Selection panelists:</h6>
+			<ul class="list-columns">
+				<li>Dr. Koan-Jeff Baysa</li>
+				<li>Luchia Meihua Lee</li>
+				<li>Reiko Tomii</li>
+			</ul>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Ernest M. Concepcion</li>
@@ -44,19 +34,9 @@ include '_inc/head.php';
 				<li>Indira Johnson</li>
 			</ul>
 
-			<h2>Nuo Masks</h2>
-			<p><span class="date">November 28, 2005 – March 10, 2006</span></p>
-			<p>The Nuo ritual has been practiced in China since primitive times over four thousand years ago. Ritual "Nuo" masks may be the only living art tradition linked historically to the Bronze Age of China. The origins of Nuo culture in Zhejiang province can be traced back to the Liangzhu culture of the Neolithic period. Nuo is a form of masked drama. Priests or specially trained shaman-dancers used masks to perform at ritual ceremonies to expell ghosts and evil spirits. Nuo is also a theatrical opera held at festival, aimed to establish ties and norms between heaven and earth, man and gods. Traditionally, following the solemn ritual, the Nuo opera was performed to entertain the spirits. According to Chinese folklore, a street parade of legendary generals and deities could best expel evil from the community.</p>
-
-			<h2>Tibetan Thangka Paintings</h2>
-			<p><span class="date">January 2 – March 10, 2006</span></p>
-			<p>This exhibition was a part of the AAAC's Lunar New Year festival in 2009. Five traditional artists/crafts people gave demonstrations, including Ye Xun — a Chinese miniature dough figurine artist, Rose Sigal-Ibsen — Sumi-e ink calligrapher, Kavita Vyas — an Indian Mahendi hand painting artist, Duong Truong — fortune teller, Kwok Kay Choey — an Er Hu performer, together with Jampa Youden — a Tibetan folk singing performance. </p>
-			<p>At the opening reception, Lama Tenzin presented a talk on the significance of Thangka paintings. Thangka paintings usually depict deities and saints, aspects of the life of the Buddha, as well as the more abstract Mandalas, astrological charts, medical descriptions and scenes, usually serving as a supplementary aid to religious educational development. They are meant as focal elements for visualization, meditation and contemplation.</p>
-			<img src="/_images/5_Medicine_Buddha.jpg" alt="A Tibetan Thangka Painting of Medicine Buddha">
-
 			<h2>Detained</h2>
 			<p><span class="date">March 24 – May 5, 2006</span></p>
-			<p>An Arab American & Asian American exhibition aimed to bring two communities together around issues of race, exclusion & spirituality. Chaplain James Yee, born and raised in New Jersey, chose to convert to Islam after serving in the US Army during the Gulf War, several years before being secretly arrested while serving at Camp Delta in Guantanamo Bay, Cuba, on allegations of espionage. He was detained for 76 days before all criminal charges were dropped. Aside from the issues raised by this incident, are the underlying spiritual questions – an American, and an Asian American, graduate of West Point who choses to convert to Islam.  It is this personal narrative that may enable people to relate to each other, not just intellectually with information and knowledge but to see and encounter each other in a different light. <a href=”www.justiceforyee.com”>Read for more information</a>. Organized by  Rabab Abdulhadi, Director of the Center for Arab American Studies, University of Michigan & Robert Lee.</p>
+			<p>An Arab American and Asian American exhibition aimed to bring two communities together around issues of race, exclusion, and spirituality. Chaplain James Yee, born and raised in New Jersey, chose to convert to Islam after serving in the US Army during the Gulf War, several years before being secretly arrested while serving at Camp Delta in Guantanamo Bay, Cuba, on allegations of espionage. He was detained for 76 days before all criminal charges were dropped. Aside from the issues raised by this incident are the underlying spiritual questions – an American, and an Asian American, graduate of West Point who chose to convert to Islam.  It is this personal narrative that may enable people to relate to each other, not just intellectually with information and knowledge but to see and encounter each other in a different light. Organized by Rabab Abdulhadi, Director, Center for Arab American Studies, University of Michigan, and Robert Lee, executive director of AAAC. Panel talks, etc., are projected with the assistance of the James Yee ad hoc Support Committee. <i>Learn more: <a href="http://artasiamerica.org/documents/2729/126" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2728/32" target="_blank">artist statement</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Wafaa Bilal</li>
@@ -74,7 +54,7 @@ include '_inc/head.php';
 
 			<h2>Chinatown Work 2006</h2>
 			<p><span class="date">April 18 – May 21, 2006</span></p>
-			<p>The façade of HSBC Bank at 58 Bowery, near Manhattan Bridge every evening 730 – 11pm was the site for an interactive art installation.  Silhouetts of pedestrians mix the footage of interior work spaces with time-laps exterior street images of unique areas that define NY’s Chinatown contemporary work culture.  The interactive and responsive installation prompts passerbys to participate, recognizing and celebrating this community and the working people who make it.</p>
+			<p>Sponsored by the artists with support from AAAC, Rebuild Chinatown Initiative, Explore Chinatown Campaign, and Chinatown Partnership LDC. Every evening from 7:30 to 11pm, the façade of HSBC Bank at 58 Bowery near Manhattan Bridge was the site for an interactive art installation.  Silhouettes of pedestrians mix the footage of interior work spaces with time-lapse exterior street images of unique areas that define New York’s Chinatown contemporary work culture.  The interactive and responsive installation prompts passerbys to participate and to recognize and celebrate this community and the working people who make it. <i><a href="http://eskyiu.com/chinatownwork/" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Eric Schuldenfrei</li>
@@ -83,7 +63,7 @@ include '_inc/head.php';
 
 			<h2>New York Eviction Blues II</h2>
 			<p><span class="date">May 15 – June 9, 2006</span></p>
-			<p>An art exhibition at the Manhattan Borough president's office to call attention to the citywide problem of housing for artists and art organizations. A silent Auction for displayed art work was held to assist the Centre with its legal fees to stay in its home of 31 years.</p>
+			<p>An art exhibition calling attention to the citywide problem of housing for artists and art organizations. A silent auction for displayed art work was held to assist the Centre with its legal fees and help it to stay in its home of 31 years. Held at Manhattan Borough President’s Office. Sponsored by Manhattan Borough President Scott M. Stringer.</p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 		    <li>Yoshiki Araki</li>
@@ -117,11 +97,23 @@ include '_inc/head.php';
 		    <li>Yoichiro Yoda</li>
 		    <li>Charles Yuen</li>
 			</ul>
+			<h6>Additional artists:</h6>
+			<ul class="list-columns">
+				<li>Yoshiki Araki</li>
+				<li>Pedro Elias Cruz-Castro</li>
+				<li>Edward Fausty</li>
+				<li>Fred Fleisher</li>
+				<li>Kay Gordon</li>
+				<li>Vikki Law</li>
+				<li>Eva Mantell</li>
+				<li>Mac McGill</li>
+				<li>Elisa Soliven</li>
+				<li>Maggie Wrigley</li>
+			</ul>
 
-			<h2>Mid Career: Mayumi Terada & Choong Sup Lim</h2>
+			<h2>Mayumi Terada / Choong Sup Lim at Mid Career</h2>
 			<p><span class="date">May 19 – June 24, 2006</span></p>
-			<p>Choong Sup Lim deals with the dichotomies of east/west, interior/exterior, rural/urban, and nature/industry through the contradictions and paradoxes in his work. Forms and materials are carefully selected for their capacity to conjure multiple associations.  He has been working both here and in Korea.</p>
-			<p>Since 2001, Mayumi Terada has been building dollhouse-sized interior spaces from balsa wood, Styrofoam and clay, then photographing and enlarging them into large silver gelatin prints. A sense of absence emerges from recent evidence of a presence in these sparsely-furnished rooms.</p>
+			<p>Choong Sup Lim deals with the dichotomies of east/west, interior/exterior, rural/urban, and nature/industry through the contradictions and paradoxes in his work. Forms and materials are carefully selected for their capacity to conjure multiple associations.  He has been working both in the United States and in Korea.  Since 2001, Mayumi Terada has been building dollhouse-sized interior spaces from balsa wood, styrofoam. and clay, then photographing and enlarging them into large silver gelatin prints. A sense of absence emerges from recent evidence of a presence in these sparsely-furnished rooms. Terada has lived in New York City for the past few years, exhibiting in Los Angeles, NYC, Korea, Tokyo and other cities throughout Japan. <i>Learn more: <a href="http://artasiamerica.org/documents/4865/201" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/4866/201" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Mayumi Terada</li>
@@ -130,7 +122,9 @@ include '_inc/head.php';
 
 			<h2>Yoshiki Araki: Hiroshima Born</h2>
 			<p><span class="date">July 7 – August 11, 2006</span></p>
-			<p>Yoshiki Araki was born in March 1950 in Hiroshima. He was raised by his mother while his father served in the navy and rose to rear admiral. When the bomb fell in Hiroshima, his mother, then a young girl, went into what was left of the city to search for her father.  She never found him. Yoshiki worked a jackhammer in mine for a year to come to the USA, then spent it all in a month.  Yoshiki did make it to New York studying at the Art Students League, living on Amsterdam Ave, in Harlem and later in Brooklyn. In 2000 he was evicted from his home and studio of many years that was filled with his art works and preparations for many more.  Under the stress and tension of trying to find a new space to accommodate all that he had gathered and planned, Araki became ill and had to be hospitalized. He died shortly thereafter. AAAC offers this opportunity to view some of the paintings and sculpture that he left behind.</p>
+			<p>Yoshiki Araki died in 2000. He was born in March of 1950. His mother, as a young teenager, went into what was left of Hiroshima, the ruins of that nuclear wasteland, to search for her father, last of an old Samari family, but never found him. As a young man Yoshiki did make it to New York studying at the Art Students League,. In 2000 he was evicted from his home and studio in Brooklyn of many years that was filled with his art works and preparing for many more.  Under the stress and tension of trying to find a new space to accomodate all that he had gathered and planned, within a month Araki became ill and had to be hospitalized. He died shortly thereafter. Today we have yet to come to terms with the inhumanity of Hiroshima. In the darkness of that moment, as a second generation survivor, Yoshiki sought its quest in his art. His work may still offer us clues as to a journey we have yet to take. Some of his paintings and sculptures that he left behind is preserved in the AAAC Permanent Collection. Curated by Robert Lee & Sayaka Araki.</p>
+			<p>Hiroshima Legacy: The Art of Yoshiki Araki was a panel talk on August 6, 2006, featuring: Jesse Fuchs, artist; Tristan Wolski, artist; Raphael Mostel, Tibetan Singing Bowl Ensemble; Sayaka Araki; Peaceful Tomorrows; September 11 Families; David McReynolds, the War Resisters League; Robert Lee, Moderator.</p>
+			<p><i>Learn more: <a href="http://artasiamerica.org/documents/2428/75" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2430/75" target="_blank">poster</a>, <a href="http://artasiamerica.org/documents/2431/75" target="_blank">review</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Yoshiki Araki</li>
