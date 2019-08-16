1089f0ba5707df82820ee5cf24507e31e151ef3e
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:08:50 -0400

    2006-2007 update

--- 2006-2007.php
+++ 2006-2007.php
@@ -15,7 +15,7 @@ include '_inc/head.php';
 		<span class="tag">2006 &ndash; 2007</span>
 			<h2>THREE WOMEN: Art and Spiritual Practice</h2>
 			<p><span class="date">September 22 – November 3, 2006</span></p>
-			<p>Exhibition of artists who find their calling in a secular society of contradictions and paradox. All art to some extent, lays claim to  concerns of the spirit.  For some artists, however, spiritual issues occupy a central concern, some indicate another realm or existential plane, others are based on access to esoteric knowledge and posit this in their work.  The art making practice of these three women is affected by such beliefs as well as the mental states and  artistic recipes that have evolved for them as individual personalities coming to bare fruit with the fragrance of each art work created  In this way, Mikyung, Anna, & Younhee is an individual, and though one may not draw a direct tie to any one proposition on the spiritual, it is possible to say that to the extent a viewer can appreciate the art created is the extent a viewer senses the fragrant moment of its conception.  Such art intimates a disclosure – that this life is staged in the context of a larger mystery and mundane habits and occurrences can be transformed by the light of this mystery. Curated by Robert Lee.</p>
+			<p>All art to some extent lays claim to concerns of the spirit.  For some artists, however, spiritual issues occupy a central concern, indicate another realm or existential plane, or are based on access to esoteric knowledge, and they posit this in their work.  The art making practice of these three women is affected by such beliefs. Their work is also informed by the mental states and artistic recipes that have evolved for them as individual personalities coming to bear fruit with the fragrance of each art work created.  In this way, Mikyung, Anna, & Younhee is an individual, and though one may not draw a direct tie to any one proposition on the spiritual, it is possible to say that to the extent a viewer can appreciate the art created is the extent a viewer senses the fragrant moment of its conception.  Such art intimates a disclosure – that this life is staged in the context of a larger mystery and mundane habits and occurrences can be transformed by the light of this mystery. Curated by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/2591/191" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2592/53" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/5238/191" target="_blank">artist statement</a>.</i></p>
 			<img src="/_images/threewomen.png" alt="Works in the Three Women: Art and Spiritual Practice exhibition">
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
@@ -26,20 +26,28 @@ include '_inc/head.php';
 
 			<h2>Understanding the Past: Freedom of Expression and Democratic Processes Today</h2>
 			<p><span class="date">October 19, 2006</span></p>
-			<p>Presented by <a href="http://www.thailinks.org/">ThaiLinks</a>. Visual presentation on Hok Tulaa by artist Prawat Laucharoen. Paul Handley author of the King Never Smiles.</p>
+			<p>Presented by <a href="http://www.thailinks.org">ThaiLinks</a>. Visual presentation on Hok Tulaa by artist Prawat Laucharoen. Paul Handley author of the King Never Smiles.</p>
 
 			<h2>Future Creatures in Chinatown</h2>
 			<p><span class="date">October 31 – November 30, 2006</span></p>
-			<p>Eunjung Hwang explores dream and subconscious imagery in public moving image installations at multiple sites in Chinatown. Two minute animation loops on each display unit offer unexpected encounters for passerbys on public streets, presenting a fragmented reality in a variety of fanciful characters and visionary narratives. This exhibitions was supported by the Lower Manhattan Cultural Council and the September 11th Fund.</p>
+			<p>Eunjung Hwang explores dream and subconscious imagery in public moving image installations at multiple sites in Chinatown. Two-minute animation loops on each display unit offer unexpected encounters for passerbys on public streets, presenting a fragmented reality in a variety of fanciful characters and visionary narratives. Partly drawn from the ancient traditions of Asia and its related symbolism.  Eunjung Hwang received her MFA from SVA and received the Principal prize in the Oberhausen International Short Film Festival in Germany. Sponsored by Asian American Arts Centre. Supported by the Lower Manhattan Cultural Council and the September 11th Fund. Held at HSBC Bank at 11 East Broadway, Chinatown Partnership Local Development Corporation (CPLDC) St. 60 James Place, Silkroad Place, 30 Mott St., AAAC 26 Bowery, and more. <i>Learn more: <a href="http://artasiamerica.org/documents/5861/162" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/5871/162" target="_blank">catalog</a>.</i></p>
 			<img src="/_images/futurecreatures.jpg" alt="Postcard of Eujung Hwang's Future Creatures">
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Eunjung Hwang</li>
 			</ul>
 
-			<h2>Fracture Fairytales: 16th AAAC Annual Exhibition</h2>
+			<h2>16th AAAC Annual: Fractured Fairytales</h2>
 			<p><span class="date">December 8, 2006 – January 19, 2007</span></p>
-			<p>Through a review of the Asian American Artists Slide Archive's previous year's submissions, artists are selected by a panel.  This year five artists were chosen who reflect a nexus of themes around personal icons - fantasy, dreams, pop culture, identity & appropriation.  In a complex society with multiple contradictory interests at work, the drive to encompass these through creativity is a sustaining mark of these artist's works. </p>
+			<p>Through a review of the Asian American Artists Slide Archive's previous year's submissions, artists are selected by a panel.  This year five artists who reflect a nexus of themes around personal icons - fantasy, dreams, pop culture, identity, and appropriation - were chosen.  In a complex society with multiple contradictory interests at work, the drive to encompass these through creativity is a sustaining mark of these artists’ works. Together they compose a very current part of the contemporary presence of Asia in America. The Archive is open to all visual artists of Asian ancestry and artists who are strongly influenced by Asia. <i>Learn more: <a href="http://artasiamerica.org/documents/3938/55" target="_blank">brochure</a>, <a href="http://artasiamerica.org/documents/1478/55" target="_blank">postcard</a>.</i></p>
+			<h6>Participating artists:</h6>
+			<ul class="list-columns">
+				<li>Dr. Koan-Jeff Baysa</li>
+				<li>Shelly Bahl</li>
+				<li>Judy Collischan</li>
+				<li>Jung Hyang Kim</li>
+				<li>Reiko Tomii</li>
+			</ul>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Jon Cuyson</li>
@@ -49,26 +57,29 @@ include '_inc/head.php';
 				<li>Jan-Ru Wan</li>
 			</ul>
 
-			<h2>Chinese Classical Architecture</h2>
-			<p><span class="date">December 18, 2006 – March 16, 2007</span></p>
-			<p>Artistic ornamental color painting on architectural structures shows the unique spirit and concept of China's classical building - an architectural culture of ornamental art. This art form first appeared on buildings during the Spring and Autumn Period (770-220 BC).</p>
-			<p>The architectural ornamental art displays the beauty of Chinese folk art and high traditional culture as these have continuously influenced each other. Embedded in this ornament we can become aware of a collage of the many dimensions, and overlapping layers that integrate structural form with non structural elements of color, symbols, rebus art and pictorial imagery.  Principles of Fung Shui, Chinese philosophical notions, even Chinese medicine, are thoroughly embedded in traditional architecture and its orientation in space.  Multi-facet symbolism in design and motifs point to a way of life at peace with nature and natural processes, from which the contemporary goals of a sustainable society can learn.</p>
-			<p>The organizer and researcher,  Ms. Chen Xiaorui, is a partner and Senior Designer of the China Architect Engineering and Consulting Company based in Beijing.</p>
+			<h2>Chinese Classical Architecture's Ornamental Art</h2>
+			<p><span class="date">February 11 – March 16, 2007</span></p>
+			<p>Artistic ornamental color painting on architectural structures shows the unique spirit and concept of China's classical building - an architectural culture of ornamental art. This art form first appeared on buildings during the Spring and Autumn Period (770-220 BC). The architectural ornamental art displays the beauty of Chinese folk art and high traditional culture as these have continuously influenced each other. Embedded in this ornament we can become aware of a collage of the many dimensions and overlapping layers that integrate structural form with non-structural elements of color, symbols, rebus art and pictorial imagery.  Principles of Fung Shui, Chinese philosophical notions, and even Chinese medicine are thoroughly embedded in traditional architecture and its orientation in space.  Multi-facet symbolism in design and motifs point to a way of life at peace with nature and natural processes, from which the contemporary goals of a sustainable society can learn. The artist, Ms. Chen Xiaorui, who gave several lectures and public appearances, is a partner and Senior Designer of the China Architect Engineering and Consulting Company based in Beijing. Organized, presented, and researched by Ms. Chen Xiaorui.</p>
 
-			<h2>Mid Career: Bing Lee & Bovey Lee</h2>
+			<h2>Bing Lee / Bovey Lee at Mid Career</h2>
 			<p><span class="date">March 30 – May 11, 2007</span></p>
-			<p>The eighth installment in the Mid Career series. Bing Lee’s pictographs are an ever-growing series of intuitively generated calligraphic entities, drawn with ink and/or graphite on squares of rice paper.  Lee mirrors the poetic, visual & logical basis of the Chinese written language, while the inspirations for his images or discipline stem from a wide variety of sources.  Lee sites a childhood interest in cartoons, Chinese Opera, urban media, Hong Kong street culture, and American popular culture that flooded Hong Kong. </p>
-			<p>Bovey Lee’s paintings and drawings depict floating and weightless objects, flying in transitional modes and planes, visual metaphors for her experiences of displacement and confusion as a recent immigrant. Since 1995, Bovey has used digital media to deconstruct and reconstruct her body into new landscapes. Bovey’s web-based work explores themes of emerging technology, de-humanization of the body and the moral issues surrounding the fabrication of multiple identities in cyberspace.</p>
+			<p>This is the eighth exhibition in the Mid Career series, which aims to bring attention to artists who have been in the field for about 20 years. It also affords an opportunity to bring professional critics, historians, and others together around artists’ directions. Bing Lee’s pictographs are an ever-growing series of intuitively generated calligraphic entities, drawn with ink and/or graphite on squares of rice paper.  Lee mirrors the poetic, visual and logical basis of the Chinese written language, while the inspirations for his images and discipline stem from a wide variety of sources.  Lee cites a childhood interest in cartoons, Chinese Opera, urban media, Hong Kong street culture, and American popular culture that flooded Hong Kong. Bovey Lee’s paintings and drawings depict floating and weightless objects, flying in transitional modes and planes, visual metaphors for her experiences of displacement and confusion as a recent immigrant. Since 1995, Bovey has used digital media to deconstruct and reconstruct her body into new landscapes. Bovey’s web-based work explores themes of emerging technology, de-humanization of the body and the moral issues surrounding the fabrication of multiple identities in cyberspace. Essay by Murtaza Vali. <i>Learn more: <a href="http://artasiamerica.org/documents/5218/197" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/5219/197" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/5929/197" target="_blank">essay</a>, <a href="http://artasiamerica.org/documents/5217/197" target="_blank">artist statement</a>.</i></p>
+			<h6>Nominating panelists:</h6>
+			<ul class="list-columns">
+				<li>Eugenie Tsai</li>
+				<li>Emily Cheng</li>
+				<li>JungLee Sanders</li>
+				<li>Mako Wakasa</li>
+			</ul>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Bing Lee</li>
 				<li>Bovey Lee</li>
 			</ul>
 
-			<h2>Mixed Skin</h2>
+			<h2>Mixed Skin: Kip Fulbeck, Dorothy Imagire & Toni Thomas</h2>
 			<p><span class="date">June 1 – July 13, 2007</span></p>
-			<p>In this age of mixing cultural identity and ethnicities, how do we define ourselves? In "Mixed Skin," artists of mixed Asian descent & mixed African roots examine this question. Through interviews with people of mixed descent, Dorothy Imagire, an artist of Japanese and Iranian descent, created  a visual metaphor for the question with individual garments that blend  fabrics from different cultures. Accompanying her unique kimonos are books of personal quotes.  Kip Fulbeck's Hapa Project involves several hundred people: Fulbeck photographs those who identify as Hapa (or those who are partially Asian) and asks them to write about themselves. His performances and slide shows are similarly focused.  Toni Thomas does quilted imagery from her researches on the history of Chinatown in Newark NJ where some interracial marriages have been found.</p>
-			<p>Organized by Robert Lee and Vikki Law, essays by Teresa Kim & Albert Chong.</p>
+			<p>In this age of mixing cultural identity and ethnicities, how do we define ourselves? AAAC recognizes and celebrates the heart centered relationships that are the basis for diversity, and that are the final answer for erection of barriers everywhere.  In "Mixed Skin," artists of mixed Asian descent and mixed African roots examine this question. Through interviews with people of mixed descent, Dorothy Imagire, an artist of Japanese and Iranian descent, created a visual metaphor for the question with individual garments that blend fabrics from different cultures. Accompanying her unique kimonos are books of personal quotes.  Kip Fulbeck's Hapa Project involves several hundred people: Fulbeck photographs those who identify as Hapa (or those who are partially Asian) and asks them to write about themselves. His performances and slide shows are similarly focused.  Toni Thomas does quilted imagery from her researches on the history of Chinatown in Newark, NJ, where some interracial marriages have been found. Organized by Vikki Law and Robert Lee. Essays by Teresa Kim and Albert Chong. <i>Learn more: <a href="http://artasiamerica.org/documents/2732/32" target="_blank">brochure</a>, <a href="http://artasiamerica.org/documents/2731/32" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/7145/32" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Kip Fulbeck</li>
