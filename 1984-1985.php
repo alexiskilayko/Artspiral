95e750ba0b9833c275444de7613fceeb5feede0a
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 16:59:19 -0400

    1984-1985 update

--- 1984-1985.php
+++ 1984-1985.php
@@ -13,12 +13,17 @@ include '_inc/head.php';
 	</nav>
 	<main class="content-wrapper narrow">
 		<span class="tag">1984 – 1985</span>
-			<h2>People of China: Photos of Lee C. Lee, Asian American Studies Scholar at Cornell University</h2>
-			<p><span class="date">February 1 - February 27, 1985 (as Asian Arts Institute)</span></p>
+			<h2>People of China: Photos by Lee C. Lee</h2>
+			<p><span class="date">February 1 - February 27, 1985</span></p>
+			<p>Lee C. Lee (or Lee Charlotte Lee) was not only an artist but a pioneer of Asian American studies. The first woman professor of Asian ancestry at Cornell University, she helped initiate the institution’s Asian American Studies program, which was the first of its kind on the East Coast. Lee was known for her passion towards student needs, children’s welfare, and especially the cross-cultural issues between Americans and Asians. Her work is informed by such causes. </p>
+			<h6>Participating artist:</h6>
+			<ul class="list-columns">
+				<li>Lee C. Lee</li>
+			</ul>
 
-			<h2>Emily, Anna and Ti Shan: the First Generation</h2>
+			<h2>Emily, Anna & Ti Shan: the First Generation</h2>
 			<p><span class="date">March 8 – April 5, 1985</span></p>
-			<p>Curated by Robert Lee.</p>
+			<p>These three artists together signify a direction Asians in America have come to take. All born and raised in the United States with little or no contact with their Asian heritage, they partake in a very American tradition of painting, yet add something unique of their own. Their works and styles bear many differences but are alike in their meditative tone. Curated by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/943/156" target="_blank">catalog</a>, <a href="http://artasiamerica.org/documents/1334/156" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Emily Cheng</li>
@@ -28,7 +33,7 @@ include '_inc/head.php';
 
 			<h2>Triangular Landscapes</h2>
 			<p><span class="date">April 12 – May 10, 1985</span></p>
-			<p>Curated by Robert Lee. Catalogue includes essays by Robert Lee.</p>
+			<p>These three young Asian American women bring a particular sensibility to the man-made object and its accompanying space. Drawing upon both Asian and American traditions and employing man’s tool of geometry, the artists create unique and unusual works. “…Awesome powers to plan, manipulate, and control the natural environment…contrast sharply with the Chinese conception of a primal timeless landscape…” Quote by Robert Lee. </p>
 			<h6>Participating scholars:</h6>
 			<ul class="list-columns">
 				<li>Young Hee Han</li>
@@ -38,7 +43,7 @@ include '_inc/head.php';
 
 			<h2>Symposium on Contemporary Asian American Art</h2>
 			<p><span class="date">May 1, 1985</span></p>
-			<p>A slide show of forty Asian American artists were presented from AAAC Artist Archive to art professionals, corporate-based art curators, and independent consultants. Held at NYC Dept. of Cultural Affairs. Organized by Margo Machida, AIR.</p>
+			<p>A slide show of forty Asian American artists were presented from AAAC Artist Archive to art professionals, corporate-based art curators and independent consultants. Organized by Margo Machida, AIR. Held at NYC Department of Cultural Affairs. </p>
 			<h6>Participating artists & speakers:<h6>
 			<ul class="list-columns">
 				<li>Benny Andrews</li>
@@ -52,7 +57,7 @@ include '_inc/head.php';
 
 			<h2>Dreams and Fantasies</h2>
 			<p><span class="date">May 17 – June 14, 1985</span></p>
-			<p>Catalogue includes essays by Barry Schwabsky and Robert Lee. Curated by Robert Lee.</p>
+			<p>In the work of Paul Wong, Helen Oji, and Ming Fay, nature is openly celebrated. Their dreams and fantasies are expressed by novel imagery, which releases a psychic drive, a source of energy and inner joy, an unhibited power contemporary society has sought to control, to hide, and even to deny. Something elemental, hidden from conventional pretenses that has eluded contemporary men and women, is expressed here. Essays by Barry Schwabsky and Robert Lee. Curated by Robert Lee. </p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Helen Oji</li>
@@ -62,7 +67,7 @@ include '_inc/head.php';
 
 			<h2>The First Annual Open Studio Show</h2>
 			<p><span class="date">June 1985</span></p>
-			<p>Featured guided tours to ten artists' studios in the Chinatown commnity and a group show of works by eleven artists.</p>
+			<p>Featured guided tours to ten artists studios in Chinatown community and a group show of works by eleven artists. <i>Learn more: <a href="http://artasiamerica.org/documents/2053" target="_blank">brochure</a>, <a href="http://artasiamerica.org/documents/2155" target="_blank">postcard</a>.</i></p>
 			<h6>Open studio participants:</h6>
 			<ul class="list-columns">
 				<li>John Duff</li>
