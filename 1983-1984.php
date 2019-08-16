93243df69356daa0eb6ded8aea0b729eb9650a20
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 16:58:28 -0400

    Update

--- 1983-1984.php
+++ 1983-1984.php
@@ -15,25 +15,24 @@ include '_inc/head.php';
 		<span class="tag">1983 &ndash; 1984</span>
 			<h2>In the Spirit of Dunhuang: Studies by Zhang Hongtu</h2>
 			<p><span class="date">March 16 – April 9, 1984</span></p>
-			<p>An exhibition of Zhang Hongtu’s works, which present a contemporary interpretation of the spirit of Dunhuang, a city in Western China and notably home to many works of Buddhist art found in caves. Catalogue includes a foreword by Robert Lee. Preface by guest curator Ross Lewis. Essays by Annette Juliano and Zhang Hongtu.</p>
-			<h6>Participating artists:</h6>
+			<p>An exhibition of Zhang Hong-tu’s works, which present a contemporary interpretation of the spirit of Dunhuang, a city in Western China and notably home to many works of Buddhist art found in caves. Catalogue includes a foreword by Robert Lee. Preface by guest curator Ross Lewis. Essays by Annette Juliano and Zhang Hongtu.</p>
+			<h6>Participating artist:</h6>
 			<ul class="list-columns">
 				<li>Zhang Hongtu</li>
 			</ul>
 
 			<h2>Chihung Yang:  Works on Paper</h2>
 			<p><span class="date">April 27 - May 26, 1984</span></p>
-			<p>An exhibition of Chihung Yang’s works on paper (a great contrast to his larger works on canvas), centered on themes of memory. His works express past and present in coexistence and his desire for his art not to be seen as tied to his identities but simply to reflect the spirit of his era. Catalogue includes an introduction by Robert Lee, essay by John Yau, and several color illustrations. Curated by Robert Lee.</p>
-			<h6>Participating artists:</h6>
+			<p>An exhibition of Chihung Yang’s works on paper (a contrast to his larger works on canvas), centered on themes of memory. His works express past and present in coexistence and his desire for his art not to be seen as tied to his identities but simply for it to reflect the spirit of his era. Catalogue includes an introduction by Robert Lee, essay by John Yau, and several color illustrations. Curated by Robert Lee. </p>
+			<h6>Participating artist:</h6>
 			<ul class="list-columns">
 				<li>Chihung Yang</li>
 			</ul>
 
 			<h2>Door Gods and Other Household Deities</h2>
-			<p><span class="date">January - February 1984</span></p>
-			<p>Rooted in the folk practices and beliefs of China, diverse paper gods are often placed on the doors and in various areas of the house, especially during holidays; they are invested with a wonderful sense of enchantment, piety and humor.</p>
-			<p>Introduction by Anne Goodrich. Essays by Maxine Miska and Robert Lee. Talk on Chinese folk religion and art on December 11 by Dr. Chi Chong Lee Lao, Howard University and Dr. Sha Shung, Pse of Georgetown University. Sponsored by the Chinese Community & Cultural Center in Washington.</p>
-			<p>This exhibition traveled to the Jamaica Arts Center (May 12 – June 16, 1984), the Martin Luther King Library in Washington, DC  (Dec 9 – Jan 2 1985).</p>
+			<p><span class="date">May 12 - June 16, 1984; December 9 - January 2, 1985</span></p>
+			<p>Rooted in the folk practices and beliefs of China, diverse paper gods are often placed on the doors and in various areas of the house, especially during holidays; they are invested with a wonderful sense of enchantment, piety and humor.  </p>
+			<p>Introduction by Anne Goodrich. Essays by Maxine Miska and Robert Lee. Talk on Chinese folk religion and art on December 11 by Dr. Chi Chong Lee Lao, Howard University and Dr. Sha Shung, Pse of Georgetown University. Sponsored by the Chinese Community & Cultural Center in Washington. This exhibition traveled to the Jamaica Arts Center (May – June), the Martin Luther King Library in Washington, DC (December – January). </p>
 			<h6>Participating scholars:</h6>
 			<ul class="list-columns">
 				<li>Maxine Miska</li>
