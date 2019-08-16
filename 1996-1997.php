c9fab3d926c4bca5682411e48fb21a8dd72a2ba7
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:04:03 -0400

    1996-1997 update

--- 1996-1997.php
+++ 1996-1997.php
@@ -13,27 +13,28 @@ include '_inc/head.php';
 	</nav>
 	<main class="content-wrapper narrow" role="main">
 		<span class="tag">1996 &ndash; 1997</span>
-			<h2>12 Cicadas in the Tree of Knowledge</h2>
-			<p><span class="date">September 1996</span></p>
+			<h2>6th AAAC Annual: 12 Cicadas in the Tree of Knowledge</h2>
+			<p><span class="date">September 20 - November 2, 1996</span></p>
+			<p>Twelve artists chosen from the permanent Asian American Artists Archive. Each individual’s work was measured under their own light and selected based on an instinctive response; no consistent theme is used in this wide-ranging show. This exhibition introduces twelve individuals who are connected only by a common desire for an artistic and visual language. Artists selected by Heejung Kim, Project Manager; Helen Shin, intern; and Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/2704/23" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/4022/23" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
-				<li>Kenta Furusho, J</li>
-				<li>Yoshio Itagaki, J</li>
-				<li>Yun Fei, J</li>
-				<li>Jessie Joo, K</li>
-				<li>Hae Yuon Kim, K (Ca)</li>
-				<li>Jinhee Kim, K</li>
-				<li>Cynthia Lin, CA (T)</li>
-				<li>B.G> Muhn, K (MD)</li>
-				<li>Lily Tsong, CA (MN)</li>
-				<li>Khangh Vo, V (Ca)</li>
-				<li>Paul Wong, CA</li>
+				<li>Kenta Furusho</li>
+				<li>Yoshio Itagaki</li>
+				<li>Yun Fei</li>
+				<li>Jessie Joo</li>
+				<li>Hae Yuon Kim</li>
+				<li>Jinhee Kim</li>
+				<li>Cynthia Lin</li>
+				<li>B.G. Muhn</li>
+				<li>Lily Tsong</li>
+				<li>Khangh Vo</li>
+				<li>Paul Wong/li>
 				<li>Furong Zhang</li>
 			</ul>
 
 			<h2>Milieu Part II</h2>
-      <p><span class="date">December 1996</span></p>
-      <p>The second of a three-part exhibition exploring Asian American artists and their social environments from the years 1945 to 1965. Based on the research project begun in 1986 entitled "Asian American Artists and Their Milieu 1945 – 1965.” Curated by Robert Lee.</p>
+      <p><span class="date">November 22, 1996 - January 11, 1997</span></p>
+      <p>Second in an exhibition series, Milieu: Part II brings together three Asian American artists who were active in the two decades of the post World War II era. It is based on a national research project begun in the mid 80s entitled, “Asian American Artists and Their Milieu: 1945-1965,” which documented nearly ninety artists and interviewed many. The aim is to trace the complex story of Asian American artists and the developing cultural presence of Asians in America. In this installment, we explore the participation of Asian American artists in the artistic movement of Abstract Expressionism—how they brought their own sensibility to the movement while establishing a connection to their Asian heritage. <i>Learn more: <a href="http://artasiamerica.org/documents/3985/169" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/3992/169" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Alfonso Ossorio</li>
@@ -51,11 +52,42 @@ include '_inc/head.php';
 
       <h2>Impressions: Contemporary Asian Artists Prints</h2>
 			<p><span class="date">February 1 – April 11, 1997</span></p>
-      <p>An exhibition and publication by Brandywine Workshop in Philadelphia. Essay by Robert Lee, “Asian American Art: One Perspective.”</p>
+      <p>Publication by Brandywine Workshop in Philadelphia. Introduction by Allan Edmunds. Essay by Robert Lee, “Asian American Art: One Perspective.” This essay enabled the opportunity to write about the early years of Asian American art at AAAC. An exhibition of twenty-eight artists, either American immigrants or American-born, currently residing in the U.S. but linked by birth or ethnic heritage to Asia.  The exhibition offers a survey of the artists’ diverse styles, themes, and printing techniques and a window into contemporary graphic art as infused by ethnic perspectives and cultural and global crosscurrents.</p>
+			<h6>Participating artists:</h6>
+			<ul class="list-columns">
+				<li>Diyan Achjadi</li>
+				<li>Tomie Arai</li>
+				<li>Debi Ray-Chaudhuri</li>
+				<li>Akiko Enokido</li>
+				<li>Leonardo M. Hidalgo</li>
+				<li>George Yu-Shu Ho</li>
+				<li>Mei-ling Hom</li>
+				<li>Arlan Huang</li>
+				<li>Eunju Kang</li>
+				<li>Betty Nobue Kano</li>
+				<li>Hideki Kimura</li>
+				<li>Mie Kurahara</li>
+				<li>Li-lan</li>
+				<li>Prawat Laucharoen</li>
+				<li>Chen Lok Lee</li>
+				<li>Paul Pak-Hing Lee</li>
+				<li>Jim Lee</li>
+				<li>Bing Lee</li>
+				<li>Yong Soon Min</li>
+				<li>George J. Miyasaki</li>
+				<li>John Takami Morita</li>
+				<li>Steve Murakishi</li>
+				<li>Helen Oji</li>
+				<li>N. Krishna Reddy</li>
+				<li>Toshio Sasaki</li>
+				<li>Roger Shimomura</li>
+				<li>Wucius Wong</li>
+				<li>Yueon Gayh Yehp</li>
+			</ul>
 
       <h2>Three Generations: Towards an Asian American Art History</h2>
-			<p><span class="date">April – May 1997</span></p>
-      <p>A mentoring relationship that spans the post WWII era to the present, from San Francisco to Oberlin to New York. Curated by Robert Lee.</p>
+			<p><span class="date">April 3 – May 17, 1997</span></p>
+      <p>A mentoring relationship that spans the post WWII era to the present, from San Francisco to Oberlin to New York, links these three artists together. The style of one artist is unlike the other, each artist having different artistic goals and concerns. Furthermore, race was not the basis for one artist calling the other their mentor. We examine, then, how these artists are related to each other, as well as how they are related to other Asian American artists and how they contribute to a coherent theme of Asian American art. The Milieu theme informs this exhibition. Curated by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/2714/46" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2715/46" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Tseng Ta- Yu</li>
