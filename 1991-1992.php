f1590f024f4ea29e0b7bcb18e7b9d41a4831fde8
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:02:12 -0400

    1991-1992 update

--- 1991-1992.php
+++ 1991-1992.php
@@ -13,8 +13,9 @@ include '_inc/head.php';
 	</nav>
 	<main role="main" class="content-wrapper narrow">
 		<span class="tag">1991 &ndash; 1992</span>
-		<h2>From “Star Star” to Avant Garde: 2nd AAAC Annual Exhibition</h2>
-		<p><span class="date">October 17 – November 14th</span>: Part one</p>
+		<h2>2nd AAAC Annual: From “Star Star” to Avant Garde: Nine Artists from China</h2>
+		<p><span class="date">October 17 – November 14th</span>: Part One</p>
+		<p>This exhibition relates the artistic developments in China from 1979 to 1989 to the current works of ten artists who participated in these developments, all of whom are living outside of China today. A panel discussion was held on November 21 featuring panelists Sui K. Zhao, Zhang Jianjun, and Zhang Hongtu and moderator Alexandra Munroe. <i><a href="http://artasiamerica.org/documents/1835/127" target="_blank">Learn more</a>.</i></p>
 		<h6>Participating artists:</h6>
 		<ul class="list-columns">
 			<li>Fang Zhongming</li>
@@ -23,7 +24,8 @@ include '_inc/head.php';
 			<li>Xu Bing</li>
 			<li>Zhang Hongtu</li>
 		</ul>
-		<p><span class="date">November 21 – December 20 </span>: Part two</p>
+		<p><span class="date">November 21 – December 20 </span>: Part Two</p>
+		<p>Ling Ling was presented twice as a memorial to his untimely death in Times Square.</p>
 		<h6>Participating artists:</h6>
 		<ul class="list-columns">
 			<li>Chen Zhen</li>
@@ -35,7 +37,7 @@ include '_inc/head.php';
 
 		<h2>Four Asian American Artists</h2>
 		<p><span class="date">January 11 – February 16, 1992</span></p>
-		<p>Curated by Robert Lee for the Council for the Arts on the North Shore. Held at Glen Cove, NY.</p>
+		<p>This exhibition centers on the complexity of ethical concerns among Asian American artists. Artists’ experiences can quietly retain a privacy that elongates time, separating itself from the noise of the marketplace, and may lead them to complex forms, with overlapping considerations, both ethical and paradoxical in tone. The paintings of Charles, Cyril, Tara, and Nguyen hold fast to this complexity of contemporary experience. In this they reflect an Asian American experience that is diverse, full, and very much another side of life and art in America. Curated by Robert Lee for the Council for the Arts on the North Shore. Held at Glen Cove, NY.</p>
 		<h6>Participating artists:</h6>
 		<ul class="list-columns">
 			<li>Long Nguyen</li>
@@ -46,7 +48,7 @@ include '_inc/head.php';
 
 		<h2>Collection of Buddhas</h2>
 		<p><span class="date">March 3 – extended to April 30, 1992</span></p>
-		<p>One person show.</p>
+		<p>A one-person exhibition showcasing the work of Ik-Joong Kang. The exhibition consists of 1,500 paintings on four square structures, each three-inch square and bearing an image of Buddha, some more unusual that others. The installation includes small, hidden-from-view loudspeakers of Buddhist monks chanting, enhancing the exhibit’s feeling of a Buddhist temple. Designed specifically for AAAC gallery space with dance company mirrors. Curarted by Robert Lee and Bing Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/1847/26" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/1850/26" target="_blank">press release</a>.</i></p>
 		<h6>Participating artists:</h6>
 		<ul class="list-columns">
 			<li>Ik-Joong Kang</li>
@@ -54,10 +56,89 @@ include '_inc/head.php';
 
 		<h2>RE... To Be Continued</h2>
 		<p><span class="date">June 12 - 13, 1992</span></p>
-		<p>Multimedia performance held at CSC Theatre, 136 E 13th St in New York, NY. Music and sound by Ari Frankel.</p>
+		<p>Multimedia performance held at CSC Theatre, 136 E 13th St in New York, NY. Music and sound by Ari Frankel. <i><a href="http://www.artasiamerica.org/documents/6459" target="_blank">Learn more</a>.</i></p>
 		<h6>Participating artists:</h6>
 		<ul class="list-columns">
 			<li>Tai Dang</li>
+		</ul>
+
+		<h2>CHINA June 4 1989: Traveling Exhibition</h2>
+		<p><span class="date">June 4 – July 14, 1992</span> at Mexic-Arte in Austin, Texas</p>
+		<p><span class="date">August 24, 1992 - October 4, 1992</span> at the Cleveland Institute of Art in Cleveland, Ohio</p>
+		<p><i>Learn more: <a href="http://www.artspiral.org/june4.php" target="_blank">Artspiral</a>, <a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157645294053044" target="_blank">photo album</a>.</i></p>
+		<h6>Participating artists:</h6>
+		<ul class="list-columns">
+				<li>Aprile, Thomas R.</li>
+				<li>Albair, Stephen J.</li>
+				<li>Allen, John Amoros, Grimanesa </li>
+				<li>Azaceta, Luis Cruz </li>
+				<li>Bahc, Mo </li>
+				<li>Baron, Ronald </li>
+				<li>Beaumont, Betty </li>
+				<li>Bowman, Daniel </li>
+				<li>Boza, Juan </li>
+				<li>Briere, Manon </li>
+				<li>Byler, James Jay</li>
+				<li>Camnitzer, Luis </li>
+				<li>Chin, Mel & Concoran School of Art </li>
+				<li>Chu, Julia Nee/Ca</li>
+				<li>Clark, Scott Klee </li>
+				<li>Colin, Muriel </li>
+				<li>Cortes, Esperanz </li>
+				<li>Decreeft, Donna Maria </li>
+				<li>Denes, Agnes </li>
+				<li>Diao, David </li>
+				<li>Dimon, Roz </li>
+				<li>Do, Lotus </li>
+				<li>Duff, John </li>
+				<li>Fowler, Paul M. Fu, Woody </li>
+				<li>Golub, Leon </li>
+				<li>Gonzalez, Maria Elena </li>
+				<li>Heap of Birds, Edgar </li>
+				<li>Hill, Robin Hoop </li>
+				<li>Hughes, Holly </li>
+				<li>Hung, Kuan - Chak </li>
+				<li>Hung, Ying S. </li>
+				<li>Ishino, Cathy </li>
+				<li>Izuka, Kunio </li>
+				<li>Kang, Ik-Joong </li>
+				<li>Kim, Byron </li>
+				<li>Koorland, Vivienne </li>
+				<li>Kruger, Barbara </li>
+				<li>Kuo, Anna </li>
+				<li>Laughin, Ian </li>
+				<li>Lascari, Anna </li>
+				<li>Lee, Bing </li>
+				<li>Lee, Gerald Ling Ling (Billy Harlem) </li>
+				<li>Lipski, Donald </li>
+				<li>Lum, Mary </li>
+				<li>Mei, Dean-I </li>
+				<li>Morr, Erik </li>
+				<li>Msika, Jean</li>
+				<li>Mur-Ray, Ming</li>
+				<li>Nabeska, Eva </li>
+				<li>Nemec, Vernita </li>
+				<li>Oshima, Mari </li>
+				<li>Palaia, Franc </li>
+				<li>Parcher, Joyce </li>
+				<li>Porter, Liliana </li>
+				<li>Rodrigo, Pascal </li>
+				<li>Red Spot (J. Allen Daugherty) </li>
+				<li>Ruhren, Andrew </li>
+				<li>Schimdt, James T. </li>
+				<li>Shih, Nora Ling-Yun </li>
+				<li>Shoemaker, Pamela </li>
+				<li>Sivilli, Valerie </li>
+				<li>Tacla, Jorge </li>
+				<li>Tsuda, Rumiko & Georges, Daniel </li>
+				<li>Hei Han Khiang</li>
+				<li>Watkins, Tim </li>
+				<li>White, Peter </li>
+				<li>Wong, Martin </li>
+				<li>Young, Laura </li>
+				<li>Yu, Sun Ching </li>
+				<li>Zezmer, Sofia </li>
+				<li>Zhang, Hongtu</li>
 		</ul>
 	</main>
 	<div class="fade"></div>
