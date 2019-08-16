e9ed5d274ee325cf8b6b5207c1777b55bbe530da
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:03:01 -0400

    1993-1994 update

--- 1993-1994.php
+++ 1993-1994.php
@@ -15,6 +15,7 @@ include '_inc/head.php';
 		<span class="tag">1993 &ndash; 1994</span>
 			<h2>Three: Works by 1992-1993 Artists-in-Residence</h2>
 			<p><span class="date">October 1 – November 1, 1993</span></p>
+			<p>This exhibition is a culmination of the three artists’ yearlong research projects on cultural diversity issues at the Asian American Arts Centre. Reflective of their own cultural background and drawing experience from their different heritages, these three artists have incorporated in their artworks a new perspective on contemporary art and demonstrate a unique artistic expression. <i>Learn more: <a href="http://artasiamerica.org/documents/2086" target="_blank">press release</a>, <a href="http://artasiamerica.org/artist/detail/34" target="_blank">artist profile</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Elaine Tin Nyo</li>
@@ -24,8 +25,7 @@ include '_inc/head.php';
 
 			<h2>Milieu Part I</h2>
 			<p><span class="date">November 19 – December 29, 1993</span></p>
-      <p>The first of a three-part exhibition exploring Asian American artists and their social environments from the years 1945 to 1965. Based on the research project begun in 1986 entitled "Asian American Artists and Their Milieu 1945 – 1965."</p>
-      <p>Curated by Robert Lee.</p>
+      <p>In the first of several exhibitions, Milieu: Part I brings together three senior Asian American artists who were active in the two decades of the post World War II era. Based on a national research project begun in the mid 80s entitled, “Asian American Artists and their Milieu 1945-1965,” which documented nearly ninety artists and interviewed many. This exhibition aims to bring to light cultural and artistic cross currents and to trace the complex story of Asian American artists. Each of the artists in the Milieu exhibition series shares a participation in the historical experience of Asians in America. We seek to document the existence of these artists, the predecessors to the young Asian American artists of today. Curated by Robert Lee. <i><a href="http://artasiamerica.org/documents/2185/193" target="_blank">Learn more</a></i>.</p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Bernice Bing</li>
@@ -33,45 +33,49 @@ include '_inc/head.php';
         <li>Ted Kurahara</li>
 			</ul>
 
-      <h2>Betrayal/Empowerment Part I</h2>
+      <h2>Betrayal/Empowerment Part II</h2>
 			<p><span class="date">March 18 – April 30, 1994</span></p>
-			<p>Held at AAAC.</p>
+			<p>Panel discussion held on April 7. Held at AAAC. <i><a href="http://artasiamerica.org/documents/6150/109" target="_blank">Learn more</a></i>.
+			<h6>Panelists:</h6>
+			<ul class="list-columns">
+				<li>Diyan Achadi</li>
+				<li>Monica Chau</li>
+				<li>Tony J. H. Cho</li>
+				<li>Madeline Fan</li>
+				<li>Franky Kong</li>
+				<li>Anjum Singh</li>
+			</ul>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Diyan Achjadi</li>
-        <li>Monica Chau</li>
-        <li>Tony J.H. Cho</li>
-        <li>Anujan Exhikode</li>
-        <li>Madeline Fan</li>
-        <li>Hanako Hanafusa</li>
-        <li>Katherine Hu Fan</li>
-        <li>Sui Ying Hung </li>
-        <li>Jung Hyang Kim	Franky Kong</li>
-        <li>Robert E. Reynolds</li>
-        <li>Suzanne K. Saylor</li>
-        <li>Anjum Singh</li>
+				<li>Monica Chau</li>
+				<li>Tony J.H. Cho</li>
+				<li>Anujan Exhikode</li>
+				<li>Madeline Fan</li>
+				<li>Hanako Hanafusa</li>
+				<li>Katherine Hu Fan</li>
+				<li>Sui Ying Hung </li>
+				<li>Jung Hyang Kim</li>
+				<li>Franky Kong</li>
+				<li>Robert E. Reynolds</li>
+				<li>Suzanne K. Saylor</li>
+				<li>Anjum Singh</li>
 			</ul>
 
-      <h2>Betrayal/Empowerment Part II</h2>
-			<p><span class="date">April 18 – May 4, 1994</span></p>
-      <p>extended to <span class="date">June 10, 1994</span></p>
-			<p>Now that diverse peoples have embarked on the path of empowerment, it is appropriate to ask, what will restore our humanity beyond empowerment?  If we have access to the race for power and, perhaps, to win it, is this clearly our aim or is the race for power itself limited, even flawed?   In seeking a humanity beyond empowerment, artists from each generation can recall for us cultural memories that, as Amalia Mesa Bains has said,  "....allows us to assert our sense of continuity against all odds".</p>
-      <p>Exhibition of works by artists who began their careers in the 1950s, 60s, 70s, and 80s. Held at and in collaboration with Columbia University Teacher's College as part of a two-day conference on Asian American issues.</p>
-      <p>Curated by Robert Lee.</p>
+      <h2>Betrayal/Empowerment Part I</h2>
+			<p><span class="date">April 18 – May 4, extended to June 10, 1994</span></p>
+			<p>Exhibition of artists who began their careers in the 1950's, the 1960's, the 1970's, and the 1980's, some artists from Milieu included. Now that diverse peoples have embarked on the path of empowerment, it is appropriate to ask, what will restore our humanity beyond empowerment?  If we have access to the race for power and, perhaps, to win it, is this clearly our aim or is the race for power itself limited, even flawed? In seeking a humanity beyond empowerment, artists from each generation can recall for us cultural memories that, as Amalia Mesa Bains has said, "...allows us to assert our sense of continuity against all odds.” Held at Teacher’s College. Collaboration with Teacher’s College, Columbia University. Event included two-day conference. <i>Learn more: <a href="http://artasiamerica.org/documents/1623/109" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/6150/109" target="_blank">press release</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
-				<li>Diyan Achjadi</li>
-        <li>Monica Chau</li>
-        <li>Tony J.H. Cho</li>
-        <li>Anujan Exhikode</li>
-        <li>Madeline Fan</li>
-        <li>Hanako Hanafusa</li>
-        <li>Katherine Hu Fan</li>
-        <li>Sui Ying Hung </li>
-        <li>Jung Hyang Kim	Franky Kong</li>
-        <li>Robert E. Reynolds</li>
-        <li>Suzanne K. Saylor</li>
-        <li>Anjum Singh</li>
+				<li>Arun Bose</li>
+        <li>Joseph Goto</li>
+        <li>Sang Nam Lee</li>
+        <li>Quynh Nguyen</li>
+        <li>Lily Yeh</li>
+        <li>Junko Yoda</li>
+        <li>Toshihisa Yoda</li>
+        <li>Charles Yuen </li>
+        <li>Sui Kang Zhao</li>
 			</ul>
     </main>
 	<div class="fade"></div>
