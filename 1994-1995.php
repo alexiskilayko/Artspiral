51794b51d2b93577c5275b2c78adeae61304c90f
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:03:18 -0400

    1994-1995 update

--- 1994-1995.php
+++ 1994-1995.php
@@ -7,202 +7,196 @@ include '_inc/head.php';
 
 <body class="exhibitions">
 <div class="page-wrapper">
-	<?php include '_inc/header.php'; ?>
-	<nav id="menu" role="navigation">
-	<?php include '_inc/menu.php';?>
-	</nav>
-	<main class="content-wrapper narrow" role="main">
-		<span class="tag">1994 &ndash; 1995</span>
-			<h2>CHINA: June 4 1989</h2>
-			<p><span class="date">October 14 – November 30, 1994</span>
-      <p>The final stage of a traveling exhibition. Held at Buckham Gallery in Flint, Michigan.</p>
-      <p>This was an art exhibition organized by the Asian American Arts Centre marking the massacre in Tiananmen Square, Beijing, on June 4, 1989. The exhibition was jointly curated by Lau Gin Wei (Hong Kong), Robert Lee, director of the Asian American Arts Centre, and John Yau, poet & art critic. This exhibition has traveled from the Asian American Arts Centre in New York to the Institute for Contemporary Art P. S. 1 Museum in Long Island City, the Hong Kong Arts Center, and to Buckham Gallery in Flint, Michigan. This exhibition was selected by Art in America as one of the most important exhibitions of the year.</p>
-      <p>This exhibition aims to educate the public about the Tiananmen Square massacre and the struggle for human rights. Hundreds of artists have joined together to create this momentous symbol in memory of Tiananmen Square. Five years after Tiananmen Square and less than a year after the World Conference on Human Rights in Vienna, the terror of Tiananmen Square has receded in time. The memory of Tiananmen Square, however, has not been forgotten. This exhibition of the art that was forged in its aftermath continues to keep that memory alive. Tiananmen Square and the terror of what a government can do to its own people is a potent reminder.</p>
-      <p>The symbolic core of the exhibition is a wall of free standing doors, painted and sculpted, which are linked together, enabling additional artists to join in shaping the form of this wall. Linked freestanding doors express and symbolize hope for the future of China through collaboration and participation.</p>
-      <p>The exhibition also called to attention the state of civil liberties in America when it was censored by the Senate Select Committee on Ethics in June 1990 when it traveled to a Congressional Rotunda. "CHINA - June 4, 1989..." educates the public about Tiananmen Square, about the story of the people who fought for freedom and democracy, about the threats to fundamental rights in the United States.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Aprile, Thomas R. </li>
-        <li>Albair, Stephen J.</li>
-        <li>Allen, John</li>
-        <li>Amoros, Grimanesa</li>
-        <li>Azaceta, Luis Cruz</li>
-        <li>Bahc, Mo</li>
-        <li>Baron, Ronald</li>
-        <li>Beaumont, Betty</li>
-        <li>Bowman</li>
-        <li>Daniel Boza</li>
-        <li>Juan Briere</li>
-        <li>Manon Byler</li>
-        <li>James Jay</li>
-        <li>Camnitzer, Luis</li>
-        <li>Chin, Mel & Concoran</li>
-        <li>School of Art </li>
-        <li>Chu, Julia Nee </li>
-        <li>Clark, Scott Klee</li>
-        <li>Colin, Muriel</li>
-        <li>Cortes, Esperanz </li>
-        <li>Decreeft, Donna Maria</li>
-        <li>Denes, Agnes</li>
-        <li>Diao, David</li>
-        <li>Dimon, Roz </li>
-        <li>Do, Lotus </li>
-        <li>Duff, John</li>
-        <li>Fowler, Paul M.</li>
-        <li>Fu, Woody</li>
-        <li>Golub, Leon </li>
-        <li>Gonzalez, Maria Elena </li>
-        <li>Heap of Birds, Edgar</li>
-        <li>Hill, Robin </li>
-        <li>Hoop</li>
-        <li>Hughes, Holly</li>
-        <li>Hung, Kuan - Chak</li>
-        <li>Hung, Ying S.</li>
-        <li>Ishino, Cathy</li>
-        <li>Izuka, Kunio </li>
-        <li>Kang, Ik-Joong</li>
-        <li>Kim, Byron </li>
-        <li>Koorland, Vivienne</li>
-        <li>Kruger, Barbara </li>
-        <li>Kuo, Anna</li>
-        <li>Laughin, Ian </li>
-        <li>Lascari, Anna</li>
-        <li>Lee, Bing </li>
-        <li>Lee, Gerald </li>
-        <li>Ling Ling (Billy Harlem)</li>
-        <li>Lipski, Donald</li>
-        <li>Lum, Mary</li>
-        <li>Mei, Dean-I</li>
-        <li>Morr, Erik</li>
-        <li>Msika, Jean	Mur-Ray, Ming</li>
-        <li>Nabeska, Eva</li>
-        <li>Nemec, Vernita</li>
-        <li>Oshima, Mari </li>
-        <li>Palaia, Franc </li>
-        <li>Parcher, Joyce </li>
-        <li>Porter, Liliana </li>
-        <li>Rodrigo, Pascal </li>
-        <li>Red Spot (J. Allen Daugherty) </li>
-        <li>Ruhren, Andrew</li>
-        <li>Schimdt, James T. </li>
-        <li>Shih, Nora Ling-Yun </li>
-        <li>Shoemaker, Pamela</li>
-        <li>Sivilli, Valerie </li>
-        <li>Tacla, Jorge </li>
-        <li>Tsuda, Rumiko & Georges, Daniel </li>
-        <li>Hei Han Khiang</li>
-        <li>Watkins, Tim </li>
-        <li>White, Peter </li>
-        <li>Wong, Martin</li>
-        <li>Young, Laura</li>
-        <li>Yu, Sun Ching</li>
-        <li>Zezmer, Sofia </li>
-        <li>Zhang, Hongtu</li>
-			</ul>
-      <h6>Additional artists from Flint:</h6>
-      <ul class="list-columns">
-        <li>Chris Waters </li>
-        <li>Kay Goodall </li>
-        <li>Connie Daros </li>
-        <li>Joseph Mihalic</li>
-        <li>Michelle Reidling </li>
-        <li>Kathryn Pekarek</li>
-        <li>Mike Lily</li>
-        <li>Meghan Carbary </li>
-        <li>Debra Grissom </li>
-        <li>Kathryn Gehrs-Pahl </li>
-        <li>Cheryl Dyelle </li>
-        <li>Laura Cloud's sculpture students at Michigan State University</li>
-        <li>Gary Gebhardt</li>
-        <li>James Lawton</li>
-        <li>Laura Cloud </li>
-        <li>Anonymous student </li>
-        <li>John Dempsey</li>
-        <li>Cathy Smith</li>
-      </ul>
+    <?php include '_inc/header.php'; ?>
+    <nav id="menu" role="navigation">
+    <?php include '_inc/menu.php';?>
+    </nav>
+    <main class="content-wrapper narrow" role="main">
+        <span class="tag">1994 &ndash; 1995</span>
+          <h2>Three: Works by Artists-in-Residence</h2>
+          <p><span class="date">September 29 – November 11, 1994</span></p>
+          <h6>Participating artists:</h6>
+          <ul class="list-columns">
+            <li>Debi Ray-Chaudhuri </li>
+            <li>Sowon Kwon</li>
+            <li>Sikay Tang </li>
+          </ul>
 
-			<h2>Three: Works by Artists-in-Residence</h2>
-      <p><span class="date">September 29 – November 11, 1994</span></p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Debi Ray-Chaudhuri </li>
-				<li>Sowon Kwon</li>
-				<li>Sikay Tang </li>
-			</ul>
+          <h2>CHINA June 4 1989: Traveling Exhibition</h2>
+          <p><span class="date">October 14 – November 30, 1994</span>
+          <p>Held at Buckham Gallery in Flint, Michigan.</p>
+          <h6>Participating artists:</h6>
+          <ul class="list-columns">
+            <li>Aprile, Thomas R. </li>
+            <li>Albair, Stephen J.</li>
+            <li>Allen, John</li>
+            <li>Amoros, Grimanesa</li>
+            <li>Azaceta, Luis Cruz</li>
+            <li>Bahc, Mo</li>
+            <li>Baron, Ronald</li>
+            <li>Beaumont, Betty</li>
+            <li>Bowman</li>
+            <li>Daniel Boza</li>
+            <li>Juan Briere</li>
+            <li>Manon Byler</li>
+            <li>James Jay</li>
+            <li>Camnitzer, Luis</li>
+            <li>Chin, Mel & Concoran</li>
+            <li>School of Art </li>
+            <li>Chu, Julia Nee </li>
+            <li>Clark, Scott Klee</li>
+            <li>Colin, Muriel</li>
+            <li>Cortes, Esperanz </li>
+            <li>Decreeft, Donna Maria</li>
+            <li>Denes, Agnes</li>
+            <li>Diao, David</li>
+            <li>Dimon, Roz </li>
+            <li>Do, Lotus </li>
+            <li>Duff, John</li>
+            <li>Fowler, Paul M.</li>
+            <li>Fu, Woody</li>
+            <li>Golub, Leon </li>
+            <li>Gonzalez, Maria Elena </li>
+            <li>Heap of Birds, Edgar</li>
+            <li>Hill, Robin </li>
+            <li>Hoop</li>
+            <li>Hughes, Holly</li>
+            <li>Hung, Kuan - Chak</li>
+            <li>Hung, Ying S.</li>
+            <li>Ishino, Cathy</li>
+            <li>Izuka, Kunio </li>
+            <li>Kang, Ik-Joong</li>
+            <li>Kim, Byron </li>
+            <li>Koorland, Vivienne</li>
+            <li>Kruger, Barbara </li>
+            <li>Kuo, Anna</li>
+            <li>Laughin, Ian </li>
+            <li>Lascari, Anna</li>
+            <li>Lee, Bing </li>
+            <li>Lee, Gerald </li>
+            <li>Ling Ling (Billy Harlem)</li>
+            <li>Lipski, Donald</li>
+            <li>Lum, Mary</li>
+            <li>Mei, Dean-I</li>
+            <li>Morr, Erik</li>
+            <li>Msika, Jean Mur-Ray, Ming</li>
+            <li>Nabeska, Eva</li>
+            <li>Nemec, Vernita</li>
+            <li>Oshima, Mari </li>
+            <li>Palaia, Franc </li>
+            <li>Parcher, Joyce </li>
+            <li>Porter, Liliana </li>
+            <li>Rodrigo, Pascal </li>
+            <li>Red Spot (J. Allen Daugherty) </li>
+            <li>Ruhren, Andrew</li>
+            <li>Schimdt, James T. </li>
+            <li>Shih, Nora Ling-Yun </li>
+            <li>Shoemaker, Pamela</li>
+            <li>Sivilli, Valerie </li>
+            <li>Tacla, Jorge </li>
+            <li>Tsuda, Rumiko & Georges, Daniel </li>
+            <li>Hei Han Khiang</li>
+            <li>Watkins, Tim </li>
+            <li>White, Peter </li>
+            <li>Wong, Martin</li>
+            <li>Young, Laura</li>
+            <li>Yu, Sun Ching</li>
+            <li>Zezmer, Sofia </li>
+            <li>Zhang, Hongtu</li>
+          </ul>
+          <h6>Additional artists from Flint:</h6>
+          <ul class="list-columns">
+            <li>Chris Waters </li>
+            <li>Kay Goodall </li>
+            <li>Connie Daros </li>
+            <li>Joseph Mihalic</li>
+            <li>Michelle Reidling </li>
+            <li>Kathryn Pekarek</li>
+            <li>Mike Lily</li>
+            <li>Meghan Carbary </li>
+            <li>Debra Grissom </li>
+            <li>Kathryn Gehrs-Pahl </li>
+            <li>Cheryl Dyelle </li>
+            <li>Laura Cloud's sculpture students at Michigan State University</li>
+            <li>Gary Gebhardt</li>
+            <li>James Lawton</li>
+            <li>Laura Cloud </li>
+            <li>Anonymous student </li>
+            <li>John Dempsey</li>
+            <li>Cathy Smith</li>
+          </ul>
 
-			<h2>Photography and Community: 4th AAAC Annual Exhibition</h2>
-			<p><span class="date">November 17 – December 31, 1994</span></p>
-			<p>Curated by Robert Lee.
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Mei-Ling Hom</li>
-				<li>Corky Lee</li>
-				<li>Wing Young Huie</li>
-			</ul>
+          <h2>4th AAAC Annual: Photography and Community</h2>
+          <p><span class="date">November 17 – December 31, 1994</span></p>
+          <p>These three photographers exemplify diverse approaches to community. A glimpse at these works in progress informs the current dialogue on identity and community. In her project “Picturing Asian American,” Mei-ling Hom distributed single-use cameras to one hundred individuals and invited them to record images that describe their Asian American identity, resulting in a panorama of Asian American life. Corky Lee has cultivated a multi-decade long career of recording the development of the Asian American community, having captured landmark moments with amicable reality. Wing Young Huie’s works capture the lives of residents in Frogtown, a once mostly white, working class neighborhood, now transformed into a multi-ethnic inner-city neighborhood.  Curated by Robert Lee. <i><a href="http://artasiamerica.org/documents/1797/200" target="_blank">Learn more.</a></i></p>
+          <h6>Participating artists:</h6>
+          <ul class="list-columns">
+            <li>Mei-Ling Hom</li>
+            <li>Corky Lee</li>
+            <li>Wing Young Huie</li>
+          </ul>
 
-			<h2>Chinese/Korean Calligraphy</h2>
-			<p><span class="date">February 9 – March 4, 1995</span></p>
-			<p>An exhibition in which local Chinatown community business leaders who practice calligraphy/ink painting were paired with two visiting contemporary calligraphers from South Korea. Organized by Dr. Sun Wuk Kim & George Young.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Tsui Jin Yuk </li>
-        <li>S.F. Chu </li>
-        <li>You ng Chu</li>
-        <li>S.Y. Huang </li>
-        <li>Sun Wuk Kim</li>
-        <li>Young Ae Lee</li>
-        <li>Kong W. Mui </li>
-        <li>M.F. Ng </li>
-        <li>Young Jin Park</li>
-        <li>H.S. Sha </li>
-        <li>C.L. Ting </li>
-        <li>George Young</li>
-        <li>Wang Fong Yu </li>
-        <li>Jeanette Hsu</li>
-			</ul>
+          <h2>Chinese/Korean Calligraphy</h2>
+          <p><span class="date">February 9 – March 4, 1995</span></p>
+          <p>An exhibition in which local Chinatown community business leaders who practice calligraphy/ink painting were paired with two visiting contemporary calligraphers from South Korea. Organized by Dr. Sun Wuk Kim & George Young.</p>
+          <h6>Participating artists:</h6>
+          <ul class="list-columns">
+            <li>Tsui Jin Yuk </li>
+            <li>S.F. Chu </li>
+            <li>You ng Chu</li>
+            <li>S.Y. Huang </li>
+            <li>Sun Wuk Kim</li>
+            <li>Young Ae Lee</li>
+            <li>Kong W. Mui </li>
+            <li>M.F. Ng </li>
+            <li>Young Jin Park</li>
+            <li>H.S. Sha </li>
+            <li>C.L. Ting </li>
+            <li>George Young</li>
+            <li>Wang Fong Yu </li>
+            <li>Jeanette Hsu</li>
+          </ul>
 
-      <h2>Ancestors: a Collaborative Project with Kenkeleba House</h2>
-      <p><span class="date">March – May 1995</span></p>
-      <p>This exhibition examines the historical ties between African and Asian Americans. Given America is a land of diverse people, "...we recognize the wealth of our heritage as Americans and encourage the act of paying homage to all the Ancestors of this Land.”</p>
-      <p>Mounted at Kenkeleba House 214 East 2nd Street and at AAAC.</p>
-      <p>Organized by Corrine Jennings & Robert Lee.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Camille Billops</li>
-        <li>Robert Craddock</li>
-        <li>Helen Oji</li>
-        <li>Roy Hiro Calloway</li>
-        <li>David Higginbotham</li>
-        <li>Yoland Skeete </li>
-        <li>Simone Leigh</li>
-        <li>Toshinori Kuga</li>
-        <li>Anton Wong</li>
-        <li>Sana Musasama</li>
-        <li>Lisa K. Yi</li>
-        <li>Lily Yeh CA</li>
-        <li>Howardena Pindell</li>
-        <li>Faith Ringgold</li>
-        <li>Eunju Kang</li>
-        <li>Thomas Vu Daniel</li>
-        <li>Lotus Do Brooks</li>
-        <li>Charles Burwell</li>
-        <li>Elaine Wong</li>
-        <li>Albert V. Chong</li>
-        <li>Hyon Joo Kim</li>
-        <li>Lui Lan Ding</li>
-        <li>Richard Mafong</li>
-        <li>Prestone Jackson</li>
-			</ul>
-			<h6>Musicians:</h6>
-			<ul class="list-columns">
-				<li>Butch Morris</li>
-				<li>Sumi Tonooka</li>
-			</ul>
-	</main>
-	<div class="fade"></div>
-	<nav id="mm" role="navigation">
-	<?php include '_inc/menu.php'; ?>
-	</nav>
+          <h2>Ancestors: a Collaborative Project with Kenkeleba House</h2>
+          <p><span class="date">March – May 1995</span></p>
+          <p>This public exhibition was designed to encourage the act of paying homage, not only to one’s own ancestors but to those of our neighbors, the ancestors of another people. It pays tribute to all of America’s forebearers, whether they be Asian, African, European, Latin American, or Native American. By focusing on two, Asian and African American ancestors, it recognizes that the rites and beliefs of diverse people permeate society as a whole and that acts of homage are fundamental to the development of the sense of community.  “…we recognize the wealth of our heritage as Americans and encourage the act of paying homage to all the Ancestors of this Land.” Organized by Corrine Jennings & Robert Lee. Mounted at Kenkeleba House 214 East 2nd Street and at AAAC. <i>Learn more: <a href="http://artasiamerica.org/documents/1710/93" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/1715/93" target="_blank">press release</a>.</i></p>
+          <h6>Participating artists:</h6>
+          <ul class="list-columns">
+            <li>Camille Billops</li>
+            <li>Robert Craddock</li>
+            <li>Helen Oji</li>
+            <li>Roy Hiro Calloway</li>
+            <li>David Higginbotham</li>
+            <li>Yoland Skeete </li>
+            <li>Simone Leigh</li>
+            <li>Toshinori Kuga</li>
+            <li>Anton Wong</li>
+            <li>Sana Musasama</li>
+            <li>Lisa K. Yi</li>
+            <li>Lily Yeh CA</li>
+            <li>Howardena Pindell</li>
+            <li>Faith Ringgold</li>
+            <li>Eunju Kang</li>
+            <li>Thomas Vu Daniel</li>
+            <li>Lotus Do Brooks</li>
+            <li>Charles Burwell</li>
+            <li>Elaine Wong</li>
+            <li>Albert V. Chong</li>
+            <li>Hyon Joo Kim</li>
+            <li>Lui Lan Ding</li>
+            <li>Richard Mafong</li>
+            <li>Prestone Jackson</li>
+          </ul>
+          <h6>Musicians:</h6>
+          <ul class="list-columns">
+            <li>Butch Morris</li>
+            <li>Sumi Tonooka</li>
+          </ul>
+    </main>
+    <div class="fade"></div>
+    <nav id="mm" role="navigation">
+    <?php include '_inc/menu.php'; ?>
+    </nav>
 </div>
 </body>
 </html>
