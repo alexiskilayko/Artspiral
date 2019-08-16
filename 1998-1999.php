da33e7b61dd329fc90ea6312657b1a430d6cbee0
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:05:51 -0400

    1998-1999 update

--- 1998-1999.php
+++ 1998-1999.php
@@ -7,26 +7,26 @@ include '_inc/head.php';
 
 <body class="exhibitions">
 <div class="page-wrapper">
-	<?php include '_inc/header.php'; ?>
-	<nav id="menu" role="navigation">
-	<?php include '_inc/menu.php';?>
-	</nav>
-	<main class="content-wrapper narrow" role="main">
-		<span class="tag">1998 &ndash; 1999</span>
-			<h2>A Continuing Struggle: The Legacy of War in Cambodia</h2>
+  <?php include '_inc/header.php'; ?>
+  <nav id="menu" role="navigation">
+  <?php include '_inc/menu.php';?>
+  </nav>
+  <main class="content-wrapper narrow" role="main">
+    <span class="tag">1998 &ndash; 1999</span>
+      <h2>A Continuing Struggle: The Legacy of War in Cambodia</h2>
       <p><span class="date">September 1 – 15, 1998</span></p>
-      <p>An exhibition of photographs of Leah Melnick's work as a college student documenting the Cambodian refugee community in the Bronx, and later, her humanitarian work in Bosnia. </p>
-      <h6>Participating artists:</h6>
+      <p>Photos by Leah Melnick: A Memorial Exhibition. An exhibition of photographs of Leah Melnick's work as a college student documenting the Cambodian refugee community in the Bronx, and later, her humanitarian work in Bosnia. <i>Learn more: <a href="http://artasiamerica.org/documents/2319/79" target="_blank">article</a>, <a href="http://artasiamerica.org/documents/2316/79" target="_blank">press release</a>.</i></p>
+      <h6>Participating artist:</h6>
       <ul class="list-columns">
         <li>Leah Melnick</li>
       </ul>
 
-			<h2>The Day and Night Transparent: 8th AAAC Annual Exhibition</h2>
-			<p><span class="date">October 16 – November 21, 1998</span></p>
-      <p>Organized by Young M. Park, Trinh Le, and Robert Lee.</p>
+      <h2>8th AAAC Annual: The Day and Night Transparent</h2>
+      <p><span class="date">October 16 – November 21, 1998</span></p>
+      <p>Eighth in a series, this exhibition introduces individuals whose search for an artistic and visual language is the only bond that joins them together. In a complex society with multiple contradictory interests at work, it is their creativity that sustains their growth. The exhibition as a whole serves as an introduction to the broad and diverse concerns of artists who together compose a part of the contemporary presence of Asia in America. Organized by Young M. Park, Trinh Le, and Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/2807/45" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2808/45" target="_blank">press release</a>.</i></p>
       <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Yang-Ah Ham</li>
+      <ul class="list-columns">
+        <li>Yang-Ah Ham</li>
         <li>Eiko Kijima</li>
         <li>Bong-Jung Kim</li>
         <li>Jin Seok Kim</li>
@@ -35,53 +35,60 @@ include '_inc/head.php';
         <li>Jung-Hoon Lee</li>
         <li>Eung Ho Park</li>
         <li>Tam Van Tran</li>
-	  	</ul>
+      </ul>
 
       <h2>Gulp and Gallop!</h2>
-			<p><span class="date">December 4, 1998 – January 16, 1999</span></p>
-      <p>An exhibition presenting two different artists, Ushio Shinohara and Rocky Kagoshima, and their works that reflect a similar stubborn awareness of their cultural complexity, an awareness that arose from their experience of global cultural conflict. Their works express chaos in values and in energy, as well as shed light on Asian American sensibility in a postmodern world. Guest curated by Hideki Kawahara.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Ushio Shinohara </li>
+      <p><span class="date">December 4, 1998 – January 16, 1999</span></p>
+      <p>An exhibition presenting two artists, Ushio Shinohara and Rocky Kagoshima, as they create their art with the stubbornness of an idea arising directly from their experience of global cultural conflict. Their works express chaos in values and in energy, as well as shed light on Asian American sensibility in a postmodern world. Curated by Hideki Kawahara. <i>Learn more: <a href="http://artasiamerica.org/documents/3960/168" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/3961/168" target="_blank">press release</a>.</i></p>
+      <h6>Participating artists:</h6>
+      <ul class="list-columns">
+        <li>Ushio Shinohara </li>
         <li>Rocky Kagoshima</li>
-			</ul>
+      </ul>
 
       <h2>7lb9oz: The Reintegration of Tradition into Contemporary Art</h2>
-			<p><span class="date">March 26, 1999 – May 8, 1999</span></p>
-      <p>Exhibition showing four artists for whom the continuity of tradition is foremost. Curated by Robert Lee.</p>
+      <p><span class="date">March 26 – May 8, 1999</span></p>
+      <p>An exhibition of four artists who each represent different approaches but are similar in that they all share Asia as their point of departure. They demonstrate a confidence embracing their Asian origins and traditions in reconciling their conundrum of identity, and in doing so, bridging in their way two seemingly irreconcilable cultures. Curated by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/5161/177" target="_blank">flyer<a>, <a href="http://artasiamerica.org/documents/5160/28" target="_blank">press release</a>.</i></p>
       <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Chee Wang Ng</li>
+      <ul class="list-columns">
+        <li>Chee Wang Ng</li>
         <li>Hisako Hibi</li>
         <li>Yeong Gill Kim</li>
         <li>Osamu Tanaka</li>
-			</ul>
+      </ul>
 
       <h2>Six O'Clock Observed: Photos by Toyo Tsuchiya</h2>
-			<p><span class="date">May 21 – June  26, 1999</span></p>
-			<p>A mid career exhibition.</p>
-			<p>Panelists: Alan W. Moore, Linda Shapiro, Kazuko Miyamoto, Reiko Tomii, Toyo Tsuchiya</p>
-			<p>Nominating panelists: Vinod Dave, Ming Fay, Ti SHan Hsu, Kazuko Miyamoto Joon Lee, Tara Sabharwal, Reiko Tomii</p>
-			<h6>Participating artists:</h6>
+      <p><span class="date">May 21 – June  26, 1999</span></p>
+      <p> A mid-career exhibition. An artist’s talk was held on June 3 with panelists: Alan W. Moore, art historian writing on artists’ organization in NY 1970-80s; Linda Shapiro, long time resident of the LES owner of Shapiro Wine, 100 yrs old landmark; Kazuko Miyamoto, who came to the LES in the mid 60's; Reiko Tomii, art historian; and Toyo Tsuchiya. <i>Learn more: <a href="http://artasiamerica.org/documents/2157/31" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2156" target="_blank">press release</a>, <a href="https://www.nytimes.com/1999/06/18/arts/art-in-review-toyo-tsuchiya-six-o-clock-observed.html" target="_blank">New York Times</a>.</i></p>
+      <h6>Nominating panelists:</h6>
       <ul class="list-columns">
-				<li>Toyo Tsuchiya</li>
-			</ul>
+        <li>Vinod Dave</li>
+        <li>Ming Fay</li>
+        <li>Ti Shan Hsu</li>
+        <li>Kazuko Miyamoto</li>
+        <li>Joon Lee</li>
+        <li>Tara Sabharwal</li>
+        <li>Reiko Tomii</li>
+      </ul>
+      <h6>Participating artist:</h6>
+      <ul class="list-columns">
+        <li>Toyo Tsuchiya</li>
+      </ul>
 
-			<h2>EE! EE! EE! TRANSlation/plantation/formation</h2>
-			<p><span class="date">August 14, 1999</span></p>
-			<p>As ice melts a great white ball rises... A collaboration with a Chinese poet, an American poet, and a sculptor, with additional four women performers.  Collages of multi-layered, multi-faceted aspects in communcation, combine visual elements and three spoken languages – Chinese, Taiwanese & English – to foreground human desire as a process of translation, transplantation, and transformation.</p>
-			<p>A free sculptural performance held in Bryant Park, 42nd Street & 6th Ave in NYC. Sponsored by Contemporary Arts International in Boston. Administered by AAAC.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Meng Lang</li>
-				<li>Yin Peet</li>
-				<li>Leonard Schwarts</li>
-			</ul>
-	</main>
-	<div class="fade"></div>
-	<nav id="mm" role="navigation">
-	<?php include '_inc/menu.php'; ?>
-	</nav>
+      <h2>EE! EE! EE! TRANSlation/plantation/formation</h2>
+      <p><span class="date">August 14, 1999</span></p>
+      <p>A sculptural performance. As ice melts, a great white ball rises… A collaboration between a Chinese poet, an American poet, and a sculptor, with an additional four women performers.  Collages of multi-layered, multi-faceted aspects in communcation combine visual elements and three spoken languages – Chinese, Taiwanese & English – to foreground human desire as a process of translation, transplantation, and transformation. Sponsored by Contemporary Arts International in Boston, Administered by AAAC. A free performance held in Bryant Park 42ed Street & 6th Ave, NYC.</p>
+      <h6>Participating artists:</h6>
+      <ul class="list-columns">
+        <li>Meng Lang</li>
+        <li>Yin Peet</li>
+        <li>Leonard Schwarts</li>
+      </ul>
+  </main>
+  <div class="fade"></div>
+  <nav id="mm" role="navigation">
+  <?php include '_inc/menu.php'; ?>
+  </nav>
 </div>
 </body>
 </html>
