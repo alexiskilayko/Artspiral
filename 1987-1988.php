a674b523ae7b084044874a8cbaf4f9100429d910
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:00:37 -0400

    1987-1988 update

--- 1987-1988.php
+++ 1987-1988.php
@@ -15,7 +15,7 @@ include '_inc/head.php';
 		<span class="tag">1987 &ndash; 1988</span>
 			<h2>Confucius Life Story</h2>
 			<p><span class="date">September 1987</span></p>
-			<p>A mural installation of original rubbings. </p>
+			<p>A mural installation of original rubbings by Sun Jingbo, who captured scenes of Confucian times. The rubbings were huge and covered the entire walls of the gallery. Thanks to Zhang Hong tu, AAAC was kindly introduced to Jingbo while he was on an official trip from Beijing. </p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Sun Jingbo</li>
@@ -23,7 +23,7 @@ include '_inc/head.php';
 
 			<h2>One Eye or Two</h2>
 			<p><span class="date">December 4, 1987 – January 6, 1988</span></p>
-			<p>Curated by Robert Lee.</p>
+			<p>This exhibition brings together three Asian American artists with unique visions. Curator Robert Lee writes, “The enormous wit that Bing, E’wao, and Kwong Chi bring to the modern spectacle is matched only by their high seriousness. To miss their Asianness is to miss the depth and range of their good ‘humors.’” Essay by Barry Schwabsky. <i>Learn more: <a href="http://artasiamerica.org/documents/5932/196" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/1119/196" target="_blank">flyer</a>.</i> </p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Tseng Kwong Chi </li>
@@ -33,7 +33,7 @@ include '_inc/head.php';
 
 			<h2>From Cambodia to the Bronx</h2>
 			<p><span class="date">January – February 1988</span></p>
-			<p>An exhibit of photographs documenting the Cambodian refugee community in the Bronx. Curated by Robert Lee.</p>
+			<p>An exhibit of photographs documenting the Cambodian refugee community in the Bronx. When still in high school Leah Melnick befriended the community of boat people who survived and came to live nearby. She learned the Khmer language and became like family photographing their events and daily lives. After college she joined the staff at the United Nations. Organized by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/2315/79" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/2317/79" target="_blank">letter</a>, <a href="http://artasiamerica.org/documents/2319/79" target="_blank">article</a>. </i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Leah Melnick</li>
@@ -41,7 +41,7 @@ include '_inc/head.php';
 
 			<h2>Yesterday: Reflections on Childhood</h2>
 			<p><span class="date">March – April, 1988</span></p>
-			<p>An exhibit of works that reflect the artists' unique perspectives as Asian American through experiences in their childhood. Catalogue essay by Kimiko Hahn.</p>
+			<p>An exhibit of works that reflected the artists’ unique perspectives as Asian American through experiences in their childhood. Catalogue essay by Kimiko Hahn. <i><a href="http://artasiamerica.org/documents/5635/191" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Yoshiki Araki</li>
@@ -88,8 +88,8 @@ include '_inc/head.php';
 			</ul>
 
 			<h2>Public Art in Chinatown</h2>
-			<p><span class="date">May 20 – July, 1988</span></p>
-			<p>A selection of sculptures, models, drawing and site plans proposed for specific locations in the Downtown/Chinatown community. Curated by Robert Lee, with catalogue essay by John Yau.</p>
+			<p><span class="date">May 20 – June 21, extended into July 1988</span></p>
+			<p>A selection of sculptures, models, drawing and site plans proposed for specific locations in the Downtown/Chinatown community. A burgeoning Asian community in Lower Manhattan needs a new public image, an art that can embody the promise of change in Asian American relations. Curated by Robert Lee. Catalogue essays by John Yau, Peter Kwong, Kyong Park, intro by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/8898/174" target="_blank">catalogue</a>, <a href="http://artasiamerica.org/documents/4900/174" target="_blank">proposal</a>, <a href="http://artasiamerica.org/documents/3238/174" target="_blank">press release</a>, <a href="http://artspiral.blogspot.com/2019/08/public-art-in-nyc-chinatown-gateways.html" target="_blank">blog</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Mikyung Kim</li>
@@ -110,7 +110,7 @@ include '_inc/head.php';
 
 			<h2>Artention International</h2>
 			<p><span class="date">July - August 1988</span></p>
-			<p>A bimonthly publication in Hong Kong, by Zie Yongder. Full color catalogue, "Public Art in Chinatown," with introduction by Robert Lee.</p>
+			<p>A bimonthly publication in Hong Kong by Zie Yongder. Public Art in Chinatown, full color catalogue with introduction by R. Lee. Pages 41 to 70, Public Art: A Reflection of Progress.</p>
 	</main>
 	<div class="fade"></div>
 	<nav role="navigation" id="mm">
