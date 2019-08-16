30bca1f69f72b4a2ee37422405372906e9c772df
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:11:30 -0400

    2018-2019 add and update

---
+++ 2018-2019.php
@@ -0,0 +1,26 @@
+<?php
+$title = '2018 &ndash; 2019';
+
+include '_inc/head.php';
+
+?>
+
+<body class="exhibitions">
+<div class="page-wrapper">
+	<?php include '_inc/header.php'; ?>
+	<nav role="navigation" id="menu">
+	<?php include '_inc/menu.php';?>
+	</nav>
+	<main role="main" class="content-wrapper narrow">
+		<span class="tag">2018 &ndash; 2019</span>
+      <h2>AAAC Assigns the CHINA: June 4, 1989 Exhibition to HC</h2>
+      <p><span class="date">March 6, 2019</span></p>
+      <p>AAAC, dedicated to promoting the presence of Asian American art in the United States since 1974, has joined together with Humanitarian China (HC), a non-profit organization incorporated in California by Tiananmen Square student leaders Fengsuo Zhou, Zhao Jing and Gang Xu. HC is dedicated to providing humanitarian care for political prisoners and Tiananmen Mothers in China, to preserving the history of China’s democracy movement, and to educating the general public.  With these goals in mind HC will carry on AAAC’s goal to exhibit, promote, educate and protect the precious artworks of the original CHINA: June 4, 1989 exhibition. HC will ensure that CHINA: June 4, 1989 will remain visible as key to this important historical movement and that the spirits of those who suffered because of this event shall never perish. AAAC assigns its care of the CHINA: June 4, 1989 exhibition to HC so that the purpose and goals of the aforesaid effort will continue. <i><a href="http://artspiral.blogspot.com/2019/07/humanitarian-china-tiananmen-square.html" target="_blank">Learn more</a></i>.</p>
+    </main>
+  	<div class="fade"></div>
+  	<nav role="navigation" id="mm">
+  	<?php include '_inc/menu.php';?>
+  	</nav>
+  </div>
+  </body>
+  </html>
