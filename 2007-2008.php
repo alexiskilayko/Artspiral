bc1fd32ba9fa5e1559d046c8985cf4e3a3c62d7e
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:09:04 -0400

    2007-2008 update

--- 2007-2008.php
+++ 2007-2008.php
@@ -15,7 +15,7 @@ include '_inc/head.php';
 		<span class="tag">2007 &ndash; 2008</span>
 			<h2>Fibers: Fiero, Kotani & Kim</h2>
 			<p><span class="date">October 11 – December 7, 2007</span></p>
-			<p>Curated by Robert Lee. The three artists were selected for their innovative use of fibers, which are physical yet convey an intangible touch of the lightness of air.</p>
+			<p>Innovative approaches to common fibers by three artists—one Asian, the other Asian American, and the last an American significantly influenced by Asia.  Fibers in both two and three dimensions are physical yet convey an intangible touch of the lightness of air.  In so doing they integrate both Asian and Western sensibilities. Organized by Robert Lee. <i>Learn more: <a href="http://artasiamerica.org/documents/7583/498" target="_blank">postcard</a>, <a href="http://artasiamerica.org/artist/detail/498" target="_blank">artist profile</a>, <a href="http://artasiamerica.org/documents/7072/189" target="_blank">artist statement</a>.</i></p>
 			<img src="/_images/fibers.jpg" alt="Poster for the Fibers exhibition">
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
@@ -24,17 +24,17 @@ include '_inc/head.php';
 				<li>Soon-Im Kim</li>
 			</ul>
 
-			<h2>boundLES: Celebrating Contemporary Art on the Lower East Side</h2>
+			<h2>boundLES</h2>
 			<p><span class="date">November 27, 2007 - January 13, 2008</span></p>
-			<p>A collaborative of exhibition venues bringing together commercial galleries, nonprofit arts institutions, and artists in celebration of the rich creative spirit of the Lower East Side.  Forty one participating organizations mount events coinciding with the December 1 opening of the New Museum of Contemporary Art on the Bowery at Prince Street.</p>
+			<p>Celebrating Contemporary Art On The Lower East Side. A collaborative of exhibition venues bringing together commercial galleries, nonprofit arts institutions, and artists in celebration of the rich creative spirit of the Lower East Side.  Forty-one participating organizations mounted events coinciding with the December 1 opening of the New Museum of Contemporary Art on the Bowery at Prince Street.</p>
 
 			<h2>Rare Tribal Textiles from China</h2>
 			<p><span class="date">December 20, 2007 – February 29, 2008</span></p>
-			<p>An exhibition of Chinese Tribal Textiles from the Minority Peoples of China held alongside the Fibers exhibition. The exhibition included museum quality fabrics representative of historical tribal traditions. They are from the peoples known as: Miao, Gejia,Yao, Yi, Dai, Li, and Zhang, and include garments, banners, festival jackets and blankets.  The collector of these tribal arts and textiles, Dr. Andrew Wang, gave a talk on this exquisite display of pattern and skill, the highest embroidery quality of China. </p>
+			<p>An exhibition of Chinese Tribal Textiles from the Minority Peoples of China will be held through the Chistmas Holidays and Chinese Lunar New Year as well. These are museum quality fabrics representative of historical tribal traditions. They are from the peoples known as: Miao, Gejia, Yao, Yi, Dai, Li, and Zhang.  Many more garments, banners, festival jackets and blankets have filled the gallery, from Dai, Hainan as well as Gejia and Miao.  The collector of these tribal arts and textiles, Dr. Andrew Wang, spoke about this exquisite display of pattern and skill—the highest embroidery quality of China.</p>
 
 			<h2>Yeong Gill Kim</h2>
 			<p><span class="date">July 11 – August 1, 2008</span></p>
-			<p>Yeong Gill Kim is a Korean American artist who has lived in NY for many years. He saw the transition in Korea from a feudal society to a modern country and the confusion it brought to the people. By 1995 he had given up purely contemporary idioms and returned to materials and ideas that had inspired so much of Korea's artistic tradition. Inflected by modernity in the uniformity of the color of the strokes, by the washed surface, his gesture to contemporary times is there, and to that extent, to the West. The complexity of his brush work on muslin has attained a breathtaking clarity and simplicity.</p>
+			<p>The complexity of Yeong Gill Kim’s brush work on muslin has attained a breathtaking clarity and simplicity.  He shows that contemporary art in America need not be founded on a break with the past, nor on claims of a new age of global technology.  He demonstrates that a crisis of identity, a seemingly unbridgeable gap born of two irreconcilable cultures, can be spanned with a renewed confidence in the vitality of one's Asian traditions. <i>Learn more: <a href="http://artasiamerica.org/documents/5159/28" target="_blank">poster</a>, <a href="http://artasiamerica.org/documents/5158/28" target="_blank">press release</a>, <a href="http://artasiamerica.org/documents/5157/28" target="_blank">artist statement</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Yeong Gill Kim</li>
@@ -42,21 +42,10 @@ include '_inc/head.php';
 
 			<h2>Marie Yoho Dorsey</h2>
 			<p><span class="date">August 8 – 29, 2008</span></p>
-			<p>An artist trained in the Japanese art of Ikebana, Marie Yoho Dorsey has turned her skilled mind to the art of painting, collage and installation to create delicate and masterful renderings on fabric, mystical journeys through landscapes and clouds despite the noise of the cities of New York and Florida.</p>
+			<p>An artist well trained in the Japanese art of Ikebana, Dorsey has turned her skilled mind to the art of painting, collage, and installation. Her creations include delicate and masterful renderings on fabric, as well as mystical journeys through landscapes and clouds despite the noise of the cities of New York and Florida. <i>Learn more: <a href="https://www.flickr.com/photos/asianamericanartscentre/3175528146/in/album-72157616235017832/" target='_blank'>artwork</a>, <a href="https://www.flickr.com/photos/asianamericanartscentre/3404657173/in/album-72157616235017832/" target='_blank'>photo</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Marie Yoho Dorsey</li>
-			</ul>
-
-			<h2>ARTSLAM</h2>
-			<p><span class="date">July 16, 2008</span></p>
-			<p>ARTSLAM is part of a series of events allowing emerging artists the opportunity to present their work, as well as meet and network with established artists and critics/curators. </p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Abha Dawesar</li>
-				<li>Vivian Wong</li>
-				<li>Kenji Kojima</li>
-				<li>Yasuko Shingu</li>
 			</ul>
 	</main>
 	<div class="fade"></div>
