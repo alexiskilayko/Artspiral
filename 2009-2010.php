3fda2b93ee871b275593499b5d8e722752799d98
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:09:32 -0400

    2009-2010 update

--- 2009-2010.php
+++ 2009-2010.php
@@ -15,11 +15,11 @@ include '_inc/head.php';
 		<span class="tag">2009 &ndash; 2010</span>
 			<h2>Out of the Archive: Process and Progress</h2>
 			<p><span class="date">September 18 – October 30, 2009</span></p>
-			<p>A special exhibition installation, a gallery talk, a 54 page color catalogue, with several essayists, based on the recently launched <a href="http://artasiamerica.org">digital archive</a>.</p>
-			<p>Guest curator Angel Velasco Shaw selected four artists from a review of the 160 artists in artasiamerica.org. The printed catalogue has been edited and coordinated by Sarita Echavez See who chose four writers/literary scholars to review the artists' work: Karen Su, Karlyn Koh, and Jan Christian Bernabe.  In seeking to expand the ways in which AAAC has presented Asian American art in a community context, a critical writing approach was taken to broaden cultural criticism and interpret visual thinking.</p>
-			<p>The essays shed light on these artists in a variety of ways such as: the artists' relationship to the work that they exhibited at AAAC in the past; the critique and contextualization of their current work; and the national and international context for these artists' creative production. These essays launch an investigation into the shifting rhetorics of art criticism and cultural criticism. The goal was to open up a critical dialogue for these artists/art works and a critical language for engaging more substantively with Asian American art. </p>
-			<p>In the era of globalization, and the discourse associated with international art developments, the exploration of identity has come to be seen as passé in many sectors of the art world.  Yet the importance of visibility and recognition for Asian American artists, from the long view of history rather than the marketplace, continues.  In this light the Gallery Talk event of artists and writers held at White Box on Oct  7th was broadcast live on artasiamerica and remains accessible for <a href="http://www.ustream.tv/channel/out-of-the-archive-process-and-progress"> viewing online.</a> Catalogues can be purchased through a <a href="/support.php">donation</a> to the AAAC. A panel conversation on the exhibition and digital archive was held at the Tribeca Performing Arts Center on December 9, 2009.</p>
-			<p>This was the last exhibition held in the gallery at 26 Bowery, AAAC’s home for thirty-three years.</p>
+			<p>A special exhibition installation, a gallery talk, a 54 page color catalogue, with several essayists, based on the recently launched digital archive, artasiamerica.org.  This digital archive, a major undertaking three years in the making, consists of 10% of the total 1,500 Asian American artists entries in the original archive, which reflects important artists exhibited by AAAC since 1983, a sampling of 60 years and several generations of artistic innovations.</p>
+			<p>Guest curator Angel Velasco Shaw selected four artists from a review of the 160 artists in artasiamerica.org. The printed catalogue has been edited and coordinated by Sarita Echavez See who chose four writers/literary scholars to review the artists' work: Karen Su, Karlyn Koh, and Jan Christian Bernabe.  In seeking to expand the ways in which AAAC has presented Asian American art in a community context, a critical writing approach was taken to broaded cultural criticism to interpret visual thinking.</p>
+			<p>The essays shed light on these artists in a variety of ways such as: the artists' relationship to the work that they exhibited at AAAC in the past; the critique and contextualization of their current work; and the national and international context for these artists' creative production. These essays launch an investigation into the shifting rhetorics of art criticism and cultural criticism. The goal - open up a critical dialogue for these artists/art works and a critical language for engaging more substantively with Asian American art. </p>
+			<p>In the era of globalization, and the discourse associated with international art developments, the exploration of identity has come to be seen as passé in many sectors of the art world. Yet the importance of visibility and recognition for Asian American artists, from the long view of history rather than the marketplace, continues. In this light the Gallery Talk event of artists and writers held at White Box on October 7 was broadcast live on artasiamerica and remains accessible for viewing.</p>
+			<p>Curated by Angel Velasco Shaw. <i><a href="https://www.flickr.com/photos/asianamericanartscentre/albums/72157622510814089" target="_blank">Learn more</a>.</i></p>
 			<img src="/_images/oota.png" alt="Works from Tomie Arai, Albert Chong, John Yoyogi Fortes, Swati Khurana in the exhibition Out of the Archive">
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
@@ -29,9 +29,9 @@ include '_inc/head.php';
 				<li>Swati Khurana</li>
 			</ul>
 
-			<h2>Out of the Archive: Progress and Community</h2>
+			<h2>Dialogues in the Visual Arts</h2>
 			<p><span class="date">December 9, 2009</span></p>
-			<p>Held at Tribeca Performing Arts Center, Borough of Manhattan Community College (BMCC). Moderated by Robert Lee. DVA curation by Susan Fleminger.</p>
+			<p>A Program of the Tribeca Arts Center sponsors Out of the Archive:  Progress and Community. Held at Tribeca Performing Arts Center, BMCC. Moderated by Robert Lee. With DVA Curator Susan Fleminger.</p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Nathalie Pham</li>
@@ -41,9 +41,36 @@ include '_inc/head.php';
 
 			<h2>China on the Way to Modernization: From the Republic to the Present</h2>
 			<p><span class="date">December 13, 2009</span></p>
-			<p>Two lectures by Dr. Fang Lili and Zhu Legeng, held at University Settlemtn, Houston St Center.</p>
-			<p>In her talk, "Inheritance and Development of Jingdezhen Ceramic Craftsmanship," Fang Lili discussed her study of how the handicraft of China has been adapted to modern times. She delineates three stages in China’s modernization and describes the relationships between tradition and modernization at Jingdezhen, a historic center for ceramics. Jindezhen is the case study because its traditional ceramic craftsmanship and the culture system based on it were attacked and essentially overthrown by a modern, industrial system. The craft’s native characteristics persisted and was preserved well in the new internationalized culture. The belief in tradition as a resource for a new culture and economy won out against the attack where reason opposed custom.</p>
-			<p>Zhu Legeng from the Chinese Academy of Arts' talk, entitled, "My Odyssey in Ceramics," recounted how the artist overcame obstacles of the communist cooperative factory ceramics system in China to found and develop one of the first private ceramics studios in the post-Mao era.</p>
+			<p>Two lectures by Dr. Fang Lili and Zhu Legeng. In “Inheritance and Development of Jingdezhen Ceramic Craftsmanship,” Fang Lili discussed her study of how the handicraft of China has been adapted to modern times. She delineates three stages in China’s modernization and describes the relationships between tradition and modernization at Jingdezhen, a historic center for ceramics. Jindezhen is the case study because its traditional ceramic craftsmanship and the culture system based on it were attacked and essentially overthrown by a modern, industrial system. The craft’s native characteristics persisted and was preserved well in the new internationalized culture. The belief in tradition as a resource for a new culture and economy won out against the attack where reason opposed custom. Zhu Legeng from the Chinese Academy of Arts gave a talk entitled, "My Odyssey in Ceramics." He recounted how he overcame obstacles of the communist cooperative factory ceramics system in China to found and develop one of the first private ceramics studios in the post-Mao era. Held at University Settlement, Houston St Center.</p>
+			<h6>Participating artists:</h6>
+			<ul class="list-columns">
+				<li>Dr. Fang Lili</li>
+				<li>Zhu Legeng</li>
+			</ul>
+
+			<h2>America’s Chinatown Voices: Silent Auction & Exhibition 藝匯唐人街</h2>
+			<p><span class="date">April 22 - May 9, 2010</span></p>
+			<p>Red Panels from Summer 2009 Outdoor Exhibition in Columbus Park, Closing Silent Auction & Mom’s Day Party. The artists, Nathalie Pham and Avani Patel, generously donated the sale of the panels to benefit Asian American Arts Centre.  In addition, photographs of Chinatown’s community life and activities in the midst of Columbus Park and the red panels were available to bid on. Held at Clemente Soto Vélez on Suffolk St, 3rd floor and online at www.chinatownvoicesNY.com.</p>
+			<h6>Participating artists:</h6>
+			<ul class="list-columns">
+				<li>Nathalie Pham</li>
+				<li>Avani Patel</li>
+			</ul>
+
+			<h2>Eight Artists: From the AAAC Archive</h2>
+			<p><span class="date">August 14 – September 11, 2010</span></p>
+			<p>In collaboration with Cuchifritos: The Artists Alliance Project/Gallery Space at Essex Street Market. Focused on AAAC’s digital archive, artasiamerica.org, with its availability and presence on site. Public tours as an introduction were promoted and booked.</p>
+			<h6>Participating artists:</h6>
+			<ul class="list-columns">
+				<li>Sin-ying Ho</li>
+				<li>Nancy Hom</li>
+				<li>Eunjung Hwang</li>
+				<li>Dorothy Imagire</li>
+				<li>Dinh Q. Le</li>
+				<li>Howardina Pindell</li>
+				<li>Roger Shimomura</li>
+				<li>Charles Yuen</li>
+			</ul>
 	</main>
 	<div class="fade"></div>
 	<nav role="navigation" id="mm">
