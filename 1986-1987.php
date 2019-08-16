bb9dee4e25e597d6a8bd004020f95c94bd63a145
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:00:09 -0400

    1986-1987 update

--- 1986-1987.php
+++ 1986-1987.php
@@ -15,7 +15,7 @@ include '_inc/head.php';
 		<span class="tag">1986 &ndash; 1987</span>
 			<h2>Roots to Reality II: Alternate Visions</h2>
 			<p><span class="date">October 17 - November 26, 1986</span></p>
-			<p>A festival of Asian American visual and performing arts and collaboration between Henry St. Settlement and the Alliance for Asian American Arts and Culture. Held at Henry St. Settlement. Introduction by Margo Machida. Project direction by Joyce Griffen. Catalogue includes introduction by Fred Wei-han Houn.</p>
+			<p>2nd Annual Asian American Multidisciplinary Arts Event. A Collaboration: Henry St. Settlement and the Alliance for Asian American Arts and Culture. A Festival of Asian American Visual & Performing Arts. Catalogue, 36 pages, with introduction by Fred Wei-han Houn, Exec. Committee. Exhibition introduction by Margo Machida, Project Director. Other selection committees for Dance, Media, Music, Theatre/Poetry. Project Director Joyce Griffen. Held at Henry St. Settlement.</p>
 			<h6>Visual Artists selection committee:</h6>
 			<ul class="list-columns">
 				<li>David Diao</li>
@@ -43,7 +43,7 @@ include '_inc/head.php';
 
 			<h2>The Mind’s I, Part 1</h2>
 			<p><span class="date">February 6 – March 6, 1987</span></p>
-			<p>First of four parts in a series that invites artists of diverse backgrounds to approach their changing self-conceptions. Curated by Robert Lee.</p>
+			<p>First in a series, an exhibition on the concept of Self in transition. Artists of diverse backgrounds approach their changing self-conceptions. “A merit society is designed for productivity then whose role is it to bring cultures and people together? Somehow it begins, the embrace not the fear of the question of difference…and the vision to accept the multicultural destiny of these United Blue Lands.” Curated and introduction by Robert Lee. Catalogue essay by Lowery S. Sims. <i><a href="http://artasiamerica.org/documents/8569/51" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Banny Andrew</li>
@@ -54,7 +54,7 @@ include '_inc/head.php';
 
 			<h2>The Mind’s I, Part 2</h2>
 			<p><span class="date">March 13 – April 10, 1987</span></p>
-			<p>Second of four parts in a series that invites artists of diverse backgrounds to approach their changing self-conceptions. Curated by Robert Lee.</p>
+			<p>Second in a series, an exhibition on the concept of Self in transition. Artists of diverse backgrounds approach their changing self-conceptions. “People have stubbornly tied their lives to their moorings although the moorings themselves are afloat…Love our artists! Their efforts and strategems to win and affirm us demonstrate there is no easy or straightforward way…” Curated by and introduction by Robert Lee. Catalogue essay by Susana Torruella Leval. <i><a href="http://artasiamerica.org/documents/8569/51" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Luis Cruz Azaceta</li>
@@ -64,7 +64,7 @@ include '_inc/head.php';
 
 			<h2>The Mind’s I, Part 3</h2>
 			<p><span class="date">April 17 – May 12, 1987</span></p>
-			<p>Third of four parts in a series that invites artists of diverse backgrounds to approach their changing self-conceptions. Curated by Robert Lee.</p>
+			<p>keep whole the multiple contradictions of our milieu. A temple, a stronghold to which we are invited, a private alter encrusted with the heart of its maker. Nest building – a guide to cultural continuity and a humanity unsuppressed.” Curated and introduction by Robert Lee. Catalogue essay by Grace Stanislaus. <i><a href="http://artasiamerica.org/documents/8569/51" target="_blank">Learn more</a>.</i></p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>William Jung</li>
@@ -72,9 +72,20 @@ include '_inc/head.php';
 				<li>Jorge Tacla</li>
 			</ul>
 
+			<h2>The Mind’s I, Part 4</h2>
+			<p><span class="date">May 29 – June 23, 1987</span></p>
+			<p>Fourth and last in a series, an exhibition on the concept of Self in transition. Artists of diverse backgrounds approach their changing self-conceptions. It is said in The Self Beseiged intro, consciousness evolves qualities of irony, satire, a critical imaginary of difference, a heroic effort to protect and empower ourselves. Each artist finds their own way to emerge from the siege. Altering what is as fundamental as one’s self concept, as the writer Russell Leong says, ‘brings the water home.’ Curated and introduction by Robert Lee. Catalogue essay by Russell Leong. <i><a href="http://artasiamerica.org/documents/8569/51" target="_blank">Learn more</a>.</i> </p>
+			<h6>Participating artists:</h6>
+			<ul class="list-columns">
+				<li>Albert Chong</li>
+				<li>Linda Peer</li>
+				<li>Alison Saar</li>
+				<li>Patti Warashina</li>
+			</ul>
+
 			<h2>The 3rd Annual Open Studio Show</h2>
 			<p><span class="date">May 1987</span></p>
-			<p>Featured guided tours to thirteen artists' studios in the Chinatown community, and a group show of works by sixteen artists. </p>
+			<p>Featured guided tours to seven artists studios in Chinatown commnity, and a group show of works by sixteen artists. <i><a href="http://artasiamerica.org/documents/3237/194" target="_blank">Learn more</a>.</i></p>
 			<h6>Open studio participants:</h6>
 			<ul class="list-columns">
 				<li>Amy Cheng</li>
@@ -102,17 +113,6 @@ include '_inc/head.php';
 				<li>Carol Sun</li>
 				<li>Carrie Yamoaka</li>
 				<li>Teru Yoshida</li>
-			</ul>
-
-			<h2>The Mind’s I, Part 4</h2>
-			<p><span class="date">May 29 – June 23, 1987</span></p>
-			<p>Final of four parts in a series that invites artists of diverse backgrounds to approach their changing self-conceptions. Curated by Robert Lee.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Albert Chong</li>
-				<li>Linda Peer</li>
-				<li>Alison Saar</li>
-				<li>Patti Warashina</li>
 			</ul>
 		</main>
 	<div class="fade"></div>
