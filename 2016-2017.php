7688af0636657be7c2b1046a14b4340d656bfbbf
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:10:54 -0400

    2016-2017 update

--- 2016-2017.php
+++ 2016-2017.php
@@ -15,7 +15,7 @@ include '_inc/head.php';
 		<span class="tag">2016 &ndash; 2017</span>
 			<h2>First Responders</h2>
 			<p><span class="date">September 10 - 30, 2016</span></p>
-			<p>A solo exhibition featuring the work of artist Yang Yi 收件人. Presented in collaboration with Chinatown Soup and in partnership with Chinatown Partnership Local Development Corporation (CPLDC). It is fitting that as Americans we should find an immigrant whose remarkable skills and devotion gives to us the fruits of his heart and his labor. Yang Yi's pentaptych took five years to complete and gives tribute to the brave members of the FDNY and paramedics, featuring all 343 faces of the first responders who were killed at the World Trade Center. Sited at 168 Orchard St.</p>
+			<p>A solo exhibition featuring the work of artist Yang Yi 收件人. Presented in collaboration with Chinatown Soup and in partnership with Chinatown Partnership Local Development Corporation (CPLDC). It is fitting that as Americans we should find an immigrant whose remarkable skills and devotion give to us the fruits of his heart and his labor. Yang Yi’s pentaptych took five years to complete and gives tribute to the brave members of the FDNY and paramedics, featuring all 343 faces of the first responders who were killed at the World Trade Center. Held at 168 Orchard St.  <i><a href="http://artspiral.blogspot.com/2015/09/exhibition-aboveground40-moments-of.html" target="_blank">Learn more</a></i>.</p>
 			<h6>Participating artists:</h6>
 			<ul class="list-columns">
 				<li>Yang Yi</li>
