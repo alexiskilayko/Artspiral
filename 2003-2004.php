7429e87c9af601b0e57542b77fa78df4ec3c3915
Alexis Kilayko <alexis.kilayko@gmail.com>
2019-08-16 17:08:03 -0400

    2003-2004

--- 2003-2004.php
+++ 2003-2004.php
@@ -7,18 +7,18 @@ include '_inc/head.php';
 
 <body class="exhibitions">
 <div class="page-wrapper">
-	<?php include '_inc/header.php'; ?>
-	<nav id="menu" role="navigation"> <!--HERE -->
-	<?php include '_inc/menu.php';?>
-	</nav><!--HERE -->
-	<main class="content-wrapper narrow" role="main"><!--HERE -->
-		<span class="tag">2003 &ndash; 2004</span>
-			<h2>In the Shadow of 9/11: A Chinatown Memorial Exhibition</h2>
-      <p><span class="date">September 11 – October 15, 2003</span> extended to <span class="date">November 3, 2003</span></p>
-      <p>An exhibition held in recognition of the events at the World Trade Center and the hardship experienced by the Chinatown community as a result of 9/11. This exhibition was created in collaboration between AAAC and SilkRoad Place to revive the attractiveness of Chinatown utilizing the asset of culture and the arts. Exhibited art works document what happened at the WTC site, the reaction in Chinatown itself, even the personal experience of being related to a professional who originally designed the Towers, among many other subjects. Held at The Gallery at Silk Road Place, 30 Mott Street. Organized by Bob Lee & Chee Wang Ng.</p>
+  <?php include '_inc/header.php'; ?>
+  <nav id="menu" role="navigation"> <!--HERE -->
+  <?php include '_inc/menu.php';?>
+  </nav><!--HERE -->
+  <main class="content-wrapper narrow" role="main"><!--HERE -->
+    <span class="tag">2003 &ndash; 2004</span>
+      <h2>In the Shadow of 9/11: A Chinatown Memorial Exhibition</h2>
+      <p><span class="date">September 11 – October 15, extended to November 3, 2003</span></p>
+      <p>An exhibition held in recognition of the events at the World Trade Center and the hardship experienced by the Chinatown community as a result of 9/11. This exhibition was created in collaboration between AAAC and SilkRoad Place to revive the attractiveness of Chinatown utilizing the asset of culture and the arts. Exhibited art works document what happened at the WTC site, the reaction in Chinatown itself, even the personal experience of being related to a professional who originally designed the Towers, among many other subjects. Held at The Gallery at Silk Road Place, 30 Mott Street. Organized by Bob Lee & Chee Wang Ng. <i>Learn more: <a href="http://artasiamerica.org/documents/2158/45" target="_blank">postcard</a>, <a href="http://artasiamerica.org/documents/2159/45" target="_blank">press release</a>.</i></p>
       <h6>Participating artists:</h6>
-			<ul class="list-columns">
-				<li>Alan Chin</li>
+      <ul class="list-columns">
+        <li>Alan Chin</li>
         <li>Sung-Hee Choi</li>
         <li>G.L. Eng</li>
         <li>Lambert Fernando </li>
@@ -36,13 +36,20 @@ include '_inc/head.php';
         <li>Toyo Tsuchiya, </li>
         <li>Katarina Wong</li>
         <li>Yoichiro Yoda</li>
-			</ul>
+      </ul>
 
-      <h2>Provocations: 13th AAAC Annual Exhibition</h2>
-			<p><span class="date">September 26 – November 7, 2003</span></p>
-      <p>AAAC in collaboration with The Korea Society.</p>
-      <p>Selection Panel: Timothy Liu, Jeff Koan Baysa, Susette Min, Midori Yoshimoto, & Christine Y. Kim.</p>
-			<h6>Participating artists:</h6>
+      <h2>13th AAAC Annual: Provocations</h2>
+      <p><span class="date">September 26 – November 7, 2003</span></p>
+      <p>AAAC in collaboration with The Korea Society. Containing a wide variety of works, this exhibition, “Provocations,” lives up to its name. The show reveals the range of directions of Asian American artists today, and the artworks pose and provoke an array of especially striking ideas and sentiments. <i>Learn more: <a href="http://artasiamerica.org/documents/3324/146" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/3325/146" target="_blank">press release</a>.</i></p>
+      <h6>Selection panelists:</h6>
+      <ul class="list-columns">
+        <li>Timothy Liu</li>
+        <li>Jeff Koan Baysa</li>
+        <li>Susette Min</li>
+        <li>Midori Yoshimoto</li>
+        <li>Christine Y. Kim</li>
+      </ul>
+      <h6>Participating artists:</h6>
       <ul class="list-columns">
         <li>Toby Barnes</li>
         <li>Irene Chan</li>
@@ -57,11 +64,10 @@ include '_inc/head.php';
       </ul>
 
       <h2>dream so much 2</h2>
-			<p><span class="date">November 21, 2003 – January 9, 2004</span></p>
-      <p>Guest Curated by Richard S. Chang and Alexandra Chang.</p>
-      <p>Japanese graffiti artists who have relocated to New York City are included in this exhibition of 2ned generation developments in graffiti. The artists' fluid movement between overlapping cultures in the realm of the cultural hybrid defy artificial definitions of cultural authenticity or straightforward appropriation.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
+      <p><span class="date">November 21, 2003 – January 9, 2004</span></p>
+      <p>Japanese graffiti artists who have relocated to New York City are included in this exhibition of 2nd generation developments in graffiti. The artists’ works demonstrate fluid movement between overlapping cultures in the realm of the cultural hybrid, defying artificial definitions of cultural authenticity or straightforward appropriation. Guest Curated by Richard S. Chang and Alexandra Chang.</p>
+      <h6>Participating artists:</h6>
+      <ul class="list-columns">
         <li>James Marshall</li>
         <li>Kenji Hirata</li>
         <li>Sandro Tchikovani</li>
@@ -69,30 +75,34 @@ include '_inc/head.php';
       </ul>
 
       <h2>Landscape and Memory</h2>
-			<p><span class="date">March 19 – April 30, 2004</span></p>
-      <p>A collaboration with the Sumei Multidisciplinary Arts Center.</p>
-      <p>Organized by Yoland Skeete & Hal Laessig .</p>
-      <p>Two artists whose works have developed in compelling, transcending ways: Ken Fukushima links historical Japan to present thought. Wei Jane Chirs abstractions seek sublime goals. Her measurements of mundane urban experience basks in the music of silence while Fukushima's work exults in the music of time, and its progeny and history.</p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
+      <p><span class="date">March 19 – April 30, 2004</span></p>
+      <p>Organized by Yoland Skeete & Hal Laessig as a collaboration with the Sumei Multidisciplinary Arts Center. Presented two artists whose works have developed in compelling, transcending ways. Ken Fukushima holds a scale up to experience, measuring one history to another, one culture to another, linking historical Japan to present thought. Wei Jane Chir’s abstractions seek sublime goals. Her measurements of mundane urban experience basks in the music of silence while Fukushima’s work exults in the music of time and its progeny and history. <i>Learn more: <a href="http://artasiamerica.org/documents/2618/52" target="_blank">flyer</a>, <a href="http://artasiamerica.org/documents/2623/52" target="_blank">press release</a>, <a href="http://artasiamerica.org/artist/detail/52" target="_blank">artist profile</a>.</i></p>
+      <h6>Participating artists:</h6>
+      <ul class="list-columns">
         <li>Ken Fukushima</li>
         <li>Wei Jane Chir</li>
       </ul>
 
-     <h2>Mid Career: Carol Sun/Nobuho Nagasawa</h2>
-			<p><span class="date">May 22 – June 25, 2004</span></p>
-      <p>Nominating Panelists: Ko'an Jeff Baysa, Emily Cheng, Johnathan Goodman, Lilly Wei.</p>
-      <p>Two artists' installations: Carol Sun's video installation builds on her grandfathers who never met, one influenced by the West, developing the notion of legacy and family into a wider dialogue of communities and cultures. Nobuho Nagasawa's "Mapping Chinatown: Making the Invisible Visible", collaborates with art students of SUNY Manhattan to print information about Chinatown on specially designed chopstick sleeves distributed to co-operating local restaurants. </p>
-			<h6>Participating artists:</h6>
-			<ul class="list-columns">
+      <h2>Carol Sun / Nobuho Nagasawa at Mid Career</h2>
+      <p><span class="date">May 22 – June 25, 2004</span></p>
+      <p>Two artist’s installations. Carol Sun’s video installation builds on her grandfathers who never met, one influenced by the West, and brings the notion of legacy and family into a wider dialogue of communities and cultures. Nobuho Nagasawa’s "Mapping Chinatown: Making the Invisible Visible" collaborates with art students of SUNY Manhattan to print information about Chinatown on specially designed chopstick sleeves distributed to co-operating local restaurants. <i>Learn more: <a href="http://artasiamerica.org/documents/992/178" target="_blank">flyer</a>, <a href="http://artasiamerica.org/artist/detail/90" target="_blank">artist profile</a>.</i></p>
+      <h6>Nominating Panelists:</h6>
+      <ul class="list-columns">
+        <li>Ko'an Jeff Baysa</li>
+        <li>Emily Cheng</li>
+        <li>Johnathan Goodman</li>
+        <li>Lilly Wei</li>
+      </ul>
+      <h6>Participating artists:</h6>
+      <ul class="list-columns">
         <li>Carol Sun</li>
         <li>Nobuho Nagasawa</li>
       </ul>
-	</main><!--HERE -->
-	<div class="fade"></div>
-	<nav id="mm" role="navigation"><!--HERE -->
-	<?php include '_inc/menu.php'; ?>
-	</nav><!--HERE -->
+  </main><!--HERE -->
+  <div class="fade"></div>
+  <nav id="mm" role="navigation"><!--HERE -->
+  <?php include '_inc/menu.php'; ?>
+  </nav><!--HERE -->
 </div>
 </body>
 </html>
